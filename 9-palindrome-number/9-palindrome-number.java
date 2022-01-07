713eca58c06d11883fff359f7f119ab5b157be15
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-01-06 21:26:02 -0700

    Time: 16 ms (20.67%), Space: 41.2 MB (22.98%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int reverse = 0, origin = x;
+        while(x > 0) {
+            reverse = reverse * 10 + x % 10;
+            x /= 10;
+        }
+        return reverse == origin;
+    }
+}
