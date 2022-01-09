d3575a6c9e38ec00ed7a262a1c767d82d75b06d6
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-01-09 15:45:24 -0700

    Time: 2 ms (97.26%), Space: 37.5 MB (61.81%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -2,14 +2,22 @@ class Solution {
     public int countSubstrings(String s) {
         //dp
         int n = s.length();
-        boolean[][] dp = new boolean[n][n];
+        int l = 0, r = 0, res = 0;
+        
+        for(int i = 0; i < n; i++) {
+            int len1 = findPanlindrome(s, i, i);
+            int len2 = findPanlindrome(s, i , i + 1);
+            res += len1 + len2;
+        }
+        return res;
+    }
+    public int findPanlindrome(String s, int l, int r) {
         int res = 0;
         
-        for(int i = n - 1; i >= 0; i--) {
-            for(int j = i; j < n; j++) {
-                dp[i][j] = (s.charAt(i) == s.charAt(j)) && (j - i <= 2 || dp[i + 1][j - 1]);
-                if(dp[i][j]) res++;
-            }
+        while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
+            res++;
         }
         return res;
     }
