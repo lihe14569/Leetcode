fd4246bff7d0e1f0ce50d0069fa14959718593fa
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-01-10 21:50:06 -0700

    Time: 47 ms (7.21%), Space: 63.3 MB (5.04%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,17 +1,20 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        //dp 双序列 bottom-up
+        //TOP DOWN: DFS+ MEMO
         int m = text1.length(), n = text2.length();
-        int[][] dp = new int[m + 1][n + 1];
-        
-        for(int i = 1; i <= m; i++) {
-            for(int j = 1; j <= n; j++) {
-                if(text1.charAt(i - 1) == text2.charAt(j - 1))
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                else 
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j  -1]);
-            }
+        //小技巧：如果不能确定是否访问的的值，可以用wrapper class
+        Integer[][] memo = new Integer[m][n];
+        return dfs(text1, text2, 0, 0, memo);
+    }
+    
+    public int dfs(String text1, String text2, int s1, int s2, Integer[][] memo) {
+        if(s1 >= text1.length() || s2 == text2.length()) return 0;
+        if(memo[s1][s2] != null) return memo[s1][s2];
+        if(text1.charAt(s1) == text2.charAt(s2)) {
+            memo[s1][s2] = 1 + dfs(text1, text2, s1 + 1, s2 + 1, memo);
+        } else {
+            memo[s1][s2] = Math.max(dfs(text1, text2, s1 + 1, s2, memo), dfs(text1, text2, s1, s2 + 1, memo));
         }
-        return dp[m][n];
+        return memo[s1][s2];
     }
 }
