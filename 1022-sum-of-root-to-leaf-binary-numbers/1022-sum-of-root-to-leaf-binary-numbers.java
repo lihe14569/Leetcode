23d8c7a630342b42c8d82c748e853aeb5f285014
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-01-10 20:17:51 -0700

    Time: 0 ms (100.00%), Space: 39.2 MB (35.37%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
@@ -15,29 +15,12 @@
  */
 class Solution {
     public int sumRootToLeaf(TreeNode root) {
-        //backtrack
-        List<String> list = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        dfs(root, list, sb);
-        for(String s: list) {
-            System.out.println(s);
-        }
-        return list.stream().map(str -> Integer.parseInt(str, 2)).collect(Collectors.summingInt(Integer::intValue));
+        return dfs(root, 0);
     }
     
-    public void dfs(TreeNode root, List<String> list, StringBuilder sb) {
-        //base case
-        if(root.left == null && root.right == null) {
-            int last = sb.length();
-            sb.append(root.val);
-            list.add(sb.toString());
-            sb.deleteCharAt(last);
-            return;
-        }
-        int len =  sb.length();
-        sb.append(root.val);
-        if(root.left != null) dfs(root.left, list, sb);
-        if(root.right != null) dfs(root.right, list, sb);
-        sb.deleteCharAt(len);
+    public int dfs(TreeNode root, int val) {
+        if(root == null) return 0;
+        val = val * 2 + root.val;
+        return root.left == root.right ? val : dfs(root.left, val) + dfs(root.right, val);
     }
 }
