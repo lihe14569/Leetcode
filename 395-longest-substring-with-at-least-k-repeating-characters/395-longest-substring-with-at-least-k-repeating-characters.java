676552fa3036dbfeb439b8ecdac5906d9dc90b03
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-01-14 00:36:45 -0700

    Time: 54 ms (27.23%), Space: 43.3 MB (6.02%) - LeetHub

--- 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.java
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.java
@@ -1,28 +1,26 @@
 class Solution {
     public int longestSubstring(String s, int k) {
-        //set unique number of characters
-        int res = 0;
-        for(int i = 1; i <= 26; i++) {
+        //1.window is number of unique character
+        //2. variable cnt to record repeating characters
+        int n = s.length(), res = 0;
+
+        for(int i = 0; i < 26; i++) {
             Map<Character, Integer> map = new HashMap<>();
-            int N = s.length();
-            int left = 0, cnt = 0;
-            for(int j = 0; j < s.length(); j++) {
-                //进
+            int cnt = 0;
+            int l = 0;
+            for(int j = 0; j < n; j++) {
                 char c1 = s.charAt(j);
                 map.put(c1, map.getOrDefault(c1, 0) + 1);
                 if(map.get(c1) == k) cnt++;
-                //出
                 while(map.size() > i) {
-                    char c2 = s.charAt(left);
-                    if(map.getOrDefault(c2, 0) == k) cnt--;
-                    map.put(c2, map.getOrDefault(c2, 0) - 1);
-                    if(map.getOrDefault(c2, 0) <= 0) map.remove(c2);
-                    left++;
+                    char c2 = s.charAt(l);
+                    if(map.get(c2) == k) cnt--;
+                    map.put(c2, map.get(c2) - 1);
+                    if(map.get(c2) == 0) map.remove(c2);
+                    l++;
                 }
-                //算
-                int size = map.size();
-                if(size == i && size == cnt)
-                    res = Math.max(res, j - left + 1);
+                if(map.size() == cnt)
+                    res = Math.max(res, j - l + 1);
             }
         }
         return res;
