d59ff550d879026647dea63cddeae053b0b0effa
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-01-15 22:35:35 -0700

    Time: 17 ms (100.00%), Space: 56.3 MB (100.00%) - LeetHub

---
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
@@ -0,0 +1,33 @@
+class Solution {
+    public long maxRunTime(int n, int[] batteries) {
+        long sum = 0;
+        for(int b : batteries) sum += b;
+        long l  = 0, r = sum / n;
+        long res =  0;
+        while(l < r) {
+            //question ask maximum number
+            long m = (l + r + 1) / 2;
+            if(canFit(n, m, batteries)) {
+                res = m;
+                l = m;
+            } else {
+                r = m - 1;
+            }
+        }
+        return l;
+    }
+    
+    public boolean canFit(int n, long time, int[] batt) {
+        long sum = 0;
+        long target = n * time;
+        for(int b : batt) {
+            if(b < time) {
+                sum += b;
+            } else {
+                sum += time;
+            }
+            if(sum >= target) return true;
+        }
+        return sum >= target;
+    }
+}
