a1371dfbb8ff906f8c1912fcb7e639b58ecad365
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-01-26 15:13:21 -0700

    Time: 54 ms (7.41%), Space: 58.6 MB (7.84%) - LeetHub

--- 38-count-and-say/38-count-and-say.java
+++ 38-count-and-say/38-count-and-say.java
@@ -1,16 +1,15 @@
 class Solution {
     public String countAndSay(int n) {
-        String s  = "1";
-        for(int i = 1; i <n ; i++) {
+        String s = "1";
+        for(int i = 1; i < n; i++) {
             String temp = "";
             for(int j = 0; j < s.length(); j++) {
                 int k = j;
-                while(k < s.length() && s.charAt(k) == s.charAt(j)) k++;
+                while(k < s.length() && s.charAt(j) == s.charAt(k)) k++;
                 temp += String.valueOf(k - j) + s.charAt(j);
-                //注意更新j,跳回k -1,因为循环之后就还要加一
-                j = k - 1;
+                j = k - 1; //易错点! 因为下一for loop 还要+1
             }
-            s = temp;
+            s=temp;
         }
         return s;
     }
