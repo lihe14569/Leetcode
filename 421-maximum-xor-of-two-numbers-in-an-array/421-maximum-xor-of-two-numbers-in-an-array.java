77802e1f75fb8119ed807a40f1715b999510346f
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-01-27 01:01:03 -0700

    Time: 133 ms (71.10%), Space: 103 MB (38.58%) - LeetHub

--- 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.java
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.java
@@ -1,25 +1,44 @@
 class Solution {
+    class Trie {
+        Trie[] children;
+        public Trie() {
+            this.children = new Trie[2];
+        }
+    }
     public int findMaximumXOR(int[] nums) {
-        //bitwise + hashset
-        int n = nums.length;
-        int max = 0;
-        for(int num : nums) max = Math.max(max, num);
-        //记住java method
-        int len = (Integer.toBinaryString(max).length()); 
-        
-        int maxXor = 0, currXor = 0;
-        for(int i = len - 1; i >= 0; i--) {
-            maxXor <<= 1;
-            Set<Integer> prefix = new HashSet<>();
-            currXor = maxXor | 1;
-            for(int num : nums) prefix.add(num >> i);
-            for(int p : prefix) {
-                if(prefix.contains(currXor^p)) {
-                    maxXor = currXor;
-                    break;
+        //bitwise trie
+        int max = Integer.MIN_VALUE;
+        for(int num : nums) max = Math.max(num, max);
+        int len = (Integer.toBinaryString(max).length());
+        Trie root = new Trie();
+        //构造trie
+        for(int num : nums) {
+            Trie node = root;
+            for(int i = len - 1; i >=0; i--) {
+                int bit = (num >> i) & 1;
+                if(node.children[bit] == null) {
+                    node.children[bit] = new Trie();
                 }
+                node = node.children[bit];
             }
         }
-        return maxXor;
+        //找最大的xor
+        int maxXOR = 0;
+        for(int num : nums) {
+            Trie node = root;
+            int currXOR = 0;
+            for(int i = len - 1; i >=0; i--) {
+                int bit = (num >> i) & 1;
+                if(node.children[bit ^ 1] != null) {
+                    currXOR = (currXOR << 1) | 1;
+                    node = node.children[bit ^ 1];
+                } else {
+                    currXOR = currXOR << 1;
+                    node = node.children[bit];
+                }
+            }
+            maxXOR = Math.max(maxXOR, currXOR);
+        }
+        return maxXOR;
     }
 }
