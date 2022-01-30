4fc92db83c3417aad7843b8697694583eee662e0
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-01-29 17:28:04 -0700

    Time: 45 ms (6.45%), Space: 54.8 MB (6.05%) - LeetHub

--- 85-maximal-rectangle/85-maximal-rectangle.java
+++ 85-maximal-rectangle/85-maximal-rectangle.java
@@ -1,21 +1,30 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
-        int[][] dp = new int[m][n]; //indicates ith row longest consecutive block
+        int[]dp = new int[n]; //indicates ith row longest consecutive block
         int maxArea = 0;
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(matrix[i][j] == '1') {
-                    dp[i][j] = j == 0 ? 1 : dp[i][j - 1] + 1;
-                }
-                int width = dp[i][j];
-                for(int k = i; k >=0; k--) {
-                    //要用width更新，因为有效的width一直要保持在k 到i 内最小
-                    width = Math.min(width, dp[k][j]);
-                    maxArea = Math.max(maxArea, width * (i - k + 1));
-                }
+                dp[j] = matrix[i][j] == '1' ? dp[j] + 1 : 0;
             }
+            maxArea = Math.max(maxArea, findLargestRec(dp));
         }
         return maxArea;
+    }
+    public int findLargestRec(int[] dp) {
+        int n = dp.length;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int res = 0;
+        for(int i = 0; i < n; i++) {
+            while(stack.peek() != -1 && dp[i] <= dp[stack.peek()]) {
+                res = Math.max(res, dp[stack.pop()] * (i - stack.peek() - 1));
+            }
+            stack.push(i);
+        }
+        while(stack.peek() != -1) {
+            res = Math.max(res, dp[stack.pop()] * (n - stack.peek() - 1));
+        }
+        return res;
     }
 }
