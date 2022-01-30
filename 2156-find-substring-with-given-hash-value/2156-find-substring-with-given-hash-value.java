38753abb920712a582c766e970ddb991649c1a9a
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-01-30 14:18:45 -0700

    Time: 44 ms (11.11%), Space: 42.5 MB (11.11%) - LeetHub

--- 2156-find-substring-with-given-hash-value/2156-find-substring-with-given-hash-value.java
+++ 2156-find-substring-with-given-hash-value/2156-find-substring-with-given-hash-value.java
@@ -1,21 +1,19 @@
 class Solution {
     public String subStrHash(String s, int power, int modulo, int k, int hashValue) {
-        int len = s.length();
+        int n = s.length();
         long weight = 1;
-        for(int i = 0; i < k -1 ; i++) {
-            weight = (weight * power) % modulo;
-        }
-        int r = len - 1;
-        long hash = 0;
+        for(int i = 0; i < k - 1; i++) weight = (weight * power) % modulo;
         String res = "";
-        for(int i = len - 1; i >= 0; i--) {
+        int r = n - 1;
+        long hash = 0;
+        
+        for(int i = n -1; i>=0; i--) {
             int val = s.charAt(i) - 'a' + 1;
             hash = (hash * power % modulo + val) % modulo;
             if(r - i + 1 == k) {
-                if(hash == hashValue) {
+                if(hash == hashValue)
                     res = s.substring(i, r + 1);
-                }
-                //remove head
+                //remove higher order element
                 hash = (hash + modulo - (s.charAt(r--) - 'a' + 1) * weight % modulo) % modulo;
             }
         }
