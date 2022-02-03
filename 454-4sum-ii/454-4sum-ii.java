192edbe6c8642834be856b233f3f1a7b0555f6f9
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-02-03 00:36:22 -0700

    Time: 174 ms (49.64%), Space: 57 MB (26.98%) - LeetHub

---
+++ 454-4sum-ii/454-4sum-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        //one pair as one element, turn into two sum probem
+        Map<Integer, Integer> map = new HashMap<>();
+        int res = 0;
+        for(int i = 0; i < nums1.length; i++) {
+            for(int j = 0; j < nums2.length; j++) {
+                int sum = nums1[i] + nums2[j];
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+        }
+        
+        for(int i = 0; i < nums3.length; i++) {
+            for(int j = 0; j < nums4.length; j++) {
+                res += map.getOrDefault(-(nums3[i] + nums4[j]), 0);
+            }
+        }
+        return res;
+    }
+}
