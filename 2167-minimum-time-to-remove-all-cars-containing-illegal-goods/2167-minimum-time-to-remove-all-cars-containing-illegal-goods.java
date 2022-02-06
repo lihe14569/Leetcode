c124cbe7623ef72fbd9daeaa51bf8bc37a71ec1c
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-02-05 22:17:50 -0700

    Time: 57 ms (100.00%), Space: 82.7 MB (100.00%) - LeetHub

--- 2167-minimum-time-to-remove-all-cars-containing-illegal-goods/2167-minimum-time-to-remove-all-cars-containing-illegal-goods.java
+++ 2167-minimum-time-to-remove-all-cars-containing-illegal-goods/2167-minimum-time-to-remove-all-cars-containing-illegal-goods.java
@@ -1,10 +1,18 @@
 class Solution {
     public int minimumTime(String s) {
-        int n = s.length(), left = 0, right = 0, res = n;
-        //i represent the ith car is the last car to remove
+        int n = s.length();
+        int[] rmR = new int[n + 1];
+        //get minimum time removing car from right
+        for(int i = n -1; i >= 0; i--) {
+            rmR[i] = Math.min(rmR[i + 1] + (s.charAt(i) - '0') * 2, n - i);
+        }
+        //get minimum time from left
+        int minLeft = 0, res = n;
         for(int i = 0; i < n; i++) {
-            left = Math.min(left + (s.charAt(i) - '0') * 2, i + 1);
-            res = Math.min(res, left + n - i - 1);
+            if(s.charAt(i) == '1') {
+                minLeft = Math.min(minLeft + 2, i + 1);
+            }
+            res = Math.min(res, minLeft + rmR[i]);
         }
         return res;
     }
