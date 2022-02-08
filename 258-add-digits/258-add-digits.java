c7a202bcaba2923622db9dc3b0e3f6c314455bce
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-02-07 19:00:18 -0700

    Time: 4 ms (8.52%), Space: 41.4 MB (9.91%) - LeetHub

---
+++ 258-add-digits/258-add-digits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int addDigits(int num) {
+        
+        while(num >= 10) {
+            String s = String.valueOf(num);
+            int res = 0;
+            for(char c : s.toCharArray()) {
+                res += c - '0';
+            }
+            if(res < 10) {
+                return res;
+            }
+            num = res;
+        }
+        return num;
+    }
+}
