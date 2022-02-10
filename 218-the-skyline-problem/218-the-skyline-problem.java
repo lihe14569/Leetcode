1a7193908825f16527a553f7b20aa1012a84038d
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-02-10 00:56:19 -0700

    Time: 227 ms (26.73%), Space: 45.3 MB (62.61%) - LeetHub

--- 218-the-skyline-problem/218-the-skyline-problem.java
+++ 218-the-skyline-problem/218-the-skyline-problem.java
@@ -1,21 +1,21 @@
 class Solution {
     public List<List<Integer>> getSkyline(int[][] buildings) {
         List<List<Integer>> res = new ArrayList<>();
-        List<int[]> heights = new ArrayList<>();
+        List<int[]> lines = new ArrayList<>();
         for(int[] b : buildings) {
-            heights.add(new int[]{b[0], -b[2]});
-            heights.add(new int[]{b[1], b[2]});
+            lines.add(new int[]{b[0], -b[2]});
+            lines.add(new int[]{b[1], b[2]});
         }
-        Collections.sort(heights, (a, b) -> a[0] == b[0] ? Integer.compare(a[1], b[1]) : Integer.compare(a[0], b[0]));
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> Integer.compare(b, a));
-        int preMax = 0;
-        for(int[] h : heights) {
-            if(h[1] < 0) pq.offer(-h[1]);
-            else pq.remove(h[1]);
-            int currMax = pq.isEmpty() ? 0 : pq.peek();
-            if(currMax != preMax) {
-                res.add(Arrays.asList(h[0], currMax));
-                preMax = currMax;
+        Collections.sort(lines, (a, b) -> a[0] == b[0] ? Integer.compare(a[1], b[1]) : Integer.compare(a[0], b[0]));
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b- a);
+        int premax = 0;
+        for(int[] b : lines) {
+            if(b[1] < 0) pq.offer(-b[1]);
+            else pq.remove(b[1]);
+            int currmax = pq.isEmpty() ? 0 : pq.peek();
+            if(currmax != premax) {
+                res.add(List.of(b[0], currmax));
+                premax = currmax;
             }
         }
         return res;
