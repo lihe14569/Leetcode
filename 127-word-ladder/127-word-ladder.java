506892e4ac3f79258f7bb77f9bcecbf3ddfe0442
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-02-11 23:15:12 -0700

    Time: 27 ms (94.96%), Space: 54.1 MB (46.91%) - LeetHub

--- 127-word-ladder/127-word-ladder.java
+++ 127-word-ladder/127-word-ladder.java
@@ -9,20 +9,34 @@ class Solution {
         while(!beginSet.isEmpty()) {
             Set<String> nextSet = new HashSet<>();
             for(String s : beginSet) {
-                char[] chrs = s.toCharArray();
-                for(int i = 0; i < n; i++) {
-                    for(char c = 'a'; c <= 'z'; c++) {
-                        char pre = chrs[i];
-                        if(pre == c) continue;
-                        chrs[i] = c;
-                        String newS = String.valueOf(chrs);
-                        if(endSet.contains(newS)) return step + 1;
-                        if(dict.contains(newS)) {
-                            nextSet.add(newS);
-                            dict.remove(newS);
-                        }
-                        chrs[i] = pre;
-                    }
+                StringBuilder sb = new StringBuilder(s);
+                // for(int i = 0; i < n; i++) {
+                //     for(char c = 'a'; c <= 'z'; c++) {
+                //         char pre = chrs[i];
+                //         if(pre == c) continue;
+                //         chrs[i] = c;
+                //         String newS = String.valueOf(chrs);
+                //         if(endSet.contains(newS)) return step + 1;
+                //         if(dict.contains(newS)) {
+                //             nextSet.add(newS);
+                //             dict.remove(newS);
+                //         }
+                //         chrs[i] = pre;
+                //     }
+                // }
+                for(int i = 0; i < s.length(); i++) {
+                    char oc = s.charAt(i);
+                     for(char c = 'a'; c <= 'z'; c++) {
+                         if(c == oc) continue;
+                         sb.setCharAt(i, c);
+                         String ns = sb.toString();
+                         if(endSet.contains(ns)) return step + 1;
+                         if(dict.contains(ns)) {
+                             nextSet.add(ns);
+                             dict.remove(ns);
+                         }
+                         sb.setCharAt(i, oc);
+                     }
                 }
             }
             if(endSet.size() < nextSet.size()) {
