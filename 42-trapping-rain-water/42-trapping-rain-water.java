548862ed4372942b5a7cd5a206bff75ffff51818
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-02-16 02:15:32 -0700

    Time: 1 ms (92.58%), Space: 47.1 MB (5.07%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,17 +1,20 @@
 class Solution {
     public int trap(int[] height) {
         //two pointer
-        int res = 0;
-        Stack<Integer> stack = new Stack<>();
-        for(int i = 0; i < height.length; i++) {
-            while(!stack.isEmpty() && height[i] > height[stack.peek()]) {
-                int l = stack.pop();
-                if(stack.isEmpty()) break;
-                int h = Math.min(height[i], height[stack.peek()]);
-                int width = i-1 - stack.peek();
-                res += width * (h  - height[l]);
+        int l = 0,  r= height.length - 1, res = 0;
+        int lMax = 0, rMax = 0;
+        while(l < r) {
+            if(height[l] < height[r]) {
+                if(lMax < height[l]) lMax = height[l];
+                else {
+                    res += lMax - height[l];
+                }
+                l++;
+            } else {
+                if(rMax < height[r]) rMax = height[r];
+                else res += rMax - height[r];
+                r--;
             }
-            stack.push(i);
         }
         return res;
     }
