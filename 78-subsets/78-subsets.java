b3ee22b409937e29abeae7bd730eaecffb197dd8
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-02-16 21:32:07 -0700

    Time: 0 ms (100.00%), Space: 42.4 MB (46.52%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,21 +1,18 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        //            []
-        //.          /. \ \
-        //         [1] [2] [3]
-        //         /\   /\  /\
-        //        [2][3]
         List<List<Integer>> res = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-        dfs(nums, res, list, 0);
+        List<Integer> sol = new ArrayList<>();
+        
+        dfs(nums, res, sol, 0);
         return res;
     }
-    public void dfs(int[] nums, List<List<Integer>> res, List<Integer> list, int idx) {
-        res.add(new ArrayList<>(list));
-        for(int i = idx; i < nums.length; i++) {
-            list.add(nums[i]);
-            dfs(nums, res, list, i + 1);
-            list.remove(list.size() - 1);
+    public void dfs(int[] nums, List<List<Integer>> res, List<Integer> sol, int start) {
+        res.add(new ArrayList<>(sol));
+
+        for(int i = start; i < nums.length; i++) {
+            sol.add(nums[i]);
+            dfs(nums, res, sol, i + 1);
+            sol.remove(sol.size()  - 1);
         }
     }
 }
