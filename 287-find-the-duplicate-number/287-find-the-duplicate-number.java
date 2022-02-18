eddcc9bc4c0534c21ddd379ebe7581cd3be721d5
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-02-17 23:05:58 -0700

    Time: 10 ms (45.49%), Space: 75.3 MB (37.64%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,18 +1,16 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        int n = nums.length;
-        int l = 1, r = n - 1;
-        while( l < r) {
-            int m = (l + r) /2;
-            int cnt = 0;
-            for(int num : nums) {
-                if(num >= l && num <= m) {
-                    cnt++;
-                }
-            }
-            if(cnt > m - l + 1) r = m;
-            else l = m + 1;
+        //find the intersection point of linkedlist
+        int slow = nums[0], fast = nums[0];
+         do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow != fast);
+        slow = nums[0];
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return l;
+        return slow;
     }
 }
