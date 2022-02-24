ebeba6a516c21cbe560eb5796171ac216260061c
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-02-23 21:38:13 -0700

    Time: 8 ms (79.74%), Space: 55.1 MB (41.74%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -10,21 +10,46 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-      //extra space -> stack
+      //iterative sort
         if(head == null || head.next == null) return head;
-        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
-        ListNode curr = head;
-        while(curr != null) {
-            pq.offer(curr);
-            curr = curr.next;
+        int n = 0;
+        for(ListNode p = head; p != null; p=p.next) n++;
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        for(int i=1; i < n; i *= 2) {
+             ListNode curr = dummy;
+            for(int j = 0; j + i < n; j += 2 * i) {
+                ListNode first= curr.next, second = curr.next;
+                for(int k = 0; k < i; k++) second = second.next;
+                int l = 0, r = 0;
+                while(l < i && r < i && second != null) {
+                    if(first.val <= second.val) {
+                        curr.next = first;
+                        first = first.next;
+                        curr = curr.next;
+                        l++;
+                    } else {
+                        curr.next =second;
+                        second = second.next;
+                        curr =curr.next;
+                        r++;
+                    }
+                }
+                while(l < i) {
+                    curr.next =first;
+                    first = first.next;
+                    curr =curr.next;
+                    l++;
+                }
+                while(r < i && second != null) {
+                    curr.next = second;
+                    second = second.next;
+                    curr= curr.next;
+                    r++;
+                }
+                curr.next = second;
+            }
         }
-        head =  pq.poll();
-        curr = head;
-        while(!pq.isEmpty()) {
-            curr.next = pq.poll();
-            curr = curr.next;
-        }
-        curr.next = null;
-        return head;
+        return dummy.next;
     }
 }
