6527f2723052765e6d157f3a37109f8c9d3aead6
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-02-23 22:50:38 -0700

    Time: 11 ms (48.69%), Space: 54.5 MB (45.54%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -10,44 +10,44 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-      //iterative sort
         if(head == null || head.next == null) return head;
-        int n = 0;
-        for(ListNode p = head; p != null; p=p.next) n++;
         ListNode dummy = new ListNode();
         dummy.next = head;
-        for(int i=1; i < n; i *= 2) {
-             ListNode curr = dummy;
+        int n = 0;
+        for(ListNode p = head; p != null; p = p.next) n++;
+        for(int i = 1; i < n; i *= 2) {
+            ListNode curr = dummy;
             for(int j = 0; j + i < n; j += 2 * i) {
-                ListNode first= curr.next, second = curr.next;
-                for(int k = 0; k < i; k++) second = second.next;
-                int l = 0, r = 0;
-                while(l < i && r < i && second != null) {
+                ListNode first = curr.next, second= curr.next;
+                for(int k = 0; k < i; k++) second= second.next;
+                //merger two list 
+                int f = 0, s = 0;
+                while(f < i && s < i && second != null) {
                     if(first.val <= second.val) {
                         curr.next = first;
                         first = first.next;
                         curr = curr.next;
-                        l++;
+                        f++;
                     } else {
-                        curr.next =second;
+                        curr.next = second;
                         second = second.next;
-                        curr =curr.next;
-                        r++;
+                        curr = curr.next;
+                        s++;
                     }
                 }
-                while(l < i) {
-                    curr.next =first;
-                    first = first.next;
-                    curr =curr.next;
-                    l++;
-                }
-                while(r < i && second != null) {
-                    curr.next = second;
-                    second = second.next;
-                    curr= curr.next;
-                    r++;
-                }
-                curr.next = second;
+                    while(f < i ) {
+                        curr.next = first;
+                        first = first.next;
+                        curr = curr.next;
+                        f++;
+                    }
+                    while(s < i && second != null) {
+                        curr.next = second;
+                        second = second.next;
+                        curr = curr.next;
+                        s++;
+                    }
+                    curr.next =second;
             }
         }
         return dummy.next;
