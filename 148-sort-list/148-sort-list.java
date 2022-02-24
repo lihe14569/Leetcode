a3acddb4434930148d69fb0ffcd552555b2f6f40
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-02-23 23:03:23 -0700

    Time: 10 ms (59.15%), Space: 57.3 MB (19.39%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -11,45 +11,50 @@
 class Solution {
     public ListNode sortList(ListNode head) {
         if(head == null || head.next == null) return head;
+        ListNode mid = split(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        return merge(left, right);
+    }
+    
+    public ListNode split(ListNode head) {
+        //split list into half at mid point using two pointer method
+        ListNode slow = head, fast = head.next;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode mid = slow.next;
+        slow.next = null;
+        return mid;
+    }
+    
+    public ListNode merge(ListNode left, ListNode right) {
+        //merge two sorted list and return the merged head
         ListNode dummy = new ListNode();
-        dummy.next = head;
-        int n = 0;
-        for(ListNode p = head; p != null; p = p.next) n++;
-        for(int i = 1; i < n; i *= 2) {
-            ListNode curr = dummy;
-            for(int j = 0; j + i < n; j += 2 * i) {
-                ListNode first = curr.next, second= curr.next;
-                for(int k = 0; k < i; k++) second= second.next;
-                //merger two list 
-                int f = 0, s = 0;
-                while(f < i && s < i && second != null) {
-                    if(first.val <= second.val) {
-                        curr.next = first;
-                        first = first.next;
-                        curr = curr.next;
-                        f++;
-                    } else {
-                        curr.next = second;
-                        second = second.next;
-                        curr = curr.next;
-                        s++;
-                    }
-                }
-                    while(f < i ) {
-                        curr.next = first;
-                        first = first.next;
-                        curr = curr.next;
-                        f++;
-                    }
-                    while(s < i && second != null) {
-                        curr.next = second;
-                        second = second.next;
-                        curr = curr.next;
-                        s++;
-                    }
-                    curr.next =second;
+        ListNode curr = dummy;
+        while(left != null && right != null) {
+            if(left.val <= right.val) {
+                curr.next = left;
+                left = left.next;
+                curr = curr.next;
+            } else {
+                curr.next = right;
+                right = right.next;
+                curr =curr.next;
             }
         }
+        while(left != null) {
+            curr.next = left;
+            left = left.next;
+            curr = curr.next;
+        }
+        while(right != null) {
+            curr.next = right;
+            right = right.next;
+            curr =curr.next;
+        }
+        curr.next = null;
         return dummy.next;
     }
 }
