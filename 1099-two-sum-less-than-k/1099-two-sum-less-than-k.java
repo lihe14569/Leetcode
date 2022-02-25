034868ff08e414ba3095b716299f1808f270892e
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-02-24 21:11:19 -0700

    Time: 1 ms (100.00%), Space: 40.8 MB (40.80%) - LeetHub

--- 1099-two-sum-less-than-k/1099-two-sum-less-than-k.java
+++ 1099-two-sum-less-than-k/1099-two-sum-less-than-k.java
@@ -1,17 +1,17 @@
 class Solution {
     public int twoSumLessThanK(int[] nums, int k) {
-        int answer = -1;
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length; ++i) {
-            int idx = Arrays.binarySearch(nums, i + 1, nums.length, k - nums[i] - 1);
-            int j = (idx >= 0 ? idx : ~idx);
-            if (j == nums.length || nums[j] > k - nums[i] - 1) {
-                j--;
-            }
-            if (j > i) {
-                answer = Math.max(answer, nums[i] + nums[j]);
+        int l = 0, r = nums.length - 1;
+        int max = -1;
+        while(l < r) {
+            int sum = nums[l] + nums[r];
+            if(sum < k) {
+                max = Math.max(max, sum);
+                l++;
+            } else {
+                r--;
             }
         }
-        return answer;
+        return max;
     }
 }
