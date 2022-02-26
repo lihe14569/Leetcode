5bad3c4854557f309bd7c69cdf1ca060995a4b55
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-02-26 13:06:54 -0700

    Time: 6 ms (78.37%), Space: 43.3 MB (40.73%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -1,48 +1,43 @@
 class Solution {
     public String reverseWords(String s) {
-        //revers whole + reverse each
-        //trim white spaces
-        StringBuilder sb = trimWhiteSpace(s);
-    
-        sb = reverse(sb);
+        //reverse whoe + reverse each
+        StringBuilder sb = trimSpaces(s);
+        reverse(sb, 0, sb.length() - 1);
         reverseEach(sb);
         return sb.toString();
     }
-    public StringBuilder trimWhiteSpace(String s) {
-        StringBuilder sb= new StringBuilder();
+    
+    public StringBuilder trimSpaces(String s) {
+        StringBuilder sb = new StringBuilder();
         int l = 0, r = s.length() - 1;
-        while(l < r && s.charAt(l) == ' ') l++;
-        while(l < r && s.charAt(r) == ' ') r--;
+        while(l <= r && s.charAt(l) == ' ') l++;
+        while(l <= r && s.charAt(r) == ' ') r--;
+        
         while(l <= r) {
-            int i = l;
-            while(i <= r && s.charAt(i) != ' ') i++;
-            sb.append(s.substring(l, i));
-            if(sb.charAt(sb.length() - 1) != ' ') sb.append(' ');
-            while(i <= r && s.charAt(i) == ' ') i++;
-            l = i;
+            char c = s.charAt(l);
+            if(c != ' ') sb.append(c);
+            else if(sb.charAt(sb.length() - 1) != ' ') sb.append(' ');
+            l++;
         }
-        sb.deleteCharAt(sb.length() -1);
         return sb;
     }
-    public StringBuilder reverse(StringBuilder sb) {
-        return sb.reverse();
-    }
-    public void reverseEach(StringBuilder sb) {
-        int l = 0, r = 0;
-        int n = sb.length();
-        while(l < n) {
-            while(r < n && sb.charAt(r) != ' ') r++;
-            reverseStr(sb, l, r - 1);
-            l = r + 1;
-            r++;
-        }
-    }
-    public void reverseStr(StringBuilder sb, int l, int r) {
-        while(l < r) {
-            char temp = sb.charAt(l);
-            sb.setCharAt(l++,sb.charAt(r));
-            sb.setCharAt(r--, temp);
+    //reverse the whole string
+    public void reverse(StringBuilder sb, int start, int end) {
+        while(start < end) {
+            char c = sb.charAt(start);
+            sb.setCharAt(start++, sb.charAt(end));
+            sb.setCharAt(end--, c);
         }
     }
     
+    public void reverseEach(StringBuilder sb) {
+        int start = 0, end = 0;
+        int n = sb.length();
+        while(start < n) {
+            while(end < n && sb.charAt(end) != ' ') end++;
+            reverse(sb, start, end - 1);
+            start = end + 1;
+            end = start;
+        }
+    }
 }
