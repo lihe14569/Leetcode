2cc8f50811a79d15990f8e9ef571e007dce163e7
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-02-26 15:05:27 -0700

    Time: 1 ms (99.45%), Space: 44.7 MB (23.95%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,23 +1,43 @@
+import java.util.Random;
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
 
-        int l = -10000, r = 10000;
-        while(l < r) {
-            int m =(l + r + 1) / 2;
-            int count = ge(nums, m);
-            if(count >= k) {
-                l = m;
-            } else {
-                r = m - 1;
+    public int findKthLargest(int[] nums, int k) {
+        //quickselect
+        int n = nums.length;
+        k = n - k;
+        return quickselect(nums, 0, n - 1, k);
+    }
+    
+    //quickselect kth smallest elemtn
+    public int quickselect(int[] nums, int l, int r, int k) {
+        //base case
+        if(l == r) return nums[l];
+        Random rand = new Random();
+        int pivotIdx = l + rand.nextInt(r - l);
+        pivotIdx = partition(nums, l, r, pivotIdx);
+        if(k == pivotIdx) return nums[k];
+        else if(k < pivotIdx) return quickselect(nums, l, pivotIdx - 1, k);
+        else return quickselect(nums, pivotIdx + 1, r, k);
+    }
+    
+    //find the pivotindex
+    public int partition(int[] nums, int l, int r, int pivotIdx) {
+        int pivot = nums[pivotIdx];
+        swap(nums, pivotIdx, r);
+        int wall = l;
+        for(int i = l; i <= r; i++) {
+            if(nums[i] < pivot) {
+                swap(nums, wall, i);
+                wall++;
             }
         }
-        return l;
+        swap(nums, wall, r);
+        return wall;
     }
-    public int ge(int[] nums, int m) {
-        int res = 0;
-        for(int num : nums) {
-            if(num >= m) res++;
-        }
-        return res;
+    //swap two numbers based on indices
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
