618cf7eefff1f5b0610af43af27f6ec50ad688b6
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-02-26 12:45:49 -0700

    Time: 4 ms (92.27%), Space: 42 MB (55.83%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -1,16 +1,48 @@
 class Solution {
     public String reverseWords(String s) {
-        int l = 0, r=  s.length() - 1;
+        //revers whole + reverse each
+        //trim white spaces
+        StringBuilder sb = trimWhiteSpace(s);
+    
+        sb = reverse(sb);
+        reverseEach(sb);
+        return sb.toString();
+    }
+    public StringBuilder trimWhiteSpace(String s) {
+        StringBuilder sb= new StringBuilder();
+        int l = 0, r = s.length() - 1;
         while(l < r && s.charAt(l) == ' ') l++;
         while(l < r && s.charAt(r) == ' ') r--;
-        List<String> list = new ArrayList<>();
         while(l <= r) {
             int i = l;
             while(i <= r && s.charAt(i) != ' ') i++;
-            if(i != l) list.add(s.substring(l, i));
-            l = i + 1;
+            sb.append(s.substring(l, i));
+            if(sb.charAt(sb.length() - 1) != ' ') sb.append(' ');
+            while(i <= r && s.charAt(i) == ' ') i++;
+            l = i;
         }
-        Collections.reverse(list);
-        return String.join(" ", list);
+        sb.deleteCharAt(sb.length() -1);
+        return sb;
     }
+    public StringBuilder reverse(StringBuilder sb) {
+        return sb.reverse();
+    }
+    public void reverseEach(StringBuilder sb) {
+        int l = 0, r = 0;
+        int n = sb.length();
+        while(l < n) {
+            while(r < n && sb.charAt(r) != ' ') r++;
+            reverseStr(sb, l, r - 1);
+            l = r + 1;
+            r++;
+        }
+    }
+    public void reverseStr(StringBuilder sb, int l, int r) {
+        while(l < r) {
+            char temp = sb.charAt(l);
+            sb.setCharAt(l++,sb.charAt(r));
+            sb.setCharAt(r--, temp);
+        }
+    }
+    
 }
