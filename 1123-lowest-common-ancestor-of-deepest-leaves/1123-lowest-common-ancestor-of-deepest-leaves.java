41a867b6a75f67d7efcc9a334d22271ec26c95e5
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-02-27 15:45:01 -0700

    Time: 1 ms (76.28%), Space: 45 MB (15.30%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.java
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.java
@@ -14,20 +14,26 @@
  * }
  */
 class Solution {
+    class Pair{
+        TreeNode node;
+        int d;
+        public Pair(TreeNode node, int d) {
+            this.node = node;
+            this.d = d;
+        }
+    }
     public TreeNode lcaDeepestLeaves(TreeNode root) {
-        //O(n^2)
-        if(root == null) return null;
-        int l = height(root.left);
-        int r = height(root.right);
-        if(l == r) return root;
-        else if(l > r) return lcaDeepestLeaves(root.left);
-        else return lcaDeepestLeaves(root.right);
+        Pair p = LCA(root, 0);
+        return p.node;
     }
-    //get the height of current node
-    public int height(TreeNode root) {
-        if(root == null) return 0;
-        return 1 + Math.max(height(root.left), height(root.right));
+    public Pair LCA(TreeNode root, int d) {
+        if(root == null) return new Pair(null, d);
+        Pair l = LCA(root.left, d + 1);
+        Pair r = LCA(root.right, d + 1);
+        if(l.d == r.d) {
+            return new Pair(root, l.d);
+        } else {
+            return l.d < r.d ? r : l;
+        }
     }
-    
-     
 }
