ac379226a1b16e2ccc0bf03997ef194d2ba5738b
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-03-04 01:08:28 -0700

    Time: 1 ms (43.17%), Space: 45.8 MB (17.04%) - LeetHub

---
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
@@ -0,0 +1,32 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    Map<Node, Node> visited = new HashMap<>();
+    
+    public Node copyRandomList(Node head) {
+        //method2: hashtable 
+        if(head == null) return head;
+        if(visited.containsKey(head))
+            return visited.get(head);
+        else {
+            Node newNode = new Node(head.val);
+            visited.put(head, newNode);
+            newNode.next = copyRandomList(head.next);
+            newNode.random = copyRandomList(head.random);
+        }
+        return visited.get(head);
+    }
+}
