45eda1eea151fa5817a9c225eb82347b3ef109d0
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-03-07 22:16:32 -0700

    Time: 0 ms (100.00%), Space: 42.7 MB (7.04%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,14 +1,15 @@
 class Solution {
+    Integer[] memo;
     public int rob(int[] nums) {
-        int[] dp = new int[nums.length];
-        if(nums.length == 1) return nums[0];
-        if(nums.length == 2) return Math.max(nums[0], nums[1]);
-        dp[0] = nums[0];
-        dp[1] = Math.max(dp[0], nums[1]);
-        
-        for(int i = 2; i < nums.length; i++) {
-            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
-        }
-        return dp[nums.length - 1];
+        //top down recursion
+        int n = nums.length;
+        memo = new Integer[n];
+        return getVal(nums, n - 1);
+    }
+    public int getVal(int[] nums, int len) {
+        if(len < 0) return 0;
+        if(memo[len] != null) return memo[len];
+        int res = Math.max(getVal(nums, len - 2) + nums[len], getVal(nums, len - 1));
+        return memo[len] = res;
     }
 }
