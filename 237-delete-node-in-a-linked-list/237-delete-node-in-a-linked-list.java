92f1a85fe71f3ecc94eb55184ae6dedab4d7521a
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-03-10 21:08:41 -0700

    Time: 0 ms (100.00%), Space: 41.6 MB (64.63%) - LeetHub

--- 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.java
+++ 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.java
@@ -8,7 +8,7 @@
  */
 class Solution {
     public void deleteNode(ListNode node) {
-        //思路：将下一个点value赋给要删除点，然后删除一下个节点
+        if(node.next == null) node = null;
         node.val = node.next.val;
         node.next = node.next.next;
     }
