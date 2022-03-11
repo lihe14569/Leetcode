adc34f9afc3ff52d8308be57f7d00d69eabf2e7d
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-03-10 21:16:52 -0700

    Time: 1 ms (71.85%), Space: 45.1 MB (6.84%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.java
@@ -9,17 +9,13 @@
  * }
  */
 class Solution {
-    //思路：condition1:下一个节点的值和当前值一样，删除一下个节点
-    //     condition2:下一个节点的值和当前值不一样，当前节点移动到下一节点
     public ListNode deleteDuplicates(ListNode head) {
+        if(head == null || head.next == null) return head;
         ListNode curr = head;
-        //判断当前节点是否为空为结束条件 ***重要
-        
         while(curr != null) {
-            if(curr.next != null && curr.val == curr.next.val)
+            while(curr.next != null && curr.val == curr.next.val)
                 curr.next = curr.next.next;
-            else
-                curr = curr.next;
+            curr = curr.next;
         }
         return head;
     }
