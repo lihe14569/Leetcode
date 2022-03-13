19b7eca60ba1c7056c136f095384db5e35cdb128
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-03-13 00:30:34 -0700

    Time: 2 ms (81.09%), Space: 42.2 MB (23.99%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean isValid(String s) {
+        int len = s.length();
+        if(len % 2 == 1) return false;
         Stack<Character> stack = new Stack<>();
         for(char c : s.toCharArray()) {
             if(c == '(') stack.push(')');
@@ -7,7 +9,7 @@ class Solution {
             else if(c == '{') stack.push('}');
             else if(stack.isEmpty() || c != stack.pop())
                 return false;
-         }
+        }
         return stack.isEmpty();
     }
 }
