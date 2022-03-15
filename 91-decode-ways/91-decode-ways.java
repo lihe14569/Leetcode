8f4f4f84f7f74c536bd6395bbdfb34f4a31358a4
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-03-15 01:19:04 -0600

    Time: 26 ms (5.19%), Space: 41.8 MB (60.70%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,16 +1,18 @@
 class Solution {
     public int numDecodings(String s) {
-        int[] dp = new int[s.length() + 1];
+        int[] memo = new int[s.length() + 1];
+        return helper(s, memo, 0);
+    }
+    public int helper(String s, int[] memo, int i) {
         //base case
-        dp[0] = 1;
-        dp[1] = s.charAt(0) == '0' ? 0 : 1;
-        
-        for(int i = 2; i <= s.length(); i++) {
-            int two = Integer.valueOf(s.substring(i - 2, i));
-            int one = Integer.valueOf(s.substring(i - 1, i));
-            if(two >= 10 && two <= 26) dp[i] += dp[i - 2];
-            if(one != 0) dp[i] += dp[i - 1];
-        }
-        return dp[s.length()];
+        if(i == s.length()) return 1; //代表recursion tree里面的一个valid的结果
+        if(memo[i] != 0) return memo[i];
+        if(s.charAt(i) == '0') return 0;
+        if(isvalidTwo(s, i)) memo[i] = helper(s, memo, i + 1) + helper(s, memo, i + 2);
+        else memo[i] = helper(s, memo, i + 1);
+        return memo[i];
+    }
+    public boolean isvalidTwo(String s, int i) {
+        return i + 1 < s.length() && (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i + 1) < '7'));
     }
 }
