5e517f5433ff8e454ff951013f0cef486c5974a1
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-03-20 17:14:57 -0600

    Time: 4 ms (68.35%), Space: 40 MB (89.12%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -4,14 +4,17 @@ class Solution {
         if(m + n != s3.length()) return false;
         boolean[][] dp = new boolean[m + 1][n + 1];
         dp[0][0] = true;
-        for(int i = 1; i <= m; i++)
-            dp[i][0] = s1.charAt(i - 1) == s3.charAt(i - 1) && dp[i - 1][0];
-        for(int j = 1; j <= n; j++)
-            dp[0][j] = s2.charAt(j - 1) == s3.charAt(j - 1) && dp[0][j  - 1];
+        for(int i = 1; i <= m; i++) {
+            dp[i][0] = s3.charAt(i - 1) == s1.charAt(i - 1) && dp[i - 1][0]; 
+        }
+        for(int i = 1; i <= n; i++) {
+            dp[0][i] = s3.charAt(i - 1) == s2.charAt(i - 1) && dp[0][i - 1]; 
+        }
+        
         for(int i = 1; i <= m; i++) {
             for(int j = 1; j <= n; j++) {
-                dp[i][j] = (s3.charAt(i + j - 1) == s1.charAt(i - 1) && dp[i - 1][j])||
-                    (s3.charAt(i + j - 1) == s2.charAt(j - 1) && dp[i][j - 1]);
+                dp[i][j] = (s1.charAt(i - 1) == s3.charAt(i + j - 1) && dp[i - 1][j]) || 
+                    (s2.charAt(j - 1) == s3.charAt(i + j - 1) && dp[i][j - 1]);
             }
         }
         return dp[m][n];
