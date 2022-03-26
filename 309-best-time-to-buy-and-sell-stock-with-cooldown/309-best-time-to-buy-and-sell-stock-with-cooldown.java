3080980aade6fc3f9c46c36b50b573bd2e8f4779
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-03-26 12:58:30 -0600

    Time: 2 ms (43.42%), Space: 42.8 MB (16.88%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -12,7 +12,7 @@ class Solution {
         dp[1][1] = Math.max(0, prices[1] + dp[0][0]);
         for(int i = 2; i < n; i++) {
             dp[i][0] = Math.max(dp[i - 1][0], dp[i - 2][1] -prices[i]);
-            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i][0] + prices[i]);
         }
         return dp[n - 1][1];
     }
