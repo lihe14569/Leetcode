4a7a442756e8c0828668079b29d80983467d65b2
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-03-26 13:21:55 -0600

    Time: 10 ms (59.27%), Space: 46.6 MB (51.57%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,16 +1,20 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
+        //top-down 
+        Set<String> dict = new HashSet<>(wordDict);
         Boolean[] memo = new Boolean[s.length()];
-        Set<String> words = new HashSet<>(wordDict);
-        return helper(s, words, memo, 0);
+        return dp(s, dict, 0, memo);
     }
-    public boolean helper(String s, Set<String> words, Boolean[] memo, int start) {
-        //base 
-        if(start == s.length()) return true;
+    public boolean dp(String s, Set<String> dict, int start, Boolean[] memo) {
+        //base case
+        if(start >= s.length()) return true;
+        //check if we already vsited here
         if(memo[start] != null) return memo[start];
-        for(int end = start + 1; end <= s.length(); end++) {
-            if(words.contains(s.substring(start, end)) && helper(s, words,memo, end))
+        for(int i = start; i < s.length(); i++) {
+            String part = s.substring(start, i + 1);
+            if(dict.contains(part) && dp(s, dict, i + 1, memo)) {
                 return memo[start] = true;
+            }
         }
         return memo[start] = false;
     }
