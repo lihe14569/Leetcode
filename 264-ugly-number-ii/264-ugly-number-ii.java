c1f632bf14fabbed5620cc4f42af2ec94f4c054a
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-03-28 22:57:13 -0600

    Time: 2 ms (96.63%), Space: 40.2 MB (97.87%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.java
+++ 264-ugly-number-ii/264-ugly-number-ii.java
@@ -1,20 +1,16 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        int ugly = 0;
-        PriorityQueue<Long> pq = new PriorityQueue<>();
-        Set<Long> set = new HashSet<>();
-        pq.offer(1L);
-        set.add(1L);
-        int[] primes = new int[]{2, 3, 5};
-        for(int i = 0; i < n; i++) {
-            long num = pq.poll();
-            ugly = (int) num;
-            for(int prime : primes) {
-                long newUgly = prime * num;
-                if(set.add(newUgly))
-                    pq.offer(newUgly);
-            }
+        //1, 2, 3, 4, 5, 6, 8...
+        int[] dp = new int[n];
+        int p2= 0, p3 = 0, p5 = 0;
+        dp[0] = 1;
+        
+        for(int i = 1; i < n; i++) {
+            dp[i] = Math.min(dp[p2] * 2, Math.min(dp[p3] * 3, dp[p5] * 5));
+            if(dp[i] == dp[p2] * 2) p2++;
+            if(dp[i] == dp[p3] * 3) p3++;
+            if(dp[i] == dp[p5] * 5) p5++;
         }
-        return ugly;
+        return dp[n - 1];
     }
 }
