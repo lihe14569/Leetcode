a8d37e84a47f051aa5ae519acfc4d156b4218493
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-03-30 13:26:52 -0600

    Time: 1 ms (100.00%), Space: 56.8 MB (83.30%) - LeetHub

---
+++ 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.java
@@ -0,0 +1,17 @@
+class SparseVector {
+
+    private int[] array;
+
+    SparseVector(int[] nums) {
+        array = nums;
+    }
+
+    public int dotProduct(SparseVector vec) {
+        int result = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            result += array[i] * vec.array[i];
+        }
+        return result;
+    }   
+}
