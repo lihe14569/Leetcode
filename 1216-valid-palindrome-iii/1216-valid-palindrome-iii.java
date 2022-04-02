0b3b2f41cd4b2642a33495d884ddd54742ceafc7
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-04-02 01:40:14 -0600

    Time: 22 ms (44.26%), Space: 49.4 MB (53.14%) - LeetHub

--- 1216-valid-palindrome-iii/1216-valid-palindrome-iii.java
+++ 1216-valid-palindrome-iii/1216-valid-palindrome-iii.java
@@ -1,20 +1,16 @@
 class Solution {
-    Integer[][] memo;
     public boolean isValidPalindrome(String s, int k) {
-        memo = new Integer[s.length()][s.length()];
-        return isValid(s, 0, s.length() - 1) <= k;
-    }
-    public int isValid(String s, int l, int r) {
-        //base case
-        if(l == r) return 0;
-        if(l == r - 1) return s.charAt(l) == s.charAt(r) ? 0 : 1;
-        //check memo
-        if(memo[l][r] != null) return memo[l][r];
-        //case1 : l 和r指的字母相同
-        if(s.charAt(l) == s.charAt(r)) return memo[l][r] = isValid(s, l + 1, r - 1);
-        //case2 : l 和r指的字母不同
-        else {
-            return memo[l][r] = 1 + Math.min(isValid(s, l + 1, r), isValid(s, l, r - 1));
+        //dp
+        int[][] dp = new int[s.length()][s.length()];
+        
+        for(int i= s.length() - 2; i >= 0; i--) {
+            for(int j = i + 1; j < s.length(); j++) {
+                if(s.charAt(i) == s.charAt(j)) dp[i][j] = dp[i + 1][j - 1];
+                else {
+                    dp[i][j] = 1 + Math.min(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
         }
+        return dp[0][s.length() - 1] <= k;
     }
 }
