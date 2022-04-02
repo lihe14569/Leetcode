11baafe7b96a4ed3e051757e02a461ff8e30c822
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-04-02 00:03:36 -0600

    Time: 12 ms (36.76%), Space: 54.7 MB (35.26%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -1,23 +1,22 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        //two pointer
-        int i = 0, j = s.length() - 1;
-        while(i < j && s.charAt(i) == s.charAt(j)) {
-            i++;
-            j--;
-        }
-        if(i >= j) return true;
-        if(isPanlindrome(s, i + 1, j) || isPanlindrome(s, i, j - 1)) return true;
-        else return false;
-    }
-    public boolean isPanlindrome(String s, int i, int j) {
-        while(i < j) {
-            if(s.charAt(i) == s.charAt(j)) {
-                i++;
-                j--;
+        int l = 0, r = s.length() - 1;
+        while(l < r) {
+            if(s.charAt(l) != s.charAt(r)) {
+                return isValid(s, l + 1, r) || isValid(s, l, r - 1);
             }
-            else return false;
+            l++;
+            r--;
         }
         return true;
     }
+    public boolean isValid(String s, int l , int r) {
+        while(l < r) {
+            if(s.charAt(l) != s.charAt(r)) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+  
 }
