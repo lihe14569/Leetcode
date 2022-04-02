d82aab1fea62ab32d9b78231aa98dfa1a613e57d
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-04-02 01:49:40 -0600

    Time: 0 ms (100.00%), Space: 39.9 MB (84.39%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -1,21 +1,19 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length == 0) return 0;
-        if(nums.length == 1) return nums[0];
-        int length = nums.length;
-        int ans = Math.max(robsub(nums, 0, length -1), robsub(nums, 1, length));
-        return ans;
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        
+        return Math.max(rob(nums, 0, n - 2), rob(nums, 1, n - 1));
     }
-    
-    private int robsub(int[] nums, int start, int end){
-        int pre1 = 0;
-        int pre2 = 0;
-        int curr = 0;
-        for(int i = start; i < end; i++){
-            curr = Math.max(pre2 + nums[i], pre1);
-            pre2 = pre1;
-            pre1 = curr;
+    public int rob(int[] nums, int start, int end) {
+        int n = end - start + 1;
+        if(n == 1) return nums[start];
+        int[] dp = new int[end - start + 1];
+        dp[0] = nums[start];
+        dp[1] = Math.max(nums[start], nums[start + 1]);
+        for(int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[start + i]);
         }
-        return curr;
+        return dp[n - 1];
     }
 }
