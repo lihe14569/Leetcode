cc0b22e1f0ac257a973a95584ebdc6c33039c7f7
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-04-02 02:15:08 -0600

    Time: 8 ms (38.23%), Space: 49.3 MB (5.88%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -1,13 +1,17 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
+        //robber + bucket
         int[] bucket = new int[10001];
-        for(int num : nums) bucket[num] += num;
-        int[] dp = new int[10002];
+        for(int num : nums) bucket[num]++;
+        
+        //rob game
+        int[] dp = new int[10001];
         dp[0] = 0;
-        dp[1] = bucket[0];
-        for(int i = 2; i <= 10001; i++) {
-            dp[i] = Math.max(dp[i - 1], dp[i - 2] + bucket[i - 1]);
+        dp[1] = bucket[1];
+        
+        for(int i = 2; i <= 10000; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + i*bucket[i]);
         }
-        return dp[10001];
+        return dp[10000];
     }
 }
