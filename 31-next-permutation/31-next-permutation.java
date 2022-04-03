6cff9efc3a17c4a263c551e8c95839c55f3298cb
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-04-02 20:10:46 -0600

    Time: 1 ms (85.00%), Space: 44.4 MB (6.35%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,20 +1,21 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int i = nums.length - 2;
+        int n = nums.length;
+        int i = n - 2;
         while(i >= 0 && nums[i] >= nums[i + 1]) i--;
-        if(i >= 0) {
-            int j = nums.length - 1;
-            while(nums[j] <= nums[i]) j--;
-            swap(nums, i , j);
+        if(i < 0) reverse(nums, 0);
+        else {
+            int j = n - 1;
+            while(j > i && nums[j] <= nums[i]) j--;
+            swap(nums, i, j);
+            reverse(nums, i + 1);
         }
-        reverse(nums,i + 1);
     }
     public void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
-    
     public void reverse(int[] nums, int i) {
         int j = nums.length - 1;
         while(i < j) {
