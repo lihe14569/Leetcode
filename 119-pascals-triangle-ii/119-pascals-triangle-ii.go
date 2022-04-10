6e3427ef70699c847c8c820aea4ea9cf1e17cb76
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-04-10 15:24:30 -0600

    Time: 2 ms (40.33%), Space: 2 MB (40.06%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.go
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.go
@@ -1,16 +1,15 @@
 func getRow(rowIndex int) []int {
-    res := make([][]int, rowIndex + 1)
-    
+    prev := make([]int, rowIndex + 1)
     for i := 0; i<= rowIndex; i++ {
         row := make([]int, i + 1)
         for j := 0; j <= i; j++ {
             if j == 0 || j == i {
                 row[j] = 1
             } else {
-                row[j] = res[i -1][j - 1] + res[i - 1][j]
+                row[j] = prev[j - 1] + prev[j]
             }
         }
-        res[i] = row
+        prev = row
     }
-    return res[len(res) - 1]
+    return prev
 }
