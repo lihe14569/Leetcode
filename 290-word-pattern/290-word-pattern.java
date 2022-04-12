5e2f76f68fdcd278d89ccb751a748f025ca0bed6
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-04-11 22:27:53 -0600

    Time: 2 ms (29.01%), Space: 41.7 MB (71.09%) - LeetHub

--- 290-word-pattern/290-word-pattern.java
+++ 290-word-pattern/290-word-pattern.java
@@ -1,27 +1,46 @@
 class Solution {
-    public boolean wordPattern(String pattern, String s) {
-        Map<Character, String> pmap = new HashMap<>();
-        Map<String, Character> smap = new HashMap<>();
+    int live = 3;
+    int dead = 2;
+    public void gameOfLife(int[][] board) {
+        if(board == null || board[0].length == 0) return;
         
-        String[] sArr = s.split(" ");
-        if(pattern.length() != sArr.length) return false;
-        for(int i = 0; i < pattern.length(); i++) {
-            char c = pattern.charAt(i);
-            String str = sArr[i];
-            if(!pmap.containsKey(c)) {
-                if(smap.containsKey(str)) return false;
-                else {
-                    pmap.put(c, str);
-                    smap.put(str, c);
+        int m = board.length, n = board[0].length;
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                int around = findLive(board, i, j);
+                if(board[i][j] == 0 && around == 3) {
+                    board[i][j] = live;
                 }
-            } else {
-                if(!smap.containsKey(str)) return false;
-                else {
-                    if(!pmap.get(c).equals(str) || smap.get(str) != c)
-                        return false;
+                else if(board[i][j] == 1) {
+                    if(around >=2 && around <= 3)
+                        continue;
+                    else
+                        board[i][j] = dead;
                 }
             }
         }
-        return true;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(board[i][j] == live)
+                    board[i][j] = 1;
+                else if(board[i][j] == dead)
+                    board[i][j] = 0;
+            }
+        }
+    }
+    private int findLive(int[][] board, int i, int j) {
+        int[][] dirs = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+        int cnt = 0;
+        for(int[] dir : dirs) {
+            int x = i + dir[0];
+            int y = j + dir[1];
+            if(x >= 0 && x < board.length && y >= 0 && y < board[0].length) {
+                if(board[x][y] == 1 || board[x][y] == dead) {
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
     }
 }
