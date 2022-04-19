4e262c76d16a156a8867873c1d5d4a5efdeec13a
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-04-19 00:58:33 -0600

    Time: 5 ms (23.11%), Space: 47 MB (67.61%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.java
@@ -14,28 +14,30 @@
  * }
  */
 class Solution {
-    TreeNode prev = new TreeNode(Integer.MIN_VALUE);
-    TreeNode first, second;
     public void recoverTree(TreeNode root) {
-        inorder(root);
+        if(root == null) return;
+        TreeNode prev = new TreeNode(Integer.MIN_VALUE);
+        TreeNode first= null, second = null;
+        Stack<TreeNode> stack = new Stack<>();
+        while(!stack.isEmpty() || root != null) {
+            while(root != null) {
+                stack.push(root);
+                root= root.left;
+            }
+            root = stack.pop();
+            if(root.val < prev.val) {
+                if(first == null) {
+                    first = prev;
+                    second = root;
+                } else second = root;
+            }
+            prev = root;
+            root = root.right;
+        }
         if(first != null && second != null) {
             int temp = first.val;
             first.val = second.val;
             second.val = temp;
         }
-    }
-    public void inorder(TreeNode root) {
-        if(root == null) return;
-        inorder(root.left);
-        if(root.val < prev.val) {
-            if(first == null) {
-                first = prev;
-                second = root;
-            } else {
-                second = root;
-            }
-        }
-        prev = root;
-        inorder(root.right);
     }
 }
