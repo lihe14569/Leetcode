adc9b533891c07f1f80f0b9df85b2ec4f73e36b9
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-04-20 12:29:44 -0600

    Time: 19 ms (68.03%), Space: 118.4 MB (57.15%) - LeetHub

--- 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.java
+++ 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.java
@@ -1,16 +1,17 @@
 class Solution {
     public List<Long> maximumEvenSplit(long finalSum) {
-        List<Long> list = new ArrayList<>();
-        if(finalSum % 2 != 0) return list;
-        long curr = 0;
-        for(long i = 2; i <= finalSum - curr; i+=2) {
-            curr += i;
-            list.add(i);
-            
-        }
-        
-        if(finalSum > curr) {
-            list.set(list.size() - 1, list.get(list.size() -1) + (finalSum - curr));
+        List<Long> list= new ArrayList<>();
+        if(finalSum % 2 == 0) {
+            long curr = 2;
+            while(curr <= finalSum) {
+                list.add(curr);
+                finalSum -= curr;
+                curr+=2;
+            }
+            if(finalSum > 0) {
+                int end = list.size() - 1;
+                list.set(end, list.get(end) +finalSum);
+            }
         }
         return list;
     }
