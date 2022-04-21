f81db7f942ebedc1bece6ee98f3eef9d513ce487
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-04-21 01:41:26 -0600

    Time: 0 ms (100.00%), Space: 41.4 MB (60.19%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -3,12 +3,12 @@ class Solution {
         int n = nums.length;
         if(n == 1) return nums[0];
         if(n == 2) return Math.max(nums[0], nums[1]);
-        int[] dp = new int[nums.length];
+        int[] dp = new int[n];
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
-        for(int i = 2; i < nums.length; i++) {
+        for(int i = 2; i < n; i++) {
             dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
         }
-        return dp[nums.length - 1];
+        return dp[n - 1];
     }
 }
