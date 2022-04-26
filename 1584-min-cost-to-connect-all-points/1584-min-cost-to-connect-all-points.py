2648eb1561b77b5c7c94eb59857570e3b8c08a76
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-04-26 01:25:48 -0600

    Time: 4268 ms (25.96%), Space: 128.1 MB (33.97%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,23 +1,20 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        #prims algo
-        distance = lambda p1,p2 : abs(p1[0]- p2[0]) + abs(p1[1] - p2[1])
-        n, dic = len(points), defaultdict(list)
-        #build edge relation
+        manhattan = lambda p1, p2: abs(p1[0]-p2[0]) + abs(p1[1]-p2[1])
+        n, c = len(points), collections.defaultdict(list)
         for i in range(n):
-            for j in range(1, n):
-                d = distance(points[i], points[j])
-                dic[i].append((d, j))
-                dic[j].append((d, i))
-        
-        res, cnt, visited, heap = 0, 1, [0] * n, dic[0]
+            for j in range(i+1, n):
+                d = manhattan(points[i], points[j])
+                c[i].append((d, j))
+                c[j].append((d, i))
+        cnt, ans, visited, heap = 1, 0, [0] * n, c[0]
         visited[0] = 1
         heapq.heapify(heap)
-        
         while heap:
-            d, i = heapq.heappop(heap)
-            if not visited[i]:
-                cnt, visited[i], res = cnt+1, 1, res + d
-                for node in dic[i]: heapq.heappush(heap, node)
-            if cnt >= n: break
-        return res
+            d, j = heapq.heappop(heap)
+            if not visited[j]:
+                visited[j], cnt, ans = 1, cnt+1, ans+d
+                for record in c[j]: heapq.heappush(heap, record)
+            if cnt >= n: break        
+        return ans
+        
