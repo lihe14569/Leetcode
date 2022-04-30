5e4c395a5045645dd25c80a0a52de96b6b749470
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-04-30 01:50:24 -0600

    Time: 29 ms (91.28%), Space: 13.9 MB (95.43%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.py
+++ 399-evaluate-division/399-evaluate-division.py
@@ -1,44 +1,34 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-
         graph = defaultdict(defaultdict)
-
-        def backtrack_evaluate(curr_node, target_node, acc_product, visited):
+        def dfs(curr_node, target_node, path, visited):
             visited.add(curr_node)
             ret = -1.0
-            neighbors = graph[curr_node]
-            if target_node in neighbors:
-                ret = acc_product * neighbors[target_node]
+            neighbours = graph[curr_node]
+            if target_node in neighbours:
+                ret = path * neighbours[target_node]
             else:
-                for neighbor, value in neighbors.items():
-                    if neighbor in visited:
-                        continue
-                    ret = backtrack_evaluate(
-                        neighbor, target_node, acc_product * value, visited)
+                for node, val in neighbours.items():
+                    if node in visited: continue
+                    ret = dfs(node, target_node, path * val, visited)
                     if ret != -1.0:
                         break
             visited.remove(curr_node)
             return ret
-
-        # Step 1). build the graph from the equations
-        for (dividend, divisor), value in zip(equations, values):
-            # add nodes and two edges into the graph
-            graph[dividend][divisor] = value
-            graph[divisor][dividend] = 1 / value
-
-        # Step 2). Evaluate each query via backtracking (DFS)
-        #  by verifying if there exists a path from dividend to divisor
-        results = []
-        for dividend, divisor in queries:
-            if dividend not in graph or divisor not in graph:
-                # case 1): either node does not exist
+        result = []
+        #build graph
+        for (division, divisor), value in zip(equations, values):
+            graph[division][divisor] = value
+            graph[divisor][division] = 1 / value
+        #check the path
+        for division, divisor in queries:
+            if division not in graph or divisor not in graph:
                 ret = -1.0
-            elif dividend == divisor:
-                # case 2): origin and destination are the same node
+            elif division == divisor:
                 ret = 1.0
             else:
                 visited = set()
-                ret = backtrack_evaluate(dividend, divisor, 1, visited)
-            results.append(ret)
-
-        return results
+                ret = dfs(division, divisor, 1, visited)
+            result.append(ret)
+        return result
+            
