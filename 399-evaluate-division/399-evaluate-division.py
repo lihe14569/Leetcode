c791dcd9d88ff3d65995190c0c1739932c5918e6
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-04-30 02:21:32 -0600

    Time: 36 ms (74.12%), Space: 14.1 MB (16.96%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.py
+++ 399-evaluate-division/399-evaluate-division.py
@@ -6,29 +6,27 @@ class Solution:
             ret = -1.0
             neighbours = graph[curr_node]
             if target_node in neighbours:
-                ret = path * neighbours[target_node]
+                return path * neighbours[target_node]
             else:
-                for node, val in neighbours.items():
-                    if node in visited: continue
-                    ret = dfs(node, target_node, path * val, visited)
+                for nei, val in neighbours.items():
+                    if nei in visited: continue
+                    ret = dfs(nei, target_node, path*val, visited)
                     if ret != -1.0:
                         break
             visited.remove(curr_node)
             return ret
-        result = []
-        #build graph
-        for (division, divisor), value in zip(equations, values):
-            graph[division][divisor] = value
-            graph[divisor][division] = 1 / value
-        #check the path
-        for division, divisor in queries:
-            if division not in graph or divisor not in graph:
+        result= []
+        ret = -1.0
+        for (div1, div2), val in zip(equations, values):
+            graph[div1][div2] = val
+            graph[div2][div1] = 1 / val
+        for div1, div2 in queries:
+            if div1 not in graph or div2 not in graph:
                 ret = -1.0
-            elif division == divisor:
+            elif div1 == div2:
                 ret = 1.0
             else:
                 visited = set()
-                ret = dfs(division, divisor, 1, visited)
+                ret = dfs(div1, div2, 1, visited)
             result.append(ret)
         return result
-            
