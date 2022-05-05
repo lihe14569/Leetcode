3af88ed53d5b0b7bde4c607ffce2bdc93829d084
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-05-05 11:13:37 -0600

    Time: 1348 ms (23.93%), Space: 60.8 MB (71.86%) - LeetHub

---
+++ 2034-stock-price-fluctuation/2034-stock-price-fluctuation.py
@@ -0,0 +1,35 @@
+from sortedcontainers import SortedDict
+class StockPrice:
+    
+    def __init__(self):
+        self.time_to_prices = SortedDict()
+        self.rec = SortedDict()
+
+    def update(self, timestamp: int, price: int) -> None:
+        if timestamp in self.time_to_prices:
+            prev_price = self.time_to_prices[timestamp]
+            self.rec[prev_price].remove(timestamp)
+            if len(self.rec[prev_price]) == 0:
+                self.rec.pop(prev_price)
+        if not price in self.rec:
+            self.rec[price] = set()
+        self.rec[price].add(timestamp)
+        self.time_to_prices[timestamp] = price
+
+    def current(self) -> int:
+        return self.time_to_prices.peekitem(-1)[1]
+
+    def maximum(self) -> int:
+        return self.rec.peekitem(-1)[0]
+
+    def minimum(self) -> int:
+        return self.rec.peekitem(0)[0]
+        
+
+
+# Your StockPrice object will be instantiated and called as such:
+# obj = StockPrice()
+# obj.update(timestamp,price)
+# param_2 = obj.current()
+# param_3 = obj.maximum()
+# param_4 = obj.minimum()
