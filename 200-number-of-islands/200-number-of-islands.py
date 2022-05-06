a96141db3266ca2518ee7f4533c869d74a8a7789
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-05-06 00:24:11 -0600

    Time: 397 ms (56.15%), Space: 20 MB (31.67%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,26 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        #union-find
         m, n, cnt= len(grid), len(grid[0]), 0
         dsu = DSU(m*n)
-        
-        for r in range(m):
-            for c in range(n):
-                if grid[r][c] == '1':
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    grid[i][j] = '0'
                     cnt += 1
-                    grid[r][c] == '0'
-                    for x, y in [(r + 1, c), (r -1,c), (r, c +1), (r, c - 1)]:
-                        if 0<=x< m and 0<= y < n and grid[x][y] == '1':
-                            if(dsu.find(r*n + c) != dsu.find(x*n + y)):
+                    for x, y in [(i + 1, j), (i -1, j), (i, j + 1), (i, j - 1)]:
+                        if 0 <= x < m and 0 <= y < n and grid[x][y] == '1':
+                            if dsu.find(i*n + j) != dsu.find(x*n + y):
+                                dsu.union(i*n + j, x*n + y)
                                 cnt -= 1
-                            dsu.union(r*n + c, x*n + y)
         return cnt
 
 class DSU:
     def __init__(self, n):
-        self.count = 0
         self.parent = [i for i in range(n)]
-        
+    
     def find(self, x):
         if self.parent[x] != x:
             self.parent[x] = self.find(self.parent[x])
