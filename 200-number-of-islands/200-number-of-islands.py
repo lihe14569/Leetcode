44bcc2b61dc90089fd5e44a1f634c91bdfeded4d
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-05-05 21:42:20 -0600

    Time: 515 ms (24.66%), Space: 19.8 MB (31.86%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,21 +1,30 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        #bfs 
-        res, m, n = 0, len(grid), len(grid[0])
-        def bfs(grid, i, j):
-            grid[i][j] = 'x'
-            q = deque([(i, j)])
-            while q:
-                size = len(q)
-                for k in range(size):
-                    i, j = q.popleft()
-                    for nx, ny in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
-                        if nx >= 0 and nx < m and ny >= 0 and ny < n and grid[nx][ny] == '1':
-                            q.append((nx, ny))
-                            grid[nx][ny] = 'x'
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == '1':
-                    bfs(grid, i, j)
-                    res += 1
-        return res
+        #union-find
+        m, n, cnt= len(grid), len(grid[0]), 0
+        dsu = DSU(m*n)
+        
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == '1':
+                    cnt += 1
+                    grid[r][c] == '0'
+                    for x, y in [(r + 1, c), (r -1,c), (r, c +1), (r, c - 1)]:
+                        if 0<=x< m and 0<= y < n and grid[x][y] == '1':
+                            if(dsu.find(r*n + c) != dsu.find(x*n + y)):
+                                cnt -= 1
+                            dsu.union(r*n + c, x*n + y)
+        return cnt
+
+class DSU:
+    def __init__(self, n):
+        self.count = 0
+        self.parent = [i for i in range(n)]
+        
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        self.parent[self.find(x)] = self.parent[y]
