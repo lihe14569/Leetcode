8930b6aae95330218358e27621d85f3b5804965a
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-05-07 13:46:59 -0600

    Time: 2949 ms (5.00%), Space: 20.4 MB (5.09%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,27 +1,25 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        #QUICKFIND
+        #quickselect
+        #based k value, search left half, or right half
         k = len(nums) - k
-        
-        def quickselect(nums, start, end, k):
-            if start == end: return nums[start]
-            
-            def partition(nums, start, end):
-                pivot = nums[end]
-                wall = start
-                for j in range(start, end):
+        def quickselect(l, r, k):
+            if l == r: return nums[l]
+            def partition(l, r):
+                pivot = nums[r]
+                wall = l
+                for j in range(l, r):
                     if nums[j] < pivot:
                         nums[j], nums[wall] = nums[wall], nums[j]
                         wall += 1
-                nums[wall], nums[end] = nums[end], nums[wall]
+                nums[wall], nums[r] = nums[r], nums[wall]
                 return wall
             
-            idx = partition(nums, start, end)
-            if idx == k:
-                return nums[k]
-            elif idx < k:
-                return quickselect(nums, idx + 1, end, k)
+            pos = partition(l, r)
+            
+            if pos == k: return nums[k]
+            elif pos < k:
+                return quickselect(pos + 1, r, k)
             else:
-                return quickselect(nums, start, idx - 1, k)
-        return quickselect(nums, 0, len(nums) - 1, k)
-                        
+                return quickselect(l, pos - 1, k)
+        return quickselect(0, len(nums) - 1, k)
