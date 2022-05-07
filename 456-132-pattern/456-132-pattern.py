a21182b0aceb573db5a2a81679363b0406256cbd
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-05-07 01:55:37 -0600

    Time: 580 ms (22.94%), Space: 32.2 MB (12.38%) - LeetHub

--- 456-132-pattern/456-132-pattern.py
+++ 456-132-pattern/456-132-pattern.py
@@ -1,8 +1,13 @@
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
-        stack, s3 = [], -float("inf")
-        for n in nums[::-1]:
-            if n < s3: return True
-            while stack and stack[-1] < n: s3 = stack.pop()
-            stack.append(n)
-        return False
+        n, num_k = len(nums), float('-inf')
+        stack = collections.deque([nums[n -1]])
+        for i in range(n - 2, -1, -1):
+            if nums[i] < num_k:
+                return True
+            while stack and stack[-1] < nums[i]:
+                num_k = max(num_k, stack[-1])
+                stack.pop()
+            if nums[i] > num_k:
+                stack.append(nums[i])
+        return False 
