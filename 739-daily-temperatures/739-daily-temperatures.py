e48754e49a828122b86c331c92e25f6fc8117df9
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-05-08 22:01:32 -0600

    Time: 1958 ms (16.85%), Space: 25 MB (50.32%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -1,14 +1,11 @@
 class Solution:
     def dailyTemperatures(self, t: List[int]) -> List[int]:
-        #正向单调栈
         res = [0] * len(t)
-        stack = deque()
-        #维持一个单调递减栈
+        stack = []
         for i in range(len(t)):
-            curr_t = t[i]
-            while len(stack) != 0 and t[stack[-1]] < curr_t:
-                idx = stack.pop()
-                res[idx] = i - idx
+            curr = t[i]
+            while stack and t[i] > t[stack[-1]]:
+                prevIdx = stack.pop()
+                res[prevIdx] = i - prevIdx
             stack.append(i)
         return res
-        
