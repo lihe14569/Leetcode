73c239189f1658d37b8773787f29b8396018c103
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-05-10 13:11:16 -0600

    Time: 181 ms (49.82%), Space: 20.1 MB (33.66%) - LeetHub

--- 218-the-skyline-problem/218-the-skyline-problem.py
+++ 218-the-skyline-problem/218-the-skyline-problem.py
@@ -1,12 +1,18 @@
 class Solution:
-    def getSkyline(self, buildings):
-        events = sorted([(L, -H, R) for L, R, H in buildings] + list({(R, 0, None) for _, R, _ in buildings}))
-        res, hp = [[0, 0]], [(0, float("inf"))]
-        for x, negH, R in events:
-            while x >= hp[0][1]: 
-                heapq.heappop(hp)
-            if negH: 
-                heapq.heappush(hp, (negH, R))
-            if res[-1][1] + hp[0][0]: 
-                res += [x, -hp[0][0]],
+    def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
+        events = [(L, -H, R) for L, R, H in buildings]
+        events += [(R, 0, 0) for _, R, _ in buildings]
+        events.sort()
+        
+        res= [[0, 0]]
+        live = [(0, float('inf'))]
+        
+        for pos, negh, r in events:
+            while live[0][1] <= pos:
+                heapq.heappop(live)
+            if negh: 
+                heapq.heappush(live, (negh, r))
+            if res[-1][1] != -live[0][0]:
+                res += [[pos, -live[0][0]]]
         return res[1:]
+            
