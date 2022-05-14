63e010d560e0ac22f9b209ad15bbe22e3c9adaf2
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-05-13 21:40:22 -0600

    Time: 554 ms (53.65%), Space: 38.9 MB (16.77%) - LeetHub

---
+++ 1146-snapshot-array/1146-snapshot-array.py
@@ -0,0 +1,27 @@
+class SnapshotArray:
+    # list of list
+    def __init__(self, length: int):
+        self.dic = [[[-1, 0]] for i in range(length)]
+        self.snapid = 0
+
+    def set(self, index: int, val: int) -> None:
+        if self.dic[index][-1][0] == self.snapid:
+            self.dic[index][-1][1] = val
+        else:
+            self.dic[index].append([self.snapid, val])
+
+    def snap(self) -> int:
+        self.snapid += 1
+        return self.snapid - 1
+
+    def get(self, index: int, snap_id: int) -> int:
+        lst = self.dic[index]
+        i = bisect.bisect(lst, [snap_id + 1]) - 1
+        return lst[i][1]
+
+
+# Your SnapshotArray object will be instantiated and called as such:
+# obj = SnapshotArray(length)
+# obj.set(index,val)
+# param_2 = obj.snap()
+# param_3 = obj.get(index,snap_id)
