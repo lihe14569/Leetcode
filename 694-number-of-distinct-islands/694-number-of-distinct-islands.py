a5e08707faf65e2ba3d03822b3ec386a94ad3ee9
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-05-15 16:45:17 -0600

    Time: 295 ms (44.42%), Space: 16.8 MB (60.54%) - LeetHub

--- 694-number-of-distinct-islands/694-number-of-distinct-islands.py
+++ 694-number-of-distinct-islands/694-number-of-distinct-islands.py
@@ -1,28 +1,25 @@
 class Solution:
     def numDistinctIslands(self, grid: List[List[int]]) -> int:
-
-        # Do a DFS to find all cells in the current island.
-        def dfs(row, col, direction):
-            if row < 0 or col < 0 or row >= len(grid) or col >= len(grid[0]):
+        def dfs(r, c, d):
+            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] == 0:
                 return
-            if (row, col) in seen or not grid[row][col]:
-                return
-            seen.add((row, col))
-            path_signature.append(direction)
-            dfs(row + 1, col, "D")
-            dfs(row - 1, col, "U")
-            dfs(row, col + 1, "R")
-            dfs(row, col - 1, "L")
-            path_signature.append("0")
-        
-        # Repeatedly start DFS's as long as there are islands remaining.
-        seen = set()
-        unique_islands = set()
-        for row in range(len(grid)):
-            for col in range(len(grid[0])):
-                path_signature = []
-                dfs(row, col, "0")
-                if path_signature:
-                    unique_islands.add(tuple(path_signature))
-        
-        return len(unique_islands)
+            path.append(d)
+            grid[r][c] = 0
+            dfs(r + 1, c, 'D')
+            dfs(r - 1, c, 'U')
+            dfs(r, c + 1, 'R')
+            dfs(r, c - 1, 'L')
+            path.append('0')
+        islands = set()
+        m, n = len(grid), len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] != 0:
+                    path = []
+                    dfs(i, j, '0')
+                    if path:
+                        islands.add(tuple(path))
+        return len(islands)
+            
+            
+            
