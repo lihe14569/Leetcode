8b0c3eb403918a866a5e398fde3a3047caa83a59
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-05-16 00:34:12 -0600

    Time: 4271 ms (13.46%), Space: 47.7 MB (43.03%) - LeetHub

--- 1610-maximum-number-of-visible-points/1610-maximum-number-of-visible-points.py
+++ 1610-maximum-number-of-visible-points/1610-maximum-number-of-visible-points.py
@@ -1,23 +1,21 @@
 class Solution:
     def visiblePoints(self, points: List[List[int]], angle: int, location: List[int]) -> int:
-        arr, extra = [], 0
+        res, origin = 0, 0
         xx, yy = location
-        
+        rad = []
         for x, y in points:
-            if x == xx and y == yy:
-                extra += 1
+            if x==xx and y == yy:
+                origin += 1
                 continue
-            arr.append(math.atan2(y - yy, x - xx))
+            rad.append(math.atan2(y - yy, x - xx))
         
-        arr.sort()
-        arr = arr + [x + 2.0 * math.pi for x in arr]
-        angle = math.pi * angle / 180
-        
-        l = ans = 0
-        for r in range(len(arr)):
-            while arr[r] - arr[l] > angle:
-                l += 1
-            ans = max(ans, r - l + 1)
-            
-        return ans + extra
-        
+        rad.sort()
+        #circular
+        rad = rad + [x + 2.0 * math.pi for x in rad]
+        angle = angle/180 * math.pi
+        l = 0
+        for i in range(len(rad)):
+            while rad[i] - rad[l] > angle:
+                l+= 1
+            res = max(res, i - l + 1)
+        return res + origin
