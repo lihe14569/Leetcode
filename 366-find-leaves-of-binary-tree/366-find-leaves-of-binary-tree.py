f0893df5c695039fe25cf0471be6e7ff582831e1
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-05-16 22:36:06 -0600

    Time: 53 ms (22.69%), Space: 13.9 MB (74.81%) - LeetHub

--- 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.py
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.py
@@ -5,30 +5,22 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def findLeaves(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[List[int]]
-        """
-        
+    def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
-        result=[]
+        res = []
         while root:
-            curLeaves = []
-            root = self._findLeaves(root, curLeaves)
-            
-            result.append(curLeaves)
-        
-        return result 
-
-    def _findLeaves(self, root, curLeaves):
+            curr_level = []
+            root = self.dfs(root, curr_level)
+            res.append(curr_level)
+        return res
+    def dfs(self,root, level):
         if not root:
             return None
         if not root.left and not root.right:
-            curLeaves.append(root.val)
+            level.append(root.val)
             return None
         else:
-            root.left = self._findLeaves(root.left, curLeaves)
-            root.right = self._findLeaves(root.right, curLeaves)
+            root.left = self.dfs(root.left, level)
+            root.right = self.dfs(root.right, level)
             return root
