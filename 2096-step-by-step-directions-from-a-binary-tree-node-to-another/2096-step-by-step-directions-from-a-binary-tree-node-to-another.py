272c2947ee3b6b6c57e097d2e6b0a9e6236240d4
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-05-16 22:51:07 -0600

    Time: 3082 ms (32.37%), Space: 57.5 MB (82.59%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -6,21 +6,24 @@
 #         self.right = right
 class Solution:
     def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
-        
-        def lca(node): 
-            """Return lowest common ancestor of start and dest nodes."""
-            if not node or node.val in (startValue , destValue): return node 
-            left, right = lca(node.left), lca(node.right)
-            return node if left and right else left or right
-        
-        root = lca(root) # only this sub-tree matters
-        
-        ps = pd = ""
-        stack = [(root, "")]
-        while stack: 
+        def lca(root):
+            if not root or root.val in (startValue, destValue):
+                return root
+            left, right = lca(root.left), lca(root.right)
+            if left and right:
+                return root
+            else:
+                return left or right
+        root = lca(root)
+        stack = []
+        stack.append((root, ''))
+        ps, pd = '', ''
+        while stack:
             node, path = stack.pop()
-            if node.val == startValue: ps = path 
+            if node.val == startValue: ps = path
             if node.val == destValue: pd = path
-            if node.left: stack.append((node.left, path + "L"))
-            if node.right: stack.append((node.right, path + "R"))
-        return "U"*len(ps) + pd
+            if node.left:
+                stack.append((node.left, path + 'L'))
+            if node.right:
+                stack.append((node.right, path + 'R'))
+        return len(ps)*'U' + pd
