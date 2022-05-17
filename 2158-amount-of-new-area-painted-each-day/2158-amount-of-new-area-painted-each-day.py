5647e0d12e1c792f1965490021ad8d82bdb58ac8
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-05-17 02:21:59 -0600

    Time: 2482 ms (46.97%), Space: 49.5 MB (47.34%) - LeetHub

--- 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.py
+++ 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.py
@@ -1,11 +1,11 @@
 class Solution:
     def amountPainted(self, paint: List[List[int]]) -> List[int]:
-        line, res = [0] * 50001, [0] * len(paint)
+        res, area = len(paint)*[0], [0]* 50001
+        
         for i, (start, end) in enumerate(paint):
             while start < end:
-                jump = max(start + 1, line[start])
-                res[i] += 1 if line[start] == 0 else 0
-                line[start] = max(line[start], end)  # compression
-                start = jump
+                next_start = max(start+ 1, area[start])
+                res[i] += 1 if area[start] == 0 else 0
+                area[start] = max(area[start], end)
+                start = next_start
         return res
-            
