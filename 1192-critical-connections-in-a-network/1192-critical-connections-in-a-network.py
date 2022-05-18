dcc7ac6f667f9b03250868109b5fccdad1b392df
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-05-17 22:51:21 -0600

    Time: 4457 ms (6.22%), Space: 80.1 MB (40.02%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.py
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.py
@@ -1,65 +1,36 @@
 class Solution:
-    
-    rank = {}
     graph = defaultdict(list)
-    conn_dict = {}
+    rank = {}
+    edges = set()
     
     def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
-
-        self.formGraph(n, connections)
+        self.buildGraph(n, connections)
         self.dfs(0, 0)
-        
-        result = []
-        for u, v in self.conn_dict:
-            result.append([u, v])
-        
-        return result
-            
-    def dfs(self, node: int, discovery_rank: int) -> int:
-        
-        # That means this node is already visited. We simply return the rank.
-        if self.rank[node]:
-            return self.rank[node]
-        
-        # Update the rank of this node.
-        self.rank[node] = discovery_rank
-        
-        # This is the max we have seen till now. So we start with this instead of INT_MAX or something.
-        min_rank = discovery_rank + 1
-        for neighbor in self.graph[node]:
-            
-            # Skip the parent.
-            if self.rank[neighbor] and self.rank[neighbor] == discovery_rank - 1:
-                continue
-                
-            # Recurse on the neighbor.    
-            recursive_rank = self.dfs(neighbor, discovery_rank + 1)
-            
-            # Step 1, check if this edge needs to be discarded.
-            if recursive_rank <= discovery_rank:
-                del self.conn_dict[(min(node, neighbor), max(node, neighbor))]
-            
-            # Step 2, update the minRank if needed.
-            min_rank = min(min_rank, recursive_rank)
-        
-        return min_rank
-    
-    def formGraph(self, n: int, connections: List[List[int]]):
-        
-        # Reinitialize for each test case
-        self.rank = {}
+        return list(self.edges)
+    def buildGraph(self, n: int, connections: List[List[int]]):
         self.graph = defaultdict(list)
-        self.conn_dict = {}
+        self.rank = {}
+        self.edges = set()
         
-        # Default rank for unvisited nodes is "null"
         for i in range(n):
             self.rank[i] = None
         
-        for edge in connections:
-            
-            # Bidirectional edges.
-            u, v = edge[0], edge[1]
+        for u, v in connections:
             self.graph[u].append(v)
             self.graph[v].append(u)
-            
-            self.conn_dict[(min(u, v), max(u, v))] = 1
+            self.edges.add((min(u, v), max(u, v)))
+    
+    def dfs(self, node: int, rank: int) -> int:
+        if self.rank[node]:
+            return self.rank[node]
+        self.rank[node] = rank
+        min_rank = rank + 1
+        for n in self.graph[node]:
+            if self.rank[n] and self.rank[n] == rank - 1:
+                continue
+            r_rank = self.dfs(n, rank + 1)
+            if r_rank <= rank:
+                self.edges.remove((min(node, n), max(node, n)))
+            min_rank = min(min_rank, r_rank)
+        return min_rank
+    
