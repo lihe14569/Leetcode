ec78af07047e1d8dc0bda6a75b71aaf561c416a2
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-05-21 14:03:01 -0600

    Time: 45 ms (40.31%), Space: 14 MB (33.36%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.py
+++ 140-word-break-ii/140-word-break-ii.py
@@ -1,18 +1,18 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        dic = set(wordDict)
-        res = []
-        def dfs(i, lst):
-            if i >= len(s):
-                res.append(' '.join(lst))
-                return
-                
-            for j in range(i + 1, len(s) + 1):
-                if s[i:j] in dic:
-                    lst.append(s[i:j])
-                    dfs(j, lst)
-                    lst.pop()
+        memo = defaultdict(list)
+        word_dic = set(wordDict)
+        def dfs(word):
+            if not word:
+                return [[]]
+            if word in memo:
+                return memo[word]
             
-        dfs(0, [])
-        return res
-        
+            for end in range(1, len(word) + 1):
+                w = word[:end]
+                if w in word_dic:
+                    for sentence in dfs(word[end:]):
+                        memo[word].append([w] + sentence)
+            return memo[word]
+        dfs(s)
+        return [ ' '.join(lst) for lst in memo[s]]
