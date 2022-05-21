8df4bbe5deb62d777868a46ad40e6c531bb723e8
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-05-21 17:37:34 -0600

    Time: 46 ms (37.71%), Space: 13.9 MB (33.36%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.py
+++ 140-word-break-ii/140-word-break-ii.py
@@ -1,18 +1,51 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        memo = defaultdict(list)
-        word_dic = set(wordDict)
-        def dfs(word):
-            if not word:
-                return [[]]
-            if word in memo:
-                return memo[word]
+        trie = Trie()
+        for w in wordDict:
+            trie.add(w)
+        res = []
+        def dfs(i, lst):
+            if i >= len(s):
+                res.append(lst[:])
+                return 
+            prefix = trie.search(s[i:])
+            for pre in prefix:
+                lst.append(s[i: i + pre + 1])
+                dfs(i + pre + 1, lst)
+                lst.pop()
+                
+        
+        dfs(0, [])
+        return [' '.join(l) for l in res]
+    
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def add(self, word):
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.is_word = True
+    
+    def search(self, word):
+        curr = self.root
+        ans = []
+        for i, c in enumerate(word):
+            if c not in curr.children:
+                break
+            curr = curr.children[c]
+            if curr.is_word:
+                ans.append(i)
+        return ans
+    
+        
             
-            for end in range(1, len(word) + 1):
-                w = word[:end]
-                if w in word_dic:
-                    for sentence in dfs(word[end:]):
-                        memo[word].append([w] + sentence)
-            return memo[word]
-        dfs(s)
-        return [ ' '.join(lst) for lst in memo[s]]
+                
+        
