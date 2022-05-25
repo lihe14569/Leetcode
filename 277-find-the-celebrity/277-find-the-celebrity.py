dd2e8793543563455d5c2c5b78ecdbef8a38aaeb
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-05-24 20:51:21 -0600

    Time: 2874 ms (5.06%), Space: 14.3 MB (17.79%) - LeetHub

--- 277-find-the-celebrity/277-find-the-celebrity.py
+++ 277-find-the-celebrity/277-find-the-celebrity.py
@@ -5,17 +5,14 @@
 class Solution:
     def findCelebrity(self, n: int) -> int:
         celeb = 0
-        for i in range(1, n):
-            if knows(celeb, i):
-                celeb = i
-        if self.checkCeleb(celeb, n):
-            return celeb
-        else:
-            return -1
-    def checkCeleb(self, p, n):
         for i in range(n):
-            if p == i: continue
-            if knows(p, i) or not knows(i, p):
-                return False
-        return True
-                
+            if celeb == i:
+                continue
+            if knows(celeb, i) or not knows(i, celeb):
+                celeb = i
+        for i in range(n):
+            if celeb == i:
+                continue
+            if knows(celeb, i) or not knows(i, celeb):
+                return -1
+        return celeb
