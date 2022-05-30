fe00227be5ee931d948143653913787194636e53
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-05-30 12:40:50 -0600

    Time: 6471 ms (33.33%), Space: 38.5 MB (100.00%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
@@ -1,15 +1,16 @@
 class Solution:
     def minimumObstacles(self, grid: List[List[int]]) -> int:
         m, n = map(len, (grid, grid[0]))
-        dist = [[inf] * n for _ in range(m)]
-        dist[0][0] = grid[0][0]
-        hp = [(dist[0][0], 0, 0)]
-        while hp:
-            o, r, c = heappop(hp)
-            if (r, c) == (m - 1, n - 1):
-                return o
-            for i, j in (r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1):
-                if m > i >= 0 <= j < n and grid[i][j] + o < dist[i][j]:
-                    dist[i][j] = grid[i][j] + o
-                    heappush(hp, (dist[i][j], i, j))
+        dis = [[inf]*n for _ in range(m)]
+        dis[0][0] = 0
+        hq = [[dis[0][0], 0 , 0]]
         
+        while hq:
+            curr_dis, r, c = heapq.heappop(hq)
+            if r == m - 1 and c == n - 1:
+                return curr_dis
+            for i, j in (r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1):
+                if 0 <= i < m and 0 <= j < n and curr_dis + grid[i][j] < dis[i][j]:
+                    dis[i][j] = curr_dis + grid[i][j]
+                    heapq.heappush(hq, [dis[i][j], i , j])
+                
