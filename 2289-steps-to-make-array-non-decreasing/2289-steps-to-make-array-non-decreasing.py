6d4a4e724a8bc7f3b29155b6e8739865fe8042a2
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-05-30 13:11:54 -0600

    Time: 2002 ms (20.00%), Space: 28.4 MB (100.00%) - LeetHub

--- 2289-steps-to-make-array-non-decreasing/2289-steps-to-make-array-non-decreasing.py
+++ 2289-steps-to-make-array-non-decreasing/2289-steps-to-make-array-non-decreasing.py
@@ -1,33 +1,13 @@
 class Solution:
-    
-#         stack = []
-#         #单调递增
-#         res = 0
-#         n = len(nums)
-#         for i in range(n):
-#             while stack and nums[i] >= nums[stack[-1]]:
-                
-#                 last = stack.pop()
-#                 while stack and nums[last] < 
-#                 res = max(res, i - stack.pop() - 1)
-#             stack.append(i)
-#         stack = []
-#         for i in range(n-1, -1, -1):
-#             while stack and nums[i] > nums[stack[-1]]:
-#                 res = max(res, stack.pop() - i)
-#             stack.append(i)
-#         return res
-
     def totalSteps(self, nums: List[int]) -> int:
-        res,stack = 0, []
-        for i in range(len(nums)-1,-1,-1):
-            cur = 0
-            while stack and nums[stack[-1][0]]<nums[i]:
-                _,v = stack.pop()
-                cur=max(cur+1,v)
-            res = max(res,cur)
-            stack.append([i,cur])
-        return res
-            
-                
-            
+        res=0
+        stack = []
+        dp = [0] * len(nums)
+        for i in range(len(nums)):
+            curr = 0
+            while stack and nums[i] >= nums[stack[-1]]:
+                curr = max(curr, dp[stack.pop()])
+            if stack:
+                dp[i] = curr + 1
+            stack.append(i)
+        return max(dp)
