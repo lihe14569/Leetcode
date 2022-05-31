db98234fc15bee9d775f327002c7198c84760e7e
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-05-30 22:35:15 -0600

    Time: 533 ms (15.00%), Space: 27.7 MB (63.10%) - LeetHub

---
+++ 1644-lowest-common-ancestor-of-a-binary-tree-ii/1644-lowest-common-ancestor-of-a-binary-tree-ii.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        foundP = foundQ = False
+        def LCA(root):
+            nonlocal foundP, foundQ
+            if not root:
+                return None
+            left = LCA(root.left)
+            right = LCA(root.right)
+            #marked found p or q
+            if root == p or root == q:
+                if root == p:
+                    foundP = True
+                if root == q:
+                    foundQ = True
+                return root
+            #case1
+            if left and right:
+                return root
+            if not left and not right:
+                return None
+            return left or right
+        res = LCA(root)
+        if foundP and foundQ:
+            return res
+        return None
+            
