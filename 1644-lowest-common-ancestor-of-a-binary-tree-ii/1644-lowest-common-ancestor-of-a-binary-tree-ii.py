35c0cb28defe2dedb52a9552328022690fd8e733
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-05-30 22:39:27 -0600

    Time: 516 ms (17.62%), Space: 30.7 MB (5.30%) - LeetHub

--- 1644-lowest-common-ancestor-of-a-binary-tree-ii/1644-lowest-common-ancestor-of-a-binary-tree-ii.py
+++ 1644-lowest-common-ancestor-of-a-binary-tree-ii/1644-lowest-common-ancestor-of-a-binary-tree-ii.py
@@ -7,28 +7,27 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        foundP = foundQ = False
-        def LCA(root):
-            nonlocal foundP, foundQ
+        parent ={}
+        parent[root] = None
+        def dfs(root):
+            nonlocal parent
             if not root:
-                return None
-            left = LCA(root.left)
-            right = LCA(root.right)
-            #marked found p or q
-            if root == p or root == q:
-                if root == p:
-                    foundP = True
-                if root == q:
-                    foundQ = True
-                return root
-            #case1
-            if left and right:
-                return root
-            if not left and not right:
-                return None
-            return left or right
-        res = LCA(root)
-        if foundP and foundQ:
-            return res
-        return None
-            
+                return 
+            if root.left:
+                parent[root.left] =root
+                dfs(root.left)
+            if root.right:
+                parent[root.right] = root
+                dfs(root.right)
+        dfs(root)
+        if not root or p not in parent or q not in parent:
+            return None
+        seen = set()
+        while p:
+            seen.add(p)
+            p = parent[p]
+        while q:
+            if q in seen:
+                return q
+            q = parent[q]
+        
