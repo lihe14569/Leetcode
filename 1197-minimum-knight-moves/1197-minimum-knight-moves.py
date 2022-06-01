97253567d880c3b4b86ce2bb64270fe7b97f6be0
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-05-31 22:37:00 -0600

    Time: 69 ms (91.93%), Space: 15.4 MB (89.60%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.py
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.py
@@ -1,15 +1,20 @@
 class Solution:
     def minKnightMoves(self, x: int, y: int) -> int:
 
-        @cache
+        cache = {}
         def dfs(x, y):
+            nonlocal cache
             if x + y == 0:
                 # base case: (0, 0)
                 return 0
             elif x + y == 2:
                 # base case: (1, 1), (0, 2), (2, 0)
                 return 2
+            elif (x, y) in cache:
+                return cache[(x, y)]
             else:
-                return min(dfs(abs(x - 1), abs(y - 2)), dfs(abs(x - 2), abs(y - 1))) + 1
+                cache[(x, y)] = min(dfs(abs(x - 1), abs(y - 2)), dfs(abs(x - 2), abs(y - 1))) + 1
+                return cache[(x, y)]
+                
 
         return dfs(abs(x), abs(y))
