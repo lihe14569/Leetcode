d0c9a86480f7291c4fb4114cd4765ecebab294e2
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-06-02 23:08:12 -0600

    Time: 3934 ms (29.42%), Space: 30.8 MB (86.03%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.py
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.py
@@ -1,34 +1,44 @@
 class BIT:
     def __init__(self, size):
-        self.bit = [0] * (size + 1)
-
-    def getSum(self, idx):  # Get sum in range [1..idx], 1-based indexing
+        self.sums = [0]*(size + 1)
+    
+    def lowbit(self, x):
+        return x & (-x)
+    
+    def getSum(self, i):
         s = 0
-        while idx > 0:
-            s += self.bit[idx]
-            idx -= idx & (-idx)
+        while i > 0:
+            s += self.sums[i]
+            i -= self.lowbit(i)
         return s
-
-    def getSumRange(self, left, right):  # left, right inclusive, 1-based indexing
-        return self.getSum(right) - self.getSum(left - 1)
-
-    def addValue(self, idx, val):  # 1-based indexing
-        while idx < len(self.bit):
-            self.bit[idx] += val
-            idx += idx & (-idx)
-
+    
+    def getSumRange(self, l, r):
+        return self.getSum(r) - self.getSum(l - 1)
+    
+    def insert(self, i, val):
+        while i < len(self.sums):
+            self.sums[i] += val
+            i += self.lowbit(i)
+    
+    
 class NumArray:
 
     def __init__(self, nums: List[int]):
         self.nums = nums
-        self.bit = BIT(len(nums))
-        for i, v in enumerate(nums):
-            self.bit.addValue(i+1, v)
-
+        self.BIT = BIT(len(nums))
+        for i, val in enumerate(nums):
+            self.BIT.insert(i + 1, val)
+            
     def update(self, index: int, val: int) -> None:
-        diff = val - self.nums[index]  # get diff amount of `val` compared to current value
-        self.bit.addValue(index+1, diff)  # add this `diff` amount at index `index+1` of BIT, plus 1 because in BIT it's 1-based indexing
-        self.nums[index] = val # update latest value of `nums[index]`
+        diff = val - self.nums[index]
+        self.BIT.insert(index + 1, diff)
+        self.nums[index] = val
 
     def sumRange(self, left: int, right: int) -> int:
-        return self.bit.getSumRange(left+1, right+1)
+        return self.BIT.getSumRange(left + 1, right + 1)
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# obj.update(index,val)
+# param_2 = obj.sumRange(left,right)
