301e5b0d24c35dd109f3a14349c175b476ef7b19
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-06-05 14:57:33 -0600

    Time: 56 ms (91.35%), Space: 18.5 MB (38.65%) - LeetHub

--- 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.py
+++ 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.py
@@ -4,32 +4,26 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
-    curr_cnt = 1
-    max_cnt = 1
-    prev = None
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
-        lst = []
-        self.inorder(root, lst)
-        return lst
-    def inorder(self, root, lst):
-        if not root:
-            return
-        self.inorder(root.left, lst)
-        if self.prev:
-            if self.prev.val == root.val:
-                self.curr_cnt +=1
+        res = []
+        max_freq = 0
+        dic = {}
+        def inorder(root):
+            nonlocal res, max_freq, dic
+            if not root:
+                return 
+            inorder(root.left)
+            if root.val not in dic:
+                dic[root.val] = 1
             else:
-                self.curr_cnt = 1
-        if self.curr_cnt > self.max_cnt:
-            self.max_cnt = self.curr_cnt
-            lst.clear()
-            lst.append(root.val)
-        elif self.curr_cnt == self.max_cnt:
-                lst.append(root.val)
-        self.prev = root
-        self.inorder(root.right, lst)
-    
-        
-        
+                dic[root.val] += 1
+            if dic[root.val] > max_freq:
+                max_freq = dic[root.val]
+                res.clear()
+                res.append(root.val)
+            elif dic[root.val] == max_freq:
+                res.append(root.val)
+            inorder(root.right)
+        inorder(root)
+        return res
