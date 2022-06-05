13de65f1f7d82c0ac4eb6a7f853b0dcdcefa269a
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-06-05 14:41:47 -0600

    Time: 61 ms (72.47%), Space: 13.8 MB (80.46%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.py
+++ 52-n-queens-ii/52-n-queens-ii.py
@@ -1,34 +1,23 @@
 class Solution:
-    def totalNQueens(self, n):
-        def backtrack(row, diagonals, anti_diagonals, cols):
-            # Base case - N queens have been placed
+    def totalNQueens(self, n: int) -> int:
+        def backtrack(row, cols, diagonals, anti_diagonals):
+            #base case
             if row == n:
                 return 1
-
             solutions = 0
             for col in range(n):
-                curr_diagonal = row - col
-                curr_anti_diagonal = row + col
-                # If the queen is not placeable
-                if (col in cols 
-                      or curr_diagonal in diagonals 
-                      or curr_anti_diagonal in anti_diagonals):
+                diagonal = row + col
+                anti_diagonal = row - col
+                if col in cols or diagonal in diagonals or anti_diagonal in anti_diagonals:
                     continue
-
-                # "Add" the queen to the board
                 cols.add(col)
-                diagonals.add(curr_diagonal)
-                anti_diagonals.add(curr_anti_diagonal)
-
-                # Move on to the next row with the updated board state
-                solutions += backtrack(row + 1, diagonals, anti_diagonals, cols)
-
-                # "Remove" the queen from the board since we have already
-                # explored all valid paths using the above function call
+                diagonals.add(diagonal)
+                anti_diagonals.add(anti_diagonal)
+                solutions +=backtrack(row + 1, cols, diagonals, anti_diagonals)
                 cols.remove(col)
-                diagonals.remove(curr_diagonal)
-                anti_diagonals.remove(curr_anti_diagonal)
-
+                diagonals.remove(diagonal)
+                anti_diagonals.remove(anti_diagonal)
             return solutions
-
+        
         return backtrack(0, set(), set(), set())
+        
