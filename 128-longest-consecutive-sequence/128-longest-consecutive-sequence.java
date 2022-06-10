10cf97c4d05c03ecc5b6d5d15ea2b54927a5ce93
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-06-09 21:52:37 -0600

    Time: 97 ms (43.19%), Space: 79 MB (35.44%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,19 +1,54 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
+        int n = nums.length;
+        if(n == 0) return 0;
+        DSU dsu = new DSU(n);
         Map<Integer, Integer> map = new HashMap<>();
-        int res = 0;
-            
-        for(int num : nums) {
-            if(!map.containsKey(num)) {
-                int left = map.getOrDefault(num - 1, 0);
-                int right = map.getOrDefault(num + 1, 0);
-                int sum = left + right + 1;
-                res = Math.max(res, sum);
-                map.put(num, sum);
-                if(left > 0) map.put(num - left, sum);
-                if(right > 0) map.put(num + right, sum);
-            }
-        }
-        return res;
+        
+        for(int i = 0; i < n; i++) {
+          if(map.containsKey(nums[i])) continue;
+          map.put(nums[i], i);
+          if(map.containsKey(nums[i] - 1))
+              dsu.union(i, map.get(nums[i] - 1));
+          if(map.containsKey(nums[i] + 1))
+              dsu.union(i, map.get(nums[i] + 1));
+      }
+        return dsu.findMax();
     }
 }
+
+class DSU {
+    int[] parent;
+    int[] size;
+    
+    public DSU(int n) {
+        parent = new int[n];
+        size = new int[n];
+        for(int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+    public int find(int x) {
+        if(parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+    public void union(int x, int y) {
+        int rootx = find(x), rooty = find(y);
+        if(rootx == rooty)
+            return;
+        if(size[rootx] < size[rooty]) {
+            parent[rootx] = rooty;
+            size[rooty] += size[rootx];
+        } else {
+            parent[rooty] = rootx;
+            size[rootx] += size[rooty];
+        }
+    }
+    public int findMax() {
+        return Arrays.stream(size).max().getAsInt();
+    }
+}
+
