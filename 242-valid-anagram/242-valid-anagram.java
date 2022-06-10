311dac0624b860e36a1e8966465fcaf0810f9a96
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-06-09 22:20:50 -0600

    Time: 10 ms (41.90%), Space: 43.1 MB (75.34%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        int[] cnt = new int[26];
         if(s.length() != t.length()) return false;
-        Map<Character, Integer> map = new HashMap<>();
-        for(char c : s.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
+        for(int i = 0; i < s.length(); i++) {
+            cnt[s.charAt(i) - 'a'] += 1;
+            cnt[t.charAt(i) - 'a'] -= 1;
         }
-        for(char c : t.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) - 1);
-            if(map.get(c) < 0) return false;
+        for(int val : cnt){
+            if(val != 0) return false;
         }
         return true;
     }
