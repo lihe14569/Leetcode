643207b1d54ff0ef8aee3871a85c788af08d7b72
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-06-12 21:37:43 -0600

    Time: 10 ms (16.62%), Space: 44.7 MB (16.57%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,20 +1,26 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
+        List<Integer> prev = triangle.get(0);
+        List<Integer> curr = new ArrayList<>();
+        
         int n = triangle.size();
-        int[][] dp = new int[n][n];
-        dp[0][0] = triangle.get(0).get(0);
-        
-        for(int i = 1; i < n; i++) {
-            dp[i][0] = dp[i - 1][0] + triangle.get(i).get(0);
-            for(int j = 1; j <= i; j++) {
-                dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j - 1]) + triangle.get(i).get(j);
+        for(int i = 1; i < triangle.size(); i++) {
+            List<Integer> currLevel = triangle.get(i);
+            curr = new ArrayList<>();
+            for(int j = 0; j < currLevel.size(); j++) {
+                if(j == 0) 
+                    curr.add(prev.get(0) + currLevel.get(0));
+                else if(j == currLevel.size() - 1)
+                    curr.add(prev.get(prev.size() - 1) + currLevel.get(currLevel.size() - 1));
+                else {
+                    curr.add(Math.min(prev.get(j - 1), prev.get(j)) + currLevel.get(j));
+                }
             }
-            dp[i][i] = dp[i - 1][i - 1] + triangle.get(i).get(i); 
+            prev = curr;
         }
-        
-        int res = dp[n - 1][0];
-        for(int i = 1; i < n; i++) {
-            res = Math.min(res, dp[n - 1][i]);
+        int res = Integer.MAX_VALUE;
+        for(int val : prev) {
+            res = Math.min(res, val);
         }
         return res;
     }
