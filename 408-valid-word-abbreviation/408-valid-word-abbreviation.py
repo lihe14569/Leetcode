e8e25cddfc8905d06397612e1587cf98ddf069a7
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-06-15 16:11:06 -0600

    Time: 69 ms (5.64%), Space: 14 MB (29.88%) - LeetHub

---
+++ 408-valid-word-abbreviation/408-valid-word-abbreviation.py
@@ -0,0 +1,17 @@
+class Solution:
+    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+        i, j = 0, 0
+        while i < len(word) and j < len(abbr):
+            if word[i] == abbr[j]:
+                i += 1
+                j += 1
+                continue
+            if ord(abbr[j]) <= ord('0') or ord(abbr[j]) >= ord('9'):
+                return False
+            start = j
+            while j < len(abbr) and ord(abbr[j]) >= ord('0') and ord(abbr[j]) <= ord('9'):
+                j += 1
+            print(j)
+            step = int(abbr[start:j])
+            i += step
+        return i == len(word) and j == len(abbr)
