3baf21247f392392d6f9355863b02a6fbdd72a39
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-06-15 22:54:11 -0600

    Time: 60 ms (49.93%), Space: 43.1 MB (64.12%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,24 +1,21 @@
 class Solution {
     public String longestPalindrome(String s) {
-        String res = "";
         int l = 0, r = 0;
         for(int i = 0; i < s.length() - 1; i++) {
-            int len1 = panlindrome(s, i, i);
-            int len2 = panlindrome(s, i, i + 1);
+            int len1 = len(s, i, i);
+            int len2 = len(s, i, i + 1);
             int len = Math.max(len1, len2);
-            if(len > r - l) {
-                r = i + len/ 2;
-                l = i - (len - 1) / 2;
+            if(len > r - l + 1) {
+                l = i - (len - 1)/ 2;
+                r = i + len / 2;
             }
         }
         return s.substring(l, r + 1);
     }
-    public int panlindrome(String s, int l , int r) {
-        while(l >= 0 && r < s.length()) {
-            if(s.charAt(l) == s.charAt(r)) {
-                l--;
-                r++;
-            } else break;
+    int len(String s, int l, int r) {
+        while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
         }
         return r - l - 1;
     }
