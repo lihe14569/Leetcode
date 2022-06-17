99be7c232dd93603581dfc329ba2c118f0f517b4
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-06-17 09:52:04 -0600

    Time: 4 ms (50.87%), Space: 54.3 MB (67.36%) - LeetHub

--- 645-set-mismatch/645-set-mismatch.java
+++ 645-set-mismatch/645-set-mismatch.java
@@ -1,27 +1,21 @@
 public class Solution {
     public int[] findErrorNums(int[] nums) {
-        int xor = 0, xor0 = 0, xor1 = 0;
-        for (int n: nums)
-            xor ^= n;
-        for (int i = 1; i <= nums.length; i++)
-            xor ^= i;
-        int rightmostbit = xor & ~(xor - 1);
-        for (int n: nums) {
-            if ((n & rightmostbit) != 0)
-                xor1 ^= n;
-            else
-                xor0 ^= n;
-        }
-        for (int i = 1; i <= nums.length; i++) {
-            if ((i & rightmostbit) != 0)
-                xor1 ^= i;
-            else
-                xor0 ^= i;
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] != i + 1 && nums[nums[i] - 1] != nums[i]) {
+                swap(nums, i, nums[i] - 1);
+            }
         }
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == xor0)
-                return new int[]{xor0, xor1};
+            if (nums[i] != i + 1) {
+                return new int[]{nums[i], i + 1};
+            }
         }
-        return new int[]{xor1, xor0};
+        return null;
+}
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 }
