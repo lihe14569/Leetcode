f1df238c0754c2e7bc3a0c5f19c2ca77f4d1caab
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-06-18 15:34:06 -0600

    Time: 258 ms (36.10%), Space: 80.8 MB (26.16%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,23 +1,20 @@
 class Solution {
     public String longestPalindrome(String s) {
-        //中心扩散
-        int l = 0, r = 0;
-        for(int i = 0; i < s.length() - 1; i++) {
-            int len1 = panlindrome(s, i , i);
-            int len2 = panlindrome(s, i , i + 1);
-            int len = Math.max(len1, len2);
-            if(len > r - l + 1) {
-                l = i - (len - 1) /2;
-                r = i +  len / 2;
+        //dp
+        int  n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int l = 0, r= 0;
+        for(int i = n - 1; i >= 0; i--) {
+            for(int j = i; j < n; j++) {
+                if(s.charAt(i) == s.charAt(j) && (j - i <= 2 || dp[i + 1][j - 1])) {
+                    dp[i][j] = true;
+                    if(j - i > r - l) {
+                        l = i;
+                        r = j;
+                    }
+                }
             }
         }
         return s.substring(l, r + 1);
-    }
-    public int panlindrome(String s, int l, int r) {
-        while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
-            l--;
-            r++;
-        }
-        return r - l - 1;
     }
 }
