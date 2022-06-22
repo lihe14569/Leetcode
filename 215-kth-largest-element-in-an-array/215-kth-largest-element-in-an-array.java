d8452c3b667ced801592e9470ab9a9f57f24dd25
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-06-21 20:28:32 -0600

    Time: 21 ms (13.46%), Space: 44.7 MB (51.64%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,19 +1,36 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        int l = -10000, r = 10000;
-        while(l < r) {
-            int m = (l + r + 1)/ 2;
-            int count = greatEqual(nums, m);
-            if(count >= k) l = m;
-            else r = m - 1;
-        }
-        return l;
+        k = nums.length - k;
+        //quickselect
+        return quickselect(nums, 0, nums.length - 1, k);
     }
-    public int greatEqual(int[] nums, int m) {
-        int res = 0;
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] >= m) res++;
+    public int quickselect(int[] nums, int l, int r, int k) {
+        //base case
+        if(l == r) return nums[l];
+        int index = partition(nums, l, r);
+        if(index == k) return nums[k];
+        else if(index < k) {
+            return quickselect(nums, index + 1, r, k);
+        } else {
+            return quickselect(nums, l, index - 1, k);
         }
-        return res;
+    }
+    public int partition(int[] nums, int l, int r) {
+        //pivot index is r
+        int pivot = nums[r];
+        int wall = l;
+        for(int i = l; i < r; i++) {
+            if(nums[i] < pivot) {
+                swap(nums, i, wall);
+                wall++;
+            }
+        }
+        swap(nums, wall, r);
+        return wall;
+    }
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
