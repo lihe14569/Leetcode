bf95d1d5761c70e264670b8080896f677393ffff
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-06-23 09:39:45 -0600

    Time: 181 ms (14.23%), Space: 75.2 MB (30.57%) - LeetHub

--- 352-data-stream-as-disjoint-intervals/352-data-stream-as-disjoint-intervals.java
+++ 352-data-stream-as-disjoint-intervals/352-data-stream-as-disjoint-intervals.java
@@ -1,30 +1,48 @@
 class SummaryRanges {
-    private TreeSet<int[]> intervalSet; 
 
+    TreeMap<Integer, Integer> intervals;
     public SummaryRanges() {
-        this.intervalSet = new TreeSet<>((a, b) -> a[0] - b[0]);
+        this.intervals = new TreeMap<>();
     }
     
     public void addNum(int val) {
-        int[] valInterval = new int[]{val, val};
-        int[] floor = intervalSet.floor(valInterval);
-        if (floor != null) {
-            if (floor[1] >= val) {
-                return;
-            } else if (floor[1] + 1 == val) {
-                valInterval[0] = floor[0];
-                intervalSet.remove(floor);
-            }
+        if (intervals.isEmpty()) {
+            intervals.put(val, val);
+            return;
         }
-        int[] higher = intervalSet.higher(valInterval);
-        if (higher != null && higher[0] == val + 1) {
-            valInterval[1] = higher[1];
-            intervalSet.remove(higher);
+        
+        Map.Entry<Integer, Integer> pre = intervals.floorEntry(val);
+        Map.Entry<Integer, Integer> post = intervals.ceilingEntry(val);
+        
+        int left = val, right = val;
+        
+        // if entry already exists in an interval
+        if (pre != null && pre.getKey() <= left && pre.getValue() >= left)
+            return;
+        
+		// if value can be merged with a floor entry
+        if (pre != null && pre.getValue() + 1 == val) {
+            left = pre.getKey();
+            intervals.remove(pre.getKey()); // remove old interval
         }
-        intervalSet.add(valInterval);
+        
+		// if the value can be merged with a ceiling entry
+        if (post != null && val + 1 == post.getKey()) {
+            right = post.getValue();
+            intervals.remove(post.getKey()); // remove old entry
+        }
+        
+        intervals.put(left, right); // add the new interval to sorted map
     }
     
     public int[][] getIntervals() {
-        return this.intervalSet.toArray(new int[0][]);
+        int[][] res = new int[intervals.size()][2];
+        int index = 0;
+        for (Map.Entry<Integer, Integer> entry : intervals.entrySet()) {
+            res[index][0] = entry.getKey();
+            res[index][1] = entry.getValue();
+            index++;
+        }
+        return res;
     }
 }
