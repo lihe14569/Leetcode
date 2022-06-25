aa837fff9661e555ddf4973c4eddacbba09b8ce1
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-06-24 20:33:21 -0600

    Time: 1 ms (97.53%), Space: 54.7 MB (5.06%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.java
+++ 665-non-decreasing-array/665-non-decreasing-array.java
@@ -4,15 +4,10 @@ class Solution {
         for(int i = 1; i < nums.length; i++) {
             if(nums[i - 1] <= nums[i])
                 continue;
-            //find non-decreasing order
-            //1. greedy change
-            //if nums[i - 1] > nums[i], we change nums[i-1]= nums[i]
-            //there is a special case nums[i - 2] > nums[i], we can only change nums[i]
             cnt++;
             if(i - 2 >= 0 && nums[i - 2] > nums[i])
                 nums[i] = nums[i - 1];
-            else 
-                nums[i - 1] = nums[i];
+            else nums[i - 1] = nums[i];
         }
         return cnt <= 1;
     }
