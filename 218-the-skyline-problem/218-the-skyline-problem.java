e52872ce56f520be845957892235ddc1993771d3
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-06-25 00:46:15 -0600

    Time: 376 ms (14.39%), Space: 51.8 MB (67.99%) - LeetHub

--- 218-the-skyline-problem/218-the-skyline-problem.java
+++ 218-the-skyline-problem/218-the-skyline-problem.java
@@ -5,14 +5,14 @@ class Solution {
             lines.add(new int[]{b[0], -b[2]});
             lines.add(new int[]{b[1], b[2]});
         }
-        Collections.sort(lines, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        Collections.sort(lines, (a , b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
         pq.offer(0);
         int preMax = 0;
-        List<List<Integer>> res= new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
         for(int[] l : lines) {
             if(l[1] < 0) {
-                pq.add(-l[1]);
+                pq.offer(-l[1]);
             } else {
                 pq.remove(l[1]);
             }
