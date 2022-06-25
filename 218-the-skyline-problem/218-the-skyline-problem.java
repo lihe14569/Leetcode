1130e314994bf91def294933719e0a86d23f4bd8
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-06-24 23:02:52 -0600

    Time: 394 ms (12.10%), Space: 51.5 MB (72.46%) - LeetHub

--- 218-the-skyline-problem/218-the-skyline-problem.java
+++ 218-the-skyline-problem/218-the-skyline-problem.java
@@ -9,15 +9,16 @@ class Solution {
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
         pq.offer(0);
         int preMax = 0;
-        List<List<Integer>> res = new ArrayList<>();
-        for(int[] b : lines) {
-            if(b[1] < 0)
-                pq.offer(-b[1]);
-            else
-                pq.remove(b[1]);
+        List<List<Integer>> res= new ArrayList<>();
+        for(int[] l : lines) {
+            if(l[1] < 0) {
+                pq.add(-l[1]);
+            } else {
+                pq.remove(l[1]);
+            }
             int currMax = pq.peek();
             if(currMax != preMax) {
-                res.add(List.of(b[0], currMax));
+                res.add(List.of(l[0], currMax));
                 preMax = currMax;
             }
         }
