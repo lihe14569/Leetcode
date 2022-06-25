620a7db7226e3adcb6cfc4f710311a98daef7e64
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-06-24 20:51:39 -0600

    Time: 12 ms (80.21%), Space: 57.3 MB (75.00%) - LeetHub

--- 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.java
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.java
@@ -1,37 +1,22 @@
 class Solution {
     public boolean isPossible(int[] target) {
-        
-        // Handle the n = 1 case.
-        if (target.length == 1) {
-            return target[0] == 1;
+        if(target.length == 1) return target[0] == 1;
+        PriorityQueue<Integer> pq =new PriorityQueue<>((a, b) -> b - a);
+        int sum = 0;
+        for(int num : target){
+            pq.offer(num);
+            sum += num;
         }
         
-        int totalSum = Arrays.stream(target).sum();
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
-        for (int num : target) {
-            pq.add(num);
+        while(pq.peek() != 1) {
+            int largest = pq.poll();
+            int others = sum - largest;
+            if(others == 1) return true;
+            int prev = largest % others;
+            if(prev == 0 || prev == largest) return false;
+            pq.offer(prev);
+            sum = sum - largest + prev;
         }
-        
-        while (pq.element() > 1) {
-            int largest = pq.remove();
-            int rest = totalSum - largest;
-            
-            // This will only occur if n = 2.
-            if (rest == 1) {
-                return true;
-            }
-            int x = largest % rest;
-            
-            // If x is now 0 (invalid) or didn't
-            // change, then we know this is impossible.
-            if (x == 0 || x == largest) {
-                return false;
-            }
-            pq.add(x);
-            totalSum = totalSum - largest + x;
-        }
-        
-        return true; 
+        return true;
     }
 }
