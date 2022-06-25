90eabe718ab531bb0e41b5add2b26a7fd684bcf7
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-06-25 16:56:39 -0600

    Time: 479 ms (100.00%), Space: 52.8 MB (75.00%) - LeetHub

--- 2318-number-of-distinct-roll-sequences/2318-number-of-distinct-roll-sequences.py
+++ 2318-number-of-distinct-roll-sequences/2318-number-of-distinct-roll-sequences.py
@@ -1,40 +1,25 @@
-class Solution {
-    public int distinctSequences(int n) {
-        int[][][] dp = new int[10010][6][6];
-        int MOD = 1000000007;
-        System.out.println(MOD);
-        //corner case : n == 1
-        if(n == 1) return 6;
-        //boundary case: n == 2
-        for(int i = 0; i < 6; i++) {
-            for(int j = 0; j < 6; j++) {
-                if(gcd(i + 1, j + 1) == 1 && i != j)
-                    dp[2][i][j] = 1;
-            }
-        }
-        //dp[i][j][k]:长度为i的倒数第二个数j，第一个数位k的
-        for(int i = 3; i <= n; i++) {
-            for(int j = 0; j < 6; j++) {
-                for(int k = 0; k < 6; k++) {
-                    if(gcd(j + 1, k + 1) == 1 && j != k) {
-                        for(int u = 0; u < 6; u++) {
-                            if(u != j && u != k && gcd(u + 1, j + 1) == 1)
-                                dp[i][j][k] = (dp[i][j][k] + dp[i - 1][u][j]) % MOD;
-                        }
-                    }
-                    
-                }
-            }
-        }
-        int res  = 0;
-        for(int i = 0; i < 6; i++) {
-            for(int j = 0; j < 6; j++) {
-                res = (res + dp[n][i][j]) % MOD;
-            }
-        }
-        return res;
-    }
-    public int gcd(int a, int b) {
-        return b != 0 ? gcd(b, a % b) : a;
-    }
-}
+mod=1000000007
+@cache
+def func(n,prev,pp):
+    if n==0:
+        return 1
+    ans=0
+    for i in range(1,7):
+        if prev==-1:
+            ans+=func(n-1,i,prev)
+            ans=ans%mod
+        elif pp==-1:
+            if(math.gcd(i,prev)==1 and i!=prev):
+                ans+=func(n-1,i,prev)
+                ans=ans%mod
+        else:
+            if(math.gcd(i,prev)==1 and i!=prev and i!=pp):
+                ans+=func(n-1,i,prev)
+                ans=ans%mod
+    return ans;
+class Solution:
+            
+    def distinctSequences(self, n: int) -> int:
+        return func(n,-1,-1)
+    
+                
