11661ec4e4efdd400e7757f0a1893324b34fbdf7
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-06-24 21:04:33 -0600

    Time: 15 ms (56.25%), Space: 57.3 MB (71.87%) - LeetHub

--- 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.java
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.java
@@ -2,20 +2,20 @@ class Solution {
     public boolean isPossible(int[] target) {
         if(target.length == 1) return target[0] == 1;
         PriorityQueue<Integer> pq =new PriorityQueue<>((a, b) -> b - a);
-        int sum = 0;
+        long sum = 0;
         for(int num : target){
             pq.offer(num);
             sum += num;
         }
         
         while(pq.peek() != 1) {
-            int largest = pq.poll();
-            int others = sum - largest;
-            if(others == 1) return true;
-            int prev = largest % others;
-            if(prev == 0 || prev == largest) return false;
-            pq.offer(prev);
-            sum = sum - largest + prev;
+            int a = pq.poll();
+            long others = sum - a;
+            if(others == 0 || a <= others) return false;
+            long b = a % others;
+            if(b == 0) b = others;
+            pq.offer((int)b);
+            sum = others + b;
         }
         return true;
     }
