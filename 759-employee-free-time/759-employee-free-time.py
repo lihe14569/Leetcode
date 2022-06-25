26387b65f0b59754b95fd0d55e134f941a80aec7
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-06-24 21:43:17 -0600

    Time: 131 ms (53.40%), Space: 15.7 MB (77.60%) - LeetHub

--- 759-employee-free-time/759-employee-free-time.py
+++ 759-employee-free-time/759-employee-free-time.py
@@ -1,37 +1,19 @@
-/*
-// Definition for an Interval.
-class Interval {
-    public int start;
-    public int end;
+"""
+# Definition for an Interval.
+class Interval:
+    def __init__(self, start: int = None, end: int = None):
+        self.start = start
+        self.end = end
+"""
 
-    public Interval() {}
-
-    public Interval(int _start, int _end) {
-        start = _start;
-        end = _end;
-    }
-};
-*/
-
-class Solution {
-    public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
-        PriorityQueue<Interval> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.start, b.start));
-        //put all interval in pq
-        for(List<Interval> lst : schedule) {
-            for(Interval i : lst) {
-                pq.offer(i);
-            }
-        }
-        Interval curr = pq.poll();
-        List<Interval> res = new ArrayList<>();
-        while(!pq.isEmpty()) {
-            if(curr.end >=  pq.peek().start) {
-                curr.end = Math.max(curr.end, pq.poll().end);
-            } else {
-                res.add(new Interval(curr.end, pq.peek().start));
-                curr = pq.poll();
-            }
-        }
-        return res;
-    }
-}
+class Solution:
+    def employeeFreeTime(self, schedule: '[[Interval]]') -> '[Interval]':
+        lst = sorted([i for s in schedule for i in s], key=lambda x : x.start)
+        prev, res = lst[0], []
+        for i in lst[1:]:
+            if prev.end >= i.start:
+                prev.end = max(prev.end, i.end)
+            else:
+                res.append(Interval(prev.end, i.start))
+                prev = i
+        return res
