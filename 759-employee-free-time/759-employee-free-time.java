872e496ca591983d39212ea49179d238363a3868
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-06-24 21:20:38 -0600

    Time: 12 ms (86.54%), Space: 55.3 MB (14.79%) - LeetHub

--- 759-employee-free-time/759-employee-free-time.java
+++ 759-employee-free-time/759-employee-free-time.java
@@ -15,16 +15,17 @@ class Interval {
 
 class Solution {
     public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
-        List<Interval> res = new ArrayList<>();
         PriorityQueue<Interval> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.start, b.start));
-        for(List<Interval> l : schedule) {
-            for(Interval interv : l) {
-                pq.offer(interv);
+        //put all interval in pq
+        for(List<Interval> lst : schedule) {
+            for(Interval i : lst) {
+                pq.offer(i);
             }
         }
         Interval curr = pq.poll();
+        List<Interval> res = new ArrayList<>();
         while(!pq.isEmpty()) {
-            if(curr.end >= pq.peek().start) {
+            if(curr.end >=  pq.peek().start) {
                 curr.end = Math.max(curr.end, pq.poll().end);
             } else {
                 res.add(new Interval(curr.end, pq.peek().start));
