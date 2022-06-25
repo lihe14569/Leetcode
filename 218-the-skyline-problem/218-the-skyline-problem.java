18257cfc99e8b346872cda1555af704b0c6417b7
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-06-24 22:48:09 -0600

    Time: 263 ms (40.26%), Space: 45.4 MB (90.26%) - LeetHub

--- 218-the-skyline-problem/218-the-skyline-problem.java
+++ 218-the-skyline-problem/218-the-skyline-problem.java
@@ -1,21 +1,24 @@
 class Solution {
-    public List<List<Integer>> getSkyline(int[][] buildings) {
-        List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> getSkyline(int[][] bs) {
         List<int[]> lines = new ArrayList<>();
-        for(int[] b : buildings) {
+        for(int[] b : bs) {
             lines.add(new int[]{b[0], -b[2]});
             lines.add(new int[]{b[1], b[2]});
         }
-        Collections.sort(lines, (a, b) -> a[0] == b[0] ? Integer.compare(a[1], b[1]) : Integer.compare(a[0], b[0]));
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b- a);
-        int premax = 0;
+        Collections.sort(lines, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        pq.offer(0);
+        int preMax = 0;
+        List<List<Integer>> res = new ArrayList<>();
         for(int[] b : lines) {
-            if(b[1] < 0) pq.offer(-b[1]);
-            else pq.remove(b[1]);
-            int currmax = pq.isEmpty() ? 0 : pq.peek();
-            if(currmax != premax) {
-                res.add(List.of(b[0], currmax));
-                premax = currmax;
+            if(b[1] < 0)
+                pq.offer(-b[1]);
+            else
+                pq.remove(b[1]);
+            int currMax = pq.peek();
+            if(currMax != preMax) {
+                res.add(List.of(b[0], currMax));
+                preMax = currMax;
             }
         }
         return res;
