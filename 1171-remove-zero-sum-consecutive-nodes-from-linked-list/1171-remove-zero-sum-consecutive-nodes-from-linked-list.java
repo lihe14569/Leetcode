b320d636fbff50e61cf2b63e6b8042bb8ce1d6f0
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-06-28 08:35:42 -0600

    Time: 3 ms (79.62%), Space: 44.4 MB (44.16%) - LeetHub

--- 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.java
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.java
@@ -10,22 +10,20 @@
  */
 class Solution {
     public ListNode removeZeroSumSublists(ListNode head) {
-  
         int prefix = 0;
-        ListNode dummy = new ListNode(0);
+        ListNode dummy = new ListNode(-1);
         dummy.next = head;
-        Map<Integer, ListNode> seen = new HashMap<>();
-        seen.put(0, dummy);
-        for (ListNode i = dummy; i != null; i = i.next) {
-            prefix += i.val;
-            seen.put(prefix, i);
+        Map<Integer, ListNode> map = new HashMap<>();
+        map.put(0, dummy);
+        for(ListNode curr = dummy; curr != null; curr = curr.next) {
+            prefix += curr.val;
+            map.put(prefix, curr);
         }
         prefix = 0;
-        for (ListNode i = dummy; i != null; i = i.next) {
-            prefix += i.val;
-            i.next = seen.get(prefix).next;
+        for(ListNode curr = dummy; curr != null; curr = curr.next) {
+            prefix += curr.val;
+            curr.next = map.get(prefix).next;
         }
         return dummy.next;
-        
     }
 }
