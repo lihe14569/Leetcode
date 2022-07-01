b81b96230b4b651b28c431f5b163dde5fcf56dc7
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-06-30 23:31:00 -0600

    Time: 3 ms (85.41%), Space: 44.3 MB (53.22%) - LeetHub

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -14,25 +14,24 @@
  * }
  */
 class Solution {
-    int[] inorder;
-    int[] postorder;
+    int[] iorder;
+    int[] porder;
     Map<Integer, Integer> map;
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        this.inorder = inorder;
-        this.postorder = postorder;
-        int n = inorder.length;
+        iorder = inorder;
+        porder = postorder;
+        int n = iorder.length;
         map = new HashMap<>();
         for(int i = 0; i < n; i++) map.put(inorder[i], i);
-        
-        return dfs(0, n - 1, 0, n - 1);
+        return dfs(0, n -1, 0, n -1);
     }
-    public TreeNode dfs(int inStart, int inEnd, int postStart, int postEnd) {
-        if(inStart > inEnd) return null;
-        TreeNode root = new TreeNode(postorder[postEnd]);
+    public TreeNode dfs(int iS, int iE, int pS, int pE) {
+        if(iS > iE) return null;
+        TreeNode root = new TreeNode(porder[pE]);
         int idx = map.get(root.val);
-        int rightLen = inEnd - idx;
-        root.left = dfs(inStart, idx - 1, postStart, postEnd - rightLen - 1);
-        root.right = dfs(idx + 1, inEnd, postEnd - rightLen, postEnd- 1);
+        int rightLen = iE - idx;
+        root.left = dfs(iS, idx - 1, pS, pE - 1 - rightLen);
+        root.right = dfs(idx + 1, iE, pE - rightLen, pE - 1);
         return root;
     }
 }
