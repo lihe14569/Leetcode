1a16912ba0fb94d632ac57d608b50a3c5904fae9
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-07-01 01:23:29 -0600

    Time: 19 ms (43.27%), Space: 76.4 MB (45.58%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -10,48 +10,15 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        //bottom up
-        if(head == null || head.next == null) return head;
-        ListNode dummy = new ListNode(-1);
-        dummy.next = head;
-        //get list length
-        int n = 0;
-        for(ListNode p = head; p != null; p =p.next) n++;
-        for(int i = 1; i < n; i*= 2) {
-            ListNode curr = dummy;
-            for(int j = 0; j + i < n; j += 2 * i) {
-                ListNode first = curr.next, second = first;
-                for(int k = 0; k < i; k++) second = second.next;
-                //merge sort two part
-                int f = 0, s = 0;
-                while(f < i && s < i && second != null) {
-                    if(first.val < second.val) {
-                        curr.next = first;
-                        first = first.next;
-                        curr = curr.next;
-                        f++;
-                    } else {
-                        curr.next = second;
-                        second = second.next;
-                        curr = curr.next;
-                        s++;
-                    }
-                }
-                while(f < i) {
-                    curr.next = first;
-                    first = first.next;
-                    curr =curr.next;
-                    f++;
-                }
-                while(s < i && second != null) {
-                    curr.next = second;
-                    second = second.next;
-                    curr = curr.next;
-                    s++;
-                }
-                curr.next = second;
-            }
+        List<Integer> lst = new ArrayList<>();
+        for(ListNode curr = head; curr != null; curr = curr.next) {
+            lst.add(curr.val);
         }
-        return dummy.next;
+        Collections.sort(lst);
+        int i = 0;
+        for(ListNode curr = head; curr != null; curr = curr.next) {
+            curr.val = lst.get(i++);
+        }
+        return head;
     }
 }
