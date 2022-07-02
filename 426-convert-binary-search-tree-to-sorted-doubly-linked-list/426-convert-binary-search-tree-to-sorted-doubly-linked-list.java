9bd428274328b806baa31c1e9a415a042eab7bcd
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-07-01 21:21:25 -0600

    Time: 0 ms (100.00%), Space: 41.7 MB (90.26%) - LeetHub

--- 426-convert-binary-search-tree-to-sorted-doubly-linked-list/426-convert-binary-search-tree-to-sorted-doubly-linked-list.java
+++ 426-convert-binary-search-tree-to-sorted-doubly-linked-list/426-convert-binary-search-tree-to-sorted-doubly-linked-list.java
@@ -20,23 +20,26 @@ class Node {
 */
 
 class Solution {
-    Node first = null, last = null;
     public Node treeToDoublyList(Node root) {
-        if(root == null) return root;
-        inorder(root);
+        if(root == null) return null;
+        Node first = null, last = null;
+        Deque<Node> stack = new ArrayDeque<>();
+        while(!stack.isEmpty() || root != null) {
+            while(root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if(first == null) first = root;
+            else if(last != null) {
+                last.right = root;
+                root.left = last;
+            }
+            last = root;
+            root = root.right;
+        }
         first.left = last;
         last.right = first;
         return first;
-    }
-    public void inorder(Node root) {
-        if(root == null) return;
-        inorder(root.left);
-        if(first == null) first = root;
-        else if(last != null) {
-            last.right = root;
-            root.left = last;
-        }
-        last = root;
-        inorder(root.right);
     }
 }
