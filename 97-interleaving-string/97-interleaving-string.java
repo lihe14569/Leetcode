7cabc052c739c40cdb5c82c3b287415265a989bd
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-07-06 21:57:23 -0600

    Time: 11 ms (22.70%), Space: 42.3 MB (64.59%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -3,18 +3,17 @@ class Solution {
         int m = s1.length(), n = s2.length();
         if(m + n != s3.length()) return false;
         boolean[][] dp = new boolean[m + 1][n + 1];
-        dp[0][0] = true;
-        for(int i = 1; i <= m; i++) {
-            dp[i][0] = s3.charAt(i - 1) == s1.charAt(i - 1) && dp[i - 1][0]; 
-        }
-        for(int i = 1; i <= n; i++) {
-            dp[0][i] = s3.charAt(i - 1) == s2.charAt(i - 1) && dp[0][i - 1]; 
-        }
         
-        for(int i = 1; i <= m; i++) {
-            for(int j = 1; j <= n; j++) {
-                dp[i][j] = (s1.charAt(i - 1) == s3.charAt(i + j - 1) && dp[i - 1][j]) || 
-                    (s2.charAt(j - 1) == s3.charAt(i + j - 1) && dp[i][j - 1]);
+        s1 = " " + s1;
+        s2 = " " + s2;
+        s3 = " " + s3;
+        for(int i = 0; i <= m; i++) {
+            for(int j = 0; j <= n; j++) {
+                if(i == 0 && j == 0) dp[i][j] = true;
+                else {
+                    if(i > 0 && s1.charAt(i) == s3.charAt(i + j)) dp[i][j] = dp[i - 1][j];
+                    if(j > 0 && s2.charAt(j) == s3.charAt(i + j)) dp[i][j] = dp[i][j] || dp[i][j - 1];
+                }
             }
         }
         return dp[m][n];
