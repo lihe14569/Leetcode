b7eb33da995884550a6ce2613a0e9ac4c436c337
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-07-08 23:02:22 -0600

    Time: 42 ms (81.34%), Space: 90.8 MB (49.50%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -1,23 +1,22 @@
 class Solution {
-    public int largestRectangleArea(int[] heights) {
-        //dp + monotonic stack
-        int n = heights.length;
-        Stack<Integer> stack = new Stack<>();
+    public int largestRectangleArea(int[] h) {
+        int res = 0;
+        int n = h.length;
+        Deque<Integer> stack = new ArrayDeque<>();
         stack.push(-1);
-        int area = 0;
         for(int i = 0; i < n; i++) {
-            while(stack.peek() != -1 && heights[i] < heights[stack.peek()]) {
-                int h = heights[stack.pop()];
-                int w = i - stack.peek() - 1;
-                area = Math.max(area, h * w);
+            while(stack.peek() != -1 && h[i] < h[stack.peek()]) {
+                int l = stack.pop();
+                res = Math.max(res, (i - stack.peek() - 1) * h[l]);
             }
             stack.push(i);
         }
+        //corner case: 一直是递增的序列
         while(stack.peek() != -1) {
-            int h = heights[stack.pop()];
+            int ht = h[stack.pop()];
             int w = n - stack.peek() - 1;
-            area = Math.max(area, h * w);
+            res = Math.max(res, ht * w);
         }
-        return area;
+        return res;
     }
 }
