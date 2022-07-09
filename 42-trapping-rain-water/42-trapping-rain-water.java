04691daabac038eaf65e9d873b8d340ff494a0f8
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-07-08 22:26:39 -0600

    Time: 1 ms (99.43%), Space: 48.9 MB (33.67%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,15 +1,25 @@
 class Solution {
     public int trap(int[] height) {
         int res = 0;
-        Deque<Integer> stack = new ArrayDeque<>();
-        for(int i = 0; i < height.length; i++) {
-            while(!stack.isEmpty() && height[i] > height[stack.peek()]) {
-                int low = stack.pop();
-                if(stack.isEmpty()) break;
-                int l = stack.peek();
-                res += (i - l - 1) * (Math.min(height[i], height[l]) - height[low]);
+        int n = height.length;
+        int l = 0, r = n - 1;
+        int lmax = height[0], rmax = height[n - 1];
+        while(l <= r) {
+            if(lmax <= rmax) {
+                if(height[l] <= lmax) {
+                    res += lmax - height[l];
+                } else {
+                    lmax = height[l];
+                }
+                l++;
+            } else {
+                if(height[r] <= rmax) {
+                    res += rmax - height[r];
+                } else {
+                    rmax = height[r];
+                }
+                r--;
             }
-            stack.push(i);
         }
         return res;
     }
