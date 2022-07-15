af38eeb99687fd4e5009ff036f6279923be0cd09
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-07-14 23:56:44 -0600

    Time: 1 ms (99.71%), Space: 42.5 MB (88.93%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -3,10 +3,11 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         
         for(int i = 0; i < nums.length; i++) {
-            if(map.containsKey(target - nums[i]))
+            if(map.containsKey(target - nums[i])) {
                 return new int[]{i, map.get(target - nums[i])};
+            }
             map.put(nums[i], i);
         }
-        return new int[2];
+        return new int[]{-1, -1};
     }
 }
