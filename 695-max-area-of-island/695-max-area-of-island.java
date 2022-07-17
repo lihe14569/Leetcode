08aa3eb69ef5f6c8f6d6e9356e40fdd73ed1e83f
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-07-17 02:06:46 -0600

    Time: 6 ms (25.46%), Space: 47.2 MB (54.50%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,32 +1,22 @@
 class Solution {
-    int[][] dirs = new int[][] {{1, 0}, {0, -1}, {-1, 0}, {0, 1}};
-    
     public int maxAreaOfIsland(int[][] grid) {
-        //dfs
+        int res  = 0;
         int m = grid.length, n = grid[0].length;
         boolean[][] seen = new boolean[m][n];
         
-        int res = 0;
-        for(int i = 0; i < m; i++) {
+        for(int i = 0;i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(!seen[i][j]) {
+                if(grid[i][j] == 1 && !seen[i][j]) {
                     res = Math.max(res, dfs(grid, i, j, seen));
                 }
             }
         }
         return res;
     }
-    
     public int dfs(int[][] grid, int i, int j, boolean[][] seen) {
         int m = grid.length, n = grid[0].length;
-        if(i >= m || i < 0 || j >= n || j < 0 || seen[i][j] || grid[i][j] == 0) return 0;
-        int count = 1;
+        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0 || seen[i][j]) return 0;
         seen[i][j] = true;
-        for(int[] dir : dirs) {
-            int x = i + dir[0];
-            int y = j + dir[1];
-            count += dfs(grid, x, y, seen);
-        }
-        return count;
+        return 1 + dfs(grid, i + 1, j, seen) + dfs(grid, i, j + 1, seen) + dfs(grid, i - 1, j, seen) + dfs(grid, i, j - 1, seen);
     }
 }
