04c77476451fd24b183e6cc4ea3c9c664e63d240
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-07-17 14:35:13 -0600

    Time: 52 ms (63.86%), Space: 48 MB (46.66%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,27 +1,24 @@
 class Solution {
     public String longestPalindrome(String s) {
         String res = "";
-        int l = 0, r = 0;
-        for(int i = 0; i < s.length() - 1; i++) {
-            int len1 = panlindrome(s, i, i);
-            int len2 = panlindrome(s, i, i + 1);
-            int len = Math.max(len1, len2);
-            if(len > r - l) {
-                //for odd length, (len -1) /2 is the left mid point of panlindrome, len /2 is right mid point
-                //for even length, len/2 and (len - 1) /2 is same mid point
-                l = i - (len - 1) / 2;
-                r = i + len/ 2;
-            }
-        }
-        return s.substring(l, r + 1);
-    }
-    public int panlindrome(String s, int l , int r) {
-        while(l >= 0 && r < s.length()) {
-            if(s.charAt(l) == s.charAt(r)) {
+        
+        for(int i = 0; i < s.length(); i++) {
+            int l = i - 1, r = i + 1;
+            while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
                 l--;
                 r++;
-            } else break;
+            }
+            if(res.length() < r - l - 1) 
+                res = s.substring(l + 1, r);
+            l = i;
+            r = i + 1;
+            while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+                    l--;
+                    r++;
+            }
+            if(res.length() < r - l - 1) 
+                res = s.substring(l + 1, r);
         }
-        return r - l - 1;
+        return res;
     }
 }
