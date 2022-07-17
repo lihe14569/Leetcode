aa64a37391155b46158af4b977766870c6f4fd1f
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-07-17 16:50:28 -0600

    Time: 21 ms (18.10%), Space: 46 MB (17.18%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,12 +1,17 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        //判断特殊情况
-        if(x < 0 || (x != 0 && x % 10 == 0)) return false;
-        int reverse = 0;
-        while(x > reverse) {
-            reverse = reverse * 10 + x % 10;
-            x /= 10;
+        if(x < 0) return false;
+        String s = String.valueOf(x);
+        return isPanlin(s);
+    }
+    public boolean isPanlin(String s) {
+        int n = s.length();
+        int i = (n - 1) / 2, j = n / 2;
+        while(i >= 0 && j < n ) {
+            if(s.charAt(i) != s.charAt(j)) return false;
+            i--;
+            j++;
         }
-        return (reverse == x) || (reverse / 10 == x);
+        return true; 
     }
 }
