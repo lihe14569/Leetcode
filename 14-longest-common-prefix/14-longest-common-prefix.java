cfe1fe2156d6bf52e72d01f7498bbc9cf47a477f
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-07-18 21:14:38 -0600

    Time: 1 ms (88.54%), Space: 42.5 MB (29.17%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -1,53 +1,18 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        Trie trie = new Trie();
-        for(String s : strs) {
-            trie.insert(s);
-        }
-        return trie.findPrefix();
-    }
-    class Node {
-        Node[] children;
-        int cnt;
-        boolean isWord;
-        Node() {
-            children = new Node[26];
-            cnt = 0;
-            isWord = false;
-        }
-    }
-    class Trie {
-        Node root;
-        Trie() {
-            root = new Node();
-        }
-        public void insert(String w) {
-            Node node = root;
-            for(int i = 0; i < w.length(); i++) {
-                if(node.children[w.charAt(i) - 'a'] == null) {
-                    node.children[w.charAt(i) - 'a'] = new Node();
-                    node.cnt++;
-                }
-                node = node.children[w.charAt(i) - 'a'];
-            }
-            node.isWord = true;
-        }
-        public String findPrefix() {
-            Node node = root;
-            StringBuilder sb = new StringBuilder();
-            sb.append("");
-            while(node.cnt == 1) {
-                if(node.isWord) return sb.toString();
-                for(int i =0; i < 26; i++) {
-                    if(node.children[i] != null) {
-                        sb.append((char)('a' + i));
-                        node = node.children[i];
-                        break;
-                    }
-                }
-            }
-            return sb.toString();
-        }
+        if(strs == null || strs.length == 0) return "";
         
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; ; i++) {
+            int len = strs[0].length();
+            if(i >= len) break;
+            char c = strs[0].charAt(i);
+            for(int j = 0; j < strs.length; j++) {
+                if(strs[j].length() <= i || c != strs[j].charAt(i)) return sb.toString();
+            }
+            sb.append(c);
+        }
+        return sb.toString();
     }
 }
