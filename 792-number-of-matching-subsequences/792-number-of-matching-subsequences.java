2d3750a858c840b7d1f641134ef3d5e7403f5f9f
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-07-19 23:07:55 -0600

    Time: 172 ms (46.53%), Space: 118.8 MB (9.40%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.java
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.java
@@ -1,37 +1,37 @@
 class Solution {
-    public int numMatchingSubseq(String S, String[] words) {
-        int ans = 0;
-        ArrayList<Node>[] heads = new ArrayList[26];
-        for (int i = 0; i < 26; ++i)
-            heads[i] = new ArrayList<Node>();
-
-        for (String word: words)
-            heads[word.charAt(0) - 'a'].add(new Node(word, 0));
-
-        for (char c: S.toCharArray()) {
-            ArrayList<Node> old_bucket = heads[c - 'a'];
-            heads[c - 'a'] = new ArrayList<Node>();
-
-            for (Node node: old_bucket) {
-                node.index++;
-                if (node.index == node.word.length()) {
-                    ans++;
-                } else {
-                    heads[node.word.charAt(node.index) - 'a'].add(node);
+    public int numMatchingSubseq(String s, String[] words) {
+        List<Pair>[] buckets = new ArrayList[26];
+        // Arrays.fill(buckets, new ArrayList<Pair>());
+        
+        for(int i= 0; i < 26; i++) buckets[i] = new ArrayList<>();
+        for(int i = 0; i < words.length; i++) {
+            buckets[words[i].charAt(0) - 'a'].add(new Pair(words[i], 0));
+        }
+        
+        int res = 0;
+        for(char c : s.toCharArray()) {
+            List<Pair> lst = buckets[c - 'a'];
+            List<Pair> buff = new ArrayList<>();
+            for(Pair p : lst) {
+                if(p.i + 1 == p.w.length()) res++;
+                else {
+                    buff.add(new Pair(p.w, p.i + 1));
                 }
             }
-            old_bucket.clear();
+            lst.clear();
+            for(Pair p : buff) {
+                buckets[p.w.charAt(p.i) - 'a'].add(p);
+            }
         }
-        return ans;
+        return res;
+        
     }
-
-}
-
-class Node {
-    String word;
-    int index;
-    public Node(String w, int i) {
-        word = w;
-        index = i;
+    class Pair {
+        String w;
+        int i;
+        public Pair(String w, int i) {
+            this.w = w;
+            this.i = i;
+        }
     }
 }
