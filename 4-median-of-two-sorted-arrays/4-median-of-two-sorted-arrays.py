0d7e81e42fe0c8798d9670db19aa1e2dca1f588a
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-07-23 14:28:30 -0600

    Time: 146 ms (50.92%), Space: 14.2 MB (25.44%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -1,31 +1,30 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         m, n = len(nums1), len(nums2)
-        
         tot = m + n
+        def find(nums1, i, nums2, j, k):
+            if len(nums1) - i > len(nums2) - j:
+                return find(nums2, j, nums1, i, k)
+            
+            if k == 1:
+                if len(nums1) == i:
+                    return nums2[j]
+                else:
+                    return min(nums1[i], nums2[j])
+            
+            if len(nums1) == i:
+                return nums2[j + k - 1]
+            
+            si = min(len(nums1), i + k // 2)
+            sj = j + k // 2
+            if nums1[si - 1] < nums2[sj - 1]:
+                return find(nums1, si, nums2, j, k - (si - i))
+            else:
+                return find(nums1, i, nums2, sj, k - (sj - j))
+            
         if tot % 2 == 0:
-            left = self.findM(nums1, 0, nums2, 0, tot // 2)
-            right = self.findM(nums1, 0, nums2, 0, tot // 2 + 1)
-            return  (left + right) / 2
+            left = find(nums1, 0, nums2, 0, tot // 2)
+            right = find(nums1, 0, nums2, 0, tot // 2 + 1)
+            return (left + right) / 2
         else:
-            return self.findM(nums1, 0, nums2, 0, tot // 2 + 1)
-        
-    def findM(self, nums1, i, nums2, j, k):
-        if(len(nums1) - i > len(nums2) - j):
-            return self.findM(nums2, j, nums1, i, k)
-        
-        if k == 1:
-            if i == len(nums1): return nums2[j]
-            else: return min(nums1[i], nums2[j])
-        
-        if i == len(nums1):
-            return nums2[j + k - 1]
-        
-        si = min(len(nums1), i + k // 2)
-        sj = j + k - k // 2 
-        
-        if(nums1[si - 1] > nums2[sj - 1]):
-            return self.findM(nums1, i, nums2, sj, k - (sj - j))
-        else:
-            return self.findM(nums1, si, nums2, j, k - (si- i))
-    
+            return find(nums1, 0, nums2, 0, tot // 2 + 1)
