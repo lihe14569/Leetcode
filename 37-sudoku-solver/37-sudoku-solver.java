0c3a4ba5e6cc394d58b2b95f29e29d4bef3fe5a0
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-07-22 22:19:35 -0600

    Time: 4 ms (97.92%), Space: 39.5 MB (96.98%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.java
+++ 37-sudoku-solver/37-sudoku-solver.java
@@ -2,39 +2,41 @@ class Solution {
     public void solveSudoku(char[][] board) {
         boolean[][] row = new boolean[9][10];
         boolean[][] col = new boolean[9][10];
-        boolean[][] block = new boolean[9][10];
+        boolean[][] blk = new boolean[9][10];
         
         for(int i = 0; i < 9; i++) {
             for(int j = 0; j < 9; j++) {
                 if(board[i][j] != '.') {
-                    int bi = 3* (i / 3) + j /3;
-                    int num = board[i][j] - '0';
-                    row[i][num] = true;
-                    col[j][num] = true;
-                    block[bi][num] = true;
+                    int idx = board[i][j] - '0';
+                    int bi = 3 * (i / 3) + j / 3;
+                    row[i][idx] = true;
+                    col[j][idx] = true;
+                    blk[bi][idx] = true;
                 }
             }
         }
-        fill(0, 0, row, col, block, board);
+        backtrack(0, 0, row, col, blk, board);
     }
-    
-    public boolean fill(int r, int c, boolean[][] row, boolean[][] col, boolean[][] block, char[][] board) {
+    public boolean backtrack(int r, int c, boolean[][] row, boolean[][] col, boolean[][] blk, char[][] board) {
         if(r == 9) return true;
         int nc = (c + 1) % 9;
         int nr = nc == 0 ? r + 1 : r;
-        if(board[r][c] != '.') return fill(nr, nc, row, col, block, board);
+        
+        if(board[r][c] != '.')  return backtrack(nr, nc, row, col, blk, board);
+        
         for(int i = 1; i <= 9; i++) {
             int bi = 3 * (r / 3) + c / 3;
-            if(!row[r][i] && !col[c][i] && !block[bi][i]) {
-                row[r][i] = true;
-                col[c][i] = true;
-                block[bi][i] = true;
+            
+            if(!row[r][i] && !col[c][i] && !blk[bi][i]) {
                 board[r][c] = (char)(i + '0');
-                if(fill(nr, nc, row, col, block, board)) return true;
+                row[r][i] = true;
+                col[c][i]= true;
+                blk[bi][i] = true;
+                if(backtrack(nr, nc, row, col, blk, board)) return true;
+                board[r][c] = '.';
                 row[r][i] = false;
                 col[c][i] = false;
-                block[bi][i] = false;
-                board[r][c] = '.';
+                blk[bi][i] = false;
             }
         }
         return false;
