d1fb123915ba596ea472aa21af490a0cd57390f6
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-07-25 21:16:59 -0600

    Time: 600 ms (63.03%), Space: 61.2 MB (87.84%) - LeetHub

--- 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
@@ -1,40 +1,35 @@
 from random import choice
-class RandomizedSet():
-    def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.dict = {}
-        self.list = []
+class RandomizedSet:
 
-        
+    def __init__(self):
+        self.dict = {}
+        self.lst = []
+
     def insert(self, val: int) -> bool:
-        """
-        Inserts a value to the set. Returns true if the set did not already contain the specified element.
-        """
         if val in self.dict:
             return False
-        self.dict[val] = len(self.list)
-        self.list.append(val)
+        self.dict[val] = len(self.lst)
+        self.lst.append(val)
         return True
         
 
     def remove(self, val: int) -> bool:
-        """
-        Removes a value from the set. Returns true if the set contained the specified element.
-        """
         if val in self.dict:
-            # move the last element to the place idx of the element to delete
-            last_element, idx = self.list[-1], self.dict[val]
-            self.list[idx], self.dict[last_element] = last_element, idx
-            # delete the last element
-            self.list.pop()
+            lst_val = self.lst[-1]
+            val_idx = self.dict[val]
+            self.lst[val_idx] = lst_val
+            self.dict[lst_val] = val_idx
+            self.lst.pop()
             del self.dict[val]
             return True
         return False
 
     def getRandom(self) -> int:
-        """
-        Get a random element from the set.
-        """
-        return choice(self.list)
+        return choice(self.lst)
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
