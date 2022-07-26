1c063d4d368c6b84229379fd0e1356e298ee4b05
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-07-25 20:26:31 -0600

    Time: 40 ms (97.37%), Space: 13.9 MB (68.66%) - LeetHub

--- 1274-number-of-ships-in-a-rectangle/1274-number-of-ships-in-a-rectangle.py
+++ 1274-number-of-ships-in-a-rectangle/1274-number-of-ships-in-a-rectangle.py
@@ -1,19 +1,25 @@
+# """
+# This is Sea's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class Sea:
+#    def hasShips(self, topRight: 'Point', bottomLeft: 'Point') -> bool:
+#
+#class Point:
+#	def __init__(self, x: int, y: int):
+#		self.x = x
+#		self.y = y
+
 class Solution:
-    def countShips(self, sea: 'Sea', topRight: 'Point', bottomLeft: 'Point') -> int:
-        # If the current rectangle does not contain any ships, return 0.         
-        if (bottomLeft.x > topRight.x) or (bottomLeft.y > topRight.y):
-            return 0
-        if not sea.hasShips(topRight, bottomLeft):
-            return 0
-
-        # If the rectangle represents a single point, a ship is located.
-        if (topRight.x == bottomLeft.x) and (topRight.y == bottomLeft.y):
+    def countShips(self, sea: 'Sea', tr: 'Point', bl: 'Point') -> int:
+        if bl.x > tr.x or bl.y > tr.y: return 0
+        if not sea.hasShips(tr, bl): return 0
+        if bl.x == tr.x and bl.y == tr.y:
             return 1
-
-        # Recursively check each of the 4 sub-rectangles for ships.
-        mid_x = (topRight.x + bottomLeft.x) // 2
-        mid_y = (topRight.y + bottomLeft.y) // 2
-        return self.countShips(sea, Point(mid_x, mid_y), bottomLeft) + \
-               self.countShips(sea, topRight, Point(mid_x + 1, mid_y + 1)) + \
-               self.countShips(sea, Point(mid_x, topRight.y), Point(bottomLeft.x, mid_y + 1)) + \
-               self.countShips(sea, Point(topRight.x, mid_y), Point(mid_x + 1, bottomLeft.y))
+        midx = (bl.x + tr.x) // 2
+        midy = (bl.y + tr.y) // 2
+        
+        return self.countShips(sea, tr, Point(midx + 1, midy + 1)) + \
+               self.countShips(sea, Point(midx, midy), bl) + \
+               self.countShips(sea, Point(midx, tr.y), Point(bl.x, midy + 1)) + \
+               self.countShips(sea, Point(tr.x, midy), Point(midx + 1, bl.y))
