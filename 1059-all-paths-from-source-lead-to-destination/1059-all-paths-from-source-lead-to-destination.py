3c72c4d5172e2d039b38502c918cd7a7d3bbb135
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-07-26 23:47:14 -0600

    Time: 416 ms (51.58%), Space: 19.3 MB (81.87%) - LeetHub

---
+++ 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.py
@@ -0,0 +1,24 @@
+class Solution:
+    def leadsToDestination(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        graph = self.buildGraph(edges)
+        seen = [0] * n
+        return self.dfs(graph, source, destination, seen)
+    
+    def dfs(self, graph, source, destination, seen):
+        #base case
+        if source not in graph:
+            return source == destination
+        seen[source] = 1
+        for next in graph[source]:
+            if seen[next] == 1: return False
+            if seen[next] == 0 and not self.dfs(graph, next, destination, seen): return False
+        seen[source] = 2
+        return True
+        
+    def buildGraph(self, edges):
+        graph = {}
+        for u, v in edges:
+            if u not in graph:
+                graph[u] = []
+            graph[u].append(v)
+        return graph
