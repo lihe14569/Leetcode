8f2edfd82baa44f84cba60287d64870880e2ae84
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-07-27 22:45:50 -0600

    Time: 3 ms (62.05%), Space: 46 MB (48.86%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -1,19 +1,17 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int[][] dp = new int[grid.length][grid[0].length];
-        for(int i = grid.length - 1; i >=0; i--) {
-            for(int j = grid[0].length - 1; j >= 0; j--) {
-               if(i == grid.length - 1 && j != grid[0].length -1) {
-                   dp[i][j] = grid[i][j] + dp[i][j + 1];
-               } else if(j == grid[0].length - 1 && i != grid.length - 1) {
-                   dp[i][j] = grid[i][j] + dp[i + 1][j];
-               } else if (i != grid.length - 1 && j != grid[0].length - 1) {
-                   dp[i][j] = grid[i][j] + Math.min(dp[i][j + 1], dp[i + 1][j]);
-               } else {
-                   dp[i][j] = grid[i][j];
-               }
+        int m = grid.length, n = grid[0].length;
+        int[][] dp = new int[m][n];
+        
+        dp[0][0] = grid[0][0];
+        for(int i = 1; i < n; i++) dp[0][i] = dp[0][i - 1] + grid[0][i];
+        for(int i = 1; i < m; i++) dp[i][0] = dp[i - 1][0] + grid[i][0];
+        
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
             }
         }
-        return dp[0][0];
+        return dp[m - 1][n - 1];
     }
 }
