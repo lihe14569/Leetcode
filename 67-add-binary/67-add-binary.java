4c9a6955e75e8257ac492b95f7c6262c708c89ac
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-07-28 20:36:06 -0600

    Time: 2 ms (90.84%), Space: 43.2 MB (34.08%) - LeetHub

--- 67-add-binary/67-add-binary.java
+++ 67-add-binary/67-add-binary.java
@@ -1,18 +1,53 @@
 class Solution {
-    public String addBinary(String a, String b) {
-        StringBuilder sb = new StringBuilder();
-        int m = a.length(), n = b.length();
-        int carry = 0;
-        int i = m - 1, j = n - 1;
-        
-        while(i >= 0 || j >= 0 || carry != 0) {
-            int c1 = i >= 0 ? a.charAt(i--) - '0' : 0;
-            int c2 = j >= 0 ? b.charAt(j--) - '0' : 0;
-            int curr = (c1 + c2 + carry)  % 2;
-            sb.append(curr);
-            carry = (c1 + c2 + carry) / 2;
-            
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> res = new ArrayList<>();
+        int left = 0;
+        while(left < words.length) {
+            int right = findRight(words, left, maxWidth);
+            String line = justify(words, left, right, maxWidth);
+            res.add(line);
+            left = right + 1;
         }
-        return sb.reverse().toString();
+        return res;
+    }
+     public int findRight(String[] words, int left, int maxWidth) {
+         int right = left;
+         int sum = words[right++].length();
+         while(right < words.length && sum + words[right].length() + 1 < maxWidth) {
+             sum += words[right++].length();
+         }
+         return right - 1;
+     }
+     public String justify(String[] words, int left, int right, int maxWidth) {
+        //corner case: single word
+        if(left == right) return padWords(words[left], maxWidth);
+        int numSpaces = right - left;
+        int totalSpaces = maxWidth - wordLength(words, left, right);
+        
+        boolean isLastLine = right == words.length - 1;
+        int avgSpaces = isLastLine ? 1 : totalSpaces / numSpaces;
+        int mod = isLastLine ? 0 : totalSpaces % numSpaces;
+        
+        StringBuilder line = new StringBuilder();
+        for(int i = left; i <= right; i++) {
+            line.append(words[i])
+                .append(blank(avgSpaces))
+                .append(mod-- > 0 ? " " : "");
+        }
+        
+        return padWords(line.toString().trim(), maxWidth);
+    }
+    public int wordLength(String[] words, int left, int right) {
+        int sum = 0;
+        for(int i = left; i <= right; i++) {
+            sum += words[i].length();
+        }
+        return sum;
+    }
+    public String padWords(String word, int maxWidth) {
+        return word + blank(maxWidth - word.length());
+    }
+    public String blank(int length) {
+        return new String(new char[length]).replace('\0', ' ');
     }
 }
