5d708d59ec1aef485a014609a25d855f8aa3b092
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-07-30 15:47:53 -0600

    Time: 0 ms (100.00%), Space: 41.9 MB (91.21%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,17 +1,15 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length, n = matrix[0].length;
-        
-        int i = 0, j = n - 1;
-        while(i < m && j >= 0) {
-            if(matrix[i][j] == target) {
-                return true;
-            } else if(matrix[i][j] < target) {
-                i++;
-            } else {
-                j--;
-            }
+        int l = 0, r = m * n  - 1;
+        while(l < r) {
+            int mid = l + (r - l) / 2;
+            if(matrix[mid/n][mid % n] == target) return true;
+            else if(matrix[mid / n][mid % n] < target) 
+                l = mid + 1;
+            else 
+                r = mid;
         }
-        return false;
+        return matrix[l/n][l%n] == target;
     }
 }
