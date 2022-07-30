f18c4720cede8bc138736a9b64ebf2c600cd34a4
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-07-29 23:31:28 -0600

    Time: 2 ms (84.05%), Space: 41.4 MB (52.24%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,16 +1,13 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x == 0 || x== 1) return x;
+        
+        if(x < 2) return x;
         int l = 1, r = x;
         while(l < r) {
-            int m = l + (r - l + 1) / 2;
-            if(m <= x / m) {
-                 l = m;
-            } else {
-                r = m - 1;
-            }
+            int mid = l + (r - l + 1) / 2;
+            if(mid <= x / mid) l = mid;
+            else r = mid - 1;
         }
-        if(l * l > x) l--;
         return l;
     }
 }
