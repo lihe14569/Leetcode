cd1e987e30df18fe3731f5edc5690a7eb3394b86
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-08-01 01:34:43 -0600

    Time: 0 ms (100.00%), Space: 43.5 MB (91.34%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -10,32 +10,7 @@
  * }
  */
 public class Solution {
-    // public boolean hasCycle(ListNode head) {
-    //     if(head == null || heade.next == null) return false;
-    //     ListNode slow = head, fast = head;
-    //     slow = slow.next;
-    //     fast = fast.next.next;
-    //     while(fast != slow) {
-    //         slow = slow.next;
-    //         fast = fasst.next.next;
-    //     }
-    //     slow = head;
-    //     while(fast != slow) {
-    //         slow = slow.next;
-    //         fast = fast.next;
-    //     }
-    //     return fast;
-    //     //----------
-    //     //       |   \
-    //     //      |     \
-    //     //      \ _ c_ / 
-    //     //       
-    //     // 2(f + a) = f + a + nC
-    //     // F+ a = nC
-    //     //
-    // }
     public boolean hasCycle(ListNode head) {
-        if(head == null || head.next == null) return false;
         ListNode slow = head, fast = head;
         while(fast != null && fast.next != null) {
             slow = slow.next;
