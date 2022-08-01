122ad44ec20fc458edfc8bedcabec32584bfa00c
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-08-01 01:30:39 -0600

    Time: 34 ms (80.92%), Space: 67.6 MB (48.52%) - LeetHub

--- 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.java
+++ 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.java
@@ -1,17 +1,17 @@
 class Solution {
     public long numberOfWays(String s) {
-        long one = 0, zero = 0, oneZero = 0, zeroOne = 0, ways = 0;
-        for (int i = 0; i < s.length(); ++i) {
-            if (s.charAt(i) == '0') {
-                ++zero;
-                oneZero += one; // Count in '10'.
-                ways += zeroOne; // Count in '010'.
-            }else {
-                ++one;
-                zeroOne += zero; // Count in '01'.
-                ways += oneZero; // Count in '101'.
+        long zero = 0, one = 0, zeroOne = 0, oneZero = 0, res = 0;
+        for(char c : s.toCharArray()) {
+            if(c == '0') {
+                zero++;
+                oneZero += one;
+                res += zeroOne;
+            } else {
+                one++;
+                zeroOne += zero;
+                res += oneZero;
             }
         }
-        return ways;
+        return res;
     }
 }
