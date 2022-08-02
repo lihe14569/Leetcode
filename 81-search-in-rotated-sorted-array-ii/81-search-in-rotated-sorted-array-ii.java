1b39a9b658b365982c62f223711050f075fe4234
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-08-01 23:38:55 -0600

    Time: 1 ms (86.92%), Space: 44.1 MB (23.90%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -1,32 +1,33 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        int l = 0, h = nums.length - 1;
-        while(l < h) {
-            int m = l + (h - l) / 2;
-            if(nums[m] < nums[h])
-                h = m;
-            else if(nums[m] > nums[h])
+        if(nums == null) return false;
+        if(nums.length == 1 && nums[0] == target) return true;
+        int left = 0;
+        while(left < nums.length && nums[left] == nums[nums.length - 1]) left++;
+        if(left >= nums.length) return nums[0] == target;
+        
+        int l = left, r = nums.length - 1;
+    
+        while(l < r) {
+            int m = l + (r - l) / 2;
+            if(nums[m] <= nums[r]) {
+                r= m;
+            } else {
                 l = m + 1;
-            else {
-                if(nums[h - 1] > nums[h]) {
-                    l = h;
-                    break;
-                }
-                h--; 
             }
         }
-        int rot = l;
-        l = 0;
-        h = nums.length -1;
-        while(l <= h) {
-            int m = l + (h - l) / 2;
-            int realMid = (m + rot) % nums.length;
-            if(nums[realMid] == target) return true;
-            else if(nums[realMid] < target)
-                l = m + 1;
-            else 
-                h = m - 1;
+        
+        if(target > nums[nums.length - 1]) {
+            r = l;
+            l = left;
+        } else {
+            r = nums.length - 1;
         }
-        return false;
+        while(l < r) {
+            int m = l +(r - l) / 2;
+            if(nums[m] >= target) r = m;
+            else l = m + 1;
+        }
+        return nums[l] == target ? true : false;
     }
 }
