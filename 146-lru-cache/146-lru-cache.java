65d8b230201821ab98ab21a8f9527f24f430a30c
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-08-04 23:49:15 -0600

    Time: 110 ms (33.87%), Space: 126 MB (53.77%) - LeetHub

--- 146-lru-cache/146-lru-cache.java
+++ 146-lru-cache/146-lru-cache.java
@@ -1,78 +1,74 @@
 class LRUCache {
-    Map<Integer, Node> map;
-    Node head, tail;
-    int size, capacity;
+    class DLLNode {
+        int key, val;
+        DLLNode prev, next;
+        public DLLNode(){};
+        public DLLNode(int key, int val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+    
+    int size;
+    int capacity;
+    DLLNode head, tail;
+    Map<Integer, DLLNode> cache;
     
     public LRUCache(int capacity) {
-        map = new HashMap<>();
-        head = new Node();
-        tail = new Node();
+        this.size = 0;
+        this.capacity = capacity;
+        head = new DLLNode();
+        tail = new DLLNode();
         head.next = tail;
         tail.prev = head;
-        size = 0;
-        this.capacity = capacity;
+        cache = new HashMap<>();
     }
     
     public int get(int key) {
-        if(map.containsKey(key)) {
-            Node node = map.get(key);
+        if(cache.containsKey(key)) {
+            DLLNode node = cache.get(key);
             moveToHead(node);
             return node.val;
-        } 
+        }
         return -1;
     }
-    
-    public void moveToHead(Node node) {
-        remove(node);
+    public void moveToHead(DLLNode node) {
+        removeNode(node);
         addToHead(node);
     }
-    
-    public void remove(Node node) {
+    public void removeNode(DLLNode node) {
         node.prev.next = node.next;
         node.next.prev = node.prev;
     }
     
-     public void addToHead(Node node) {
-        node.next = head.next;
-        node.prev = head;
-        head.next.prev = node;
-        head.next = node;
-    }
-    
-    public Node removeTail() {
-        Node node = tail.prev;
-        remove(node);
-        return node;
-    }
-    
     public void put(int key, int value) {
-        //1. exist, update value, move to head
-        //2. not exist, add node to head, update map. if exceeds the capacity, evict last node
-        if(map.containsKey(key)) {
-            Node node = map.get(key);
+        if(cache.containsKey(key)) {
+            DLLNode node = cache.get(key);
             node.val = value;
             moveToHead(node);
+            return;
         } else {
-            Node node = new Node(key, value);
+            DLLNode node = new DLLNode(key, value);
+            cache.put(key, node);
             addToHead(node);
-            map.put(key, node);
             size++;
             if(size > capacity) {
-                Node rmNode = removeTail();
-                map.remove(rmNode.key);
+                DLLNode lastNode = removeTail();
+                cache.remove(lastNode.key);
                 size--;
             }
         }
     }
-   
-}
-class Node {
-    int key, val;
-    Node next, prev;
-    public Node(){};
-    public Node(int key, int val) {
-        this.key = key;
-        this.val = val;
+    public DLLNode removeTail() {
+        DLLNode node = tail.prev;
+        removeNode(node);
+        return node;
+    }
+    public void addToHead(DLLNode node) {
+        node.next = head.next;
+        node.prev = head;
+        node.next.prev = node;
+        head.next = node;
     }
 }
 
