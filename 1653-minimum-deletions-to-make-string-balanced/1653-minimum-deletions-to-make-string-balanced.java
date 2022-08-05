46bb2c7cc684c9540509401e14b5cb8bb95688d1
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-08-04 23:19:45 -0600

    Time: 47 ms (53.66%), Space: 67.6 MB (29.20%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -1,24 +1,15 @@
 class Solution {
     public int minimumDeletions(String s) {
-        int l = s.length();
-		//dp stores number of chars to remove to make s.substring(0, i) valid
-        int[] dp = new int[l + 1];
         int bcount = 0;
-        for (int i = 0; i < l; i++) {
-            if (s.charAt(i) == 'a') {
-                //case 1: keep current a. ==> prev chars must be a...a
-                //so need to remove all 'b' chars before i, which is bcount
-                
-                //case 2: remove current a ==> prev chars must be a...ab...b
-                //so need to remove current a and whatever makes substring before current i valid which is dp[i];
-                dp[i + 1] = Math.min(dp[i] + 1, bcount);
+        int[] dp = new int[s.length() + 1];
+        for(int i = 1; i <= s.length(); i++) {
+            if(s.charAt(i - 1) == 'a') {
+                dp[i] = Math.min(dp[i- 1] + 1, bcount);
             } else {
-                //since it is always valid to append 'b' if substring before current i is valid, so just copy whatever makes substring before i valid which is dp[i];
-                dp[i + 1] = dp[i];
+                dp[i] = dp[i - 1];
                 bcount++;
             }
         }
-        
-        return dp[l];
+        return dp[s.length()];
     }
 }
