8124ffb2613cd8f4e7e5e45c7274225107e19f79
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-08-05 23:55:06 -0600

    Time: 16 ms (72.99%), Space: 54.3 MB (65.97%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,33 +1,19 @@
 class Solution {
     public int minDeletions(String s) {
-        // Store the frequency of each character
-        int[] frequency = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            frequency[s.charAt(i) - 'a']++;
+        int[] freq = new int[26];
+        for(int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
         }
-        
-        // Add the frequencies to the priority queue
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for (int i = 0; i < 26; i++) {
-            if (frequency[i] > 0) {
-                pq.add(frequency[i]);
+        int cnt = 0;
+        Arrays.sort(freq);
+        int maxFreq = s.length();
+        for(int i = 25; i >= 0 && freq[i] > 0; i--) {
+            if(freq[i] > maxFreq) {
+                cnt += freq[i] - maxFreq;
+                freq[i] = maxFreq;    
             }
+            maxFreq = Math.max(freq[i] - 1, 0);
         }
-        
-        int deleteCount = 0;
-        while (pq.size() > 1) {
-            int topElement  = pq.remove();
-            
-            // If the top two elements in the priority queue are the same
-            if (topElement == pq.peek()) {
-                // Decrement the popped value and push it back into the queue
-                if (topElement - 1 > 0) {
-                    pq.add(topElement - 1);
-                }
-                deleteCount++;
-            }
-        }
-        
-        return deleteCount;
+        return cnt;
     }
 }
