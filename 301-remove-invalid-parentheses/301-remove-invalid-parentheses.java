016977dc669c12863e9fe0e7adacfd5cfd66357c
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-08-08 01:34:06 -0600

    Time: 153 ms (39.89%), Space: 43.2 MB (59.41%) - LeetHub

---
+++ 301-remove-invalid-parentheses/301-remove-invalid-parentheses.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<String> removeInvalidParentheses(String s) {
+        int rmL = 0, rmR= 0;
+        int n = s.length();
+        for(int i = 0; i < n; i++) {
+            if(s.charAt(i) == '(')
+                rmL++;
+            else if(s.charAt(i) == ')') {
+                if(rmL > 0) rmL--;
+                else rmR++;
+            }
+        }
+        Set<String> res = new HashSet<>();
+        helper(s, res, 0, rmL, rmR, 0, new StringBuilder());
+        return new ArrayList<>(res);
+    }
+    public void helper(String s, Set<String> res, int i, int rmL, int rmR, int open, StringBuilder sb) {
+        //base case
+        if(rmL  < 0 || rmR < 0 || open < 0 || i > s.length()) return;
+        if(i == s.length()) {
+            if(rmL == 0 && rmR == 0 && open == 0) {
+                res.add(sb.toString());
+            }
+            return;//不管是不是答案，都要return，因为已经遍历完了
+        }
+        int len = sb.length();
+        char c = s.charAt(i);
+
+        if(c == '(') {
+            helper(s, res, i + 1, rmL - 1, rmR, open, sb); //not use this parentheses
+            helper(s, res, i + 1, rmL, rmR, open + 1, sb.append(c)); //use this parentheses
+        } else if(c == ')') {
+            helper(s, res, i + 1, rmL, rmR - 1, open, sb); //not use this parentheses
+            helper(s, res, i + 1, rmL, rmR, open - 1, sb.append(c)); // use this parentheses
+        } else {
+            helper(s, res, i + 1, rmL, rmR, open, sb.append(c)); 
+        }
+        sb.setLength(len);
+    }
+}
