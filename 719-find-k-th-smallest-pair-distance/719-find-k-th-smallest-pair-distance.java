61cb29b5ba0fef6f45c83341c895eb012be5ec03
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-08-10 00:42:27 -0600

    Time: 304 ms (11.97%), Space: 49.3 MB (5.79%) - LeetHub

--- 719-find-k-th-smallest-pair-distance/719-find-k-th-smallest-pair-distance.java
+++ 719-find-k-th-smallest-pair-distance/719-find-k-th-smallest-pair-distance.java
@@ -1,36 +1,22 @@
 class Solution {
     public int smallestDistancePair(int[] nums, int k) {
+        //sort
         Arrays.sort(nums);
-        int WIDTH = 2 * nums[nums.length - 1];
-
-        //multiplicity[i] = number of nums[j] == nums[i] (j < i)
-        int[] multiplicity = new int[nums.length];
-        for (int i = 1; i < nums.length; ++i) {
-            if (nums[i] == nums[i-1]) {
-                multiplicity[i] = 1 + multiplicity[i - 1];
+        int N = nums[nums.length - 1];
+        
+        int n = nums.length;
+        int[] buckets = new int[N + 1];
+        
+        //put all pairs and its distance into buckets
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                buckets[nums[j] - nums[i]]++;
             }
         }
-
-        //prefix[v] = number of values <= v
-        int[] prefix = new int[WIDTH];
-        int left = 0;
-        for (int i = 0; i < WIDTH; ++i) {
-            while (left < nums.length && nums[left] == i) left++;
-            prefix[i] = left;
+        for(int i = 0; i <= N; i++) {
+            k -= buckets[i];
+            if(k <= 0) return i;
         }
-
-        int lo = 0;
-        int hi = nums[nums.length - 1] - nums[0];
-        while (lo < hi) {
-            int mi = (lo + hi) / 2;
-            int count = 0;
-            for (int i = 0; i < nums.length; ++i) {
-                count += prefix[nums[i] + mi] - prefix[nums[i]] + multiplicity[i];
-            }
-            //count = number of pairs with distance <= mi
-            if (count >= k) hi = mi;
-            else lo = mi + 1;
-        }
-        return lo;
+        return 0;
     }
 }
