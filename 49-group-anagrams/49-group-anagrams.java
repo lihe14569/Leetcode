5790c809af6e756bf37c9d95eaaf561569e566da
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-08-13 01:46:47 -0600

    Time: 33 ms (21.80%), Space: 59.2 MB (21.45%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,19 +1,21 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> res = new ArrayList<>();
-        Map<String, List<String>> map = new HashMap<>();
-        
-        for(String s : strs) {
-            char[] chrs = s.toCharArray();
-            Arrays.sort(chrs);
-            String str = String.valueOf(chrs);
-            System.out.println(str);
-            if(!map.containsKey(str)) map.put(str, new ArrayList<>());
-            map.get(str).add(s);
+        if (strs.length == 0) return new ArrayList();
+        Map<String, List> ans = new HashMap<String, List>();
+        int[] count = new int[26];
+        for (String s : strs) {
+            Arrays.fill(count, 0);
+            for (char c : s.toCharArray()) count[c - 'a']++;
+
+            StringBuilder sb = new StringBuilder("");
+            for (int i = 0; i < 26; i++) {
+                sb.append('#');
+                sb.append(count[i]);
+            }
+            String key = sb.toString();
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList());
+            ans.get(key).add(s);
         }
-        for(List<String> lst : map.values()) {
-            res.add(lst);
-        }
-        return res;
+        return new ArrayList(ans.values());
     }
 }
