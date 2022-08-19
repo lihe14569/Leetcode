e0eb05b57d449cb9204ca92d43325ca7e4515fd9
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-08-18 22:01:14 -0600

    Time: 3 ms (7.48%), Space: 42 MB (95.96%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -1,33 +1,33 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        if(nums == null) return false;
-        if(nums.length == 1 && nums[0] == target) return true;
-        int left = 0;
-        while(left < nums.length && nums[left] == nums[nums.length - 1]) left++;
-        if(left >= nums.length) return nums[0] == target;
+        //预处理：移除右端于最左端相同的点
+        //1.找到初始点
+        //2.判断在哪一段
+        if(nums == null || nums.length == 0) return false;
+        int R = nums.length - 1;
+        while(R >= 0 && nums[R] == nums[0]) R--;
+        if(R < 0) return nums[0] == target;
         
-        int l = left, r = nums.length - 1;
-    
+        int l = 0, r = R;
+        while(l < r) {
+            int m = l + (r - l + 1) / 2;
+            if(nums[m] >= nums[0]) l = m;
+            else r = m - 1;
+        }
+        System.out.println(l);
+        if(target >= nums[0]) {
+            r = l;
+            l = 0;
+        } else {
+            l++;//corner case:数组递增数组，l++会越界
+            r = R;
+        }
+        
         while(l < r) {
             int m = l + (r - l) / 2;
-            if(nums[m] <= nums[r]) {
-                r= m;
-            } else {
-                l = m + 1;
-            }
-        }
-        
-        if(target > nums[nums.length - 1]) {
-            r = l;
-            l = left;
-        } else {
-            r = nums.length - 1;
-        }
-        while(l < r) {
-            int m = l +(r - l) / 2;
             if(nums[m] >= target) r = m;
             else l = m + 1;
         }
-        return nums[l] == target ? true : false;
+        return nums[r] == target; //返回的是r,因为l可能越界
     }
 }
