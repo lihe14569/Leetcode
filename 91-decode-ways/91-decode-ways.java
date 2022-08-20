e3d05833acb5f3aceedf37b62eeac324e4397206
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-08-20 15:19:35 -0600

    Time: 2 ms (68.29%), Space: 42.7 MB (25.62%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,16 +1,15 @@
 class Solution {
     public int numDecodings(String s) {
+        //iterative dp (botton-up method)
+        if(s.length() == 0) return 1;
         int[] dp = new int[s.length() + 1];
-        
-        //base case
         dp[0] = 1;
         dp[1] = s.charAt(0) == '0' ? 0 : 1;
-        
         for(int i = 2; i <= s.length(); i++) {
-            int one = Integer.valueOf(s.substring(i - 1, i));
-            int two = Integer.valueOf(s.substring(i - 2, i));
-            if(one != 0) dp[i] += dp[i - 1];
-            if(two >= 10 && two <= 26) dp[i] += dp[i - 2];
+            int twoDigits = Integer.valueOf(s.substring(i - 2, i));
+            int oneDigit = Integer.valueOf(s.substring(i - 1, i));
+            if(twoDigits >= 10 && twoDigits <= 26) dp[i] += dp[i - 2];
+            if(oneDigit > 0) dp[i] += dp[i - 1];
         }
         return dp[s.length()];
     }
