6defba5c7a177ec3d30827ac31e171b513b7639d
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-08-25 15:41:55 -0600

    Time: 3 ms (83.24%), Space: 54.1 MB (81.96%) - LeetHub

--- 645-set-mismatch/645-set-mismatch.java
+++ 645-set-mismatch/645-set-mismatch.java
@@ -1,21 +1,21 @@
-public class Solution {
+class Solution {
     public int[] findErrorNums(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            while (nums[i] != i + 1 && nums[nums[i] - 1] != nums[i]) {
+        //把所有数有序的放在对应的位置上
+        for(int i = 0; i < nums.length; i++) {
+            while(nums[i] != i + 1 && nums[i] != nums[nums[i] - 1]) {
                 swap(nums, i, nums[i] - 1);
             }
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != i + 1) {
-                return new int[]{nums[i], i + 1};
-            }
+        
+        //扫描一遍
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != i + 1) return new int[]{nums[i], i + 1};
         }
-        return null;
-}
-
-    private void swap(int[] nums, int i, int j) {
-        int tmp = nums[i];
+        return new int[]{-1, -1};
+    }
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
         nums[i] = nums[j];
-        nums[j] = tmp;
+        nums[j] = temp;
     }
 }
