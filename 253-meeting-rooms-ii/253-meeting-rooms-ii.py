b081ed04efaeebed48afbddf59f903cad402ae75
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-08-26 22:46:05 -0600

    Time: 126 ms (47.59%), Space: 17.5 MB (50.37%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -1,12 +1,11 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        #pq
-        intervals.sort(key = lambda x: x[0])
-        heap = []
-        heapq.heappush(heap, intervals[0][1])
-        for i in intervals[1:]:
-            if i[0] >= heap[0]:
-                heapq.heappop(heap)
-            heapq.heappush(heap, i[1])
-        return len(heap)
-                
+        intervals.sort(key = lambda x : x[0])
+        rooms = []
+        heapq.heappush(rooms, intervals[0][1])
+        
+        for interval in intervals[1:]:
+            if interval[0] >= rooms[0]:
+                heapq.heappop(rooms)
+            heapq.heappush(rooms, interval[1])
+        return len(rooms)
