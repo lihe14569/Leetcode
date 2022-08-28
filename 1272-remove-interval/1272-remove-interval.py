f46f9e4fbdd42ed60882f9090af0e7cac265cf21
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-08-28 15:03:48 -0600

    Time: 597 ms (47.30%), Space: 20.7 MB (86.31%) - LeetHub

--- 1272-remove-interval/1272-remove-interval.py
+++ 1272-remove-interval/1272-remove-interval.py
@@ -1,14 +1,21 @@
 class Solution:
     def removeInterval(self, intervals: List[List[int]], toBeRemoved: List[int]) -> List[List[int]]:
         res = []
-        for curr in intervals:
-            if curr[1] < toBeRemoved[0] or curr[0] > toBeRemoved[1]:
-                res.append(curr)
+        intervals.sort(key = lambda x : x[0])
+        for interval in intervals:
+            if toBeRemoved:
+                if interval[1] <= toBeRemoved[0]:
+                    res.append(interval)
+                elif interval[0] >= toBeRemoved[1]:
+                    res.append(interval)
+                    toBeRemoved = None
+                else:#remove interval
+                    if interval[0] < toBeRemoved[0]:
+                        res.append([interval[0], toBeRemoved[0]])
+                    if interval[1] > toBeRemoved[1]:
+                        res.append([toBeRemoved[1], interval[1]])
+                    
             else:
-                if curr[0] < toBeRemoved[0]:
-                    res.append([curr[0], toBeRemoved[0]])
-                if curr[1] > toBeRemoved[1]:
-                    res.append([toBeRemoved[1], curr[1]])
+                res.append(interval)
         return res
-                    
-                    
+        
