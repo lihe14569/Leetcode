f2f901ae9cfd0f2386a92107254aa40840648aa9
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-08-28 21:49:42 -0600

    Time: 31 ms (85.15%), Space: 50.2 MB (96.67%) - LeetHub

--- 1229-meeting-scheduler/1229-meeting-scheduler.java
+++ 1229-meeting-scheduler/1229-meeting-scheduler.java
@@ -2,17 +2,17 @@ class Solution {
     public List<Integer> minAvailableDuration(int[][] slots1, int[][] slots2, int duration) {
         Arrays.sort(slots1, (a, b) -> a[0] - b[0]);
         Arrays.sort(slots2, (a, b) -> a[0] - b[0]);
-        
-        int p1 = 0, p2 = 0;
-        while(p1 < slots1.length && p2 < slots2.length) {
-            int start = Math.max(slots1[p1][0], slots2[p2][0]);
-            int end = Math.min(slots1[p1][1], slots2[p2][1]);
-            if(end - start >= duration) return Arrays.asList(start, start + duration);
-            if(slots1[p1][1] < slots2[p2][1]) {
-                p1++;
-            } else {
-                p2++;
+        int i = 0, j = 0;
+        while(i < slots1.length && j < slots2.length) {
+            int start = Math.max(slots1[i][0],slots2[j][0]);
+            int end = Math.min(slots1[i][1], slots2[j][1]);
+            if(end - start >= duration) {
+                return Arrays.asList(start, start + duration);
             }
+            if(slots1[i][1] < slots2[j][1])
+                i++;
+            else 
+                j++;
         }
         return new ArrayList<>();
     }
