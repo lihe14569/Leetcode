ba6197b93ae77441fa57370e69c50745c0ce3786
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-09-03 00:28:17 -0600

    Time: 75 ms (34.01%), Space: 14.2 MB (73.81%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
@@ -1,31 +1,25 @@
 class Solution:
-    def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
-        res = set()
-        lst= []
-        def backtrack(lst):
-            nonlocal res
-            if len(lst) == n: 
-                val = 0
-                for num in lst:
-                    val = val * 10 + num
-                res.add(val)
-                return
-            if not lst:
-                for i in range(1, 10):
-                    lst.append(i)
-                    backtrack(lst)
-                    lst.pop()
-            else:
-                prev = lst[-1]
-                if 0 <= prev + k < 10:
-                    lst.append(prev + k)
-                    backtrack(lst)
-                    lst.pop()
-                if 0 <= prev - k < 10:
-                    lst.append(prev - k)
-                    backtrack(lst)
-                    lst.pop()
-        backtrack(lst)
-        
-        return list(res)                
-            
+    def numsSameConsecDiff(self, N: int, K: int) -> List[int]:
+
+        if N == 1:
+            return [i for i in range(10)]
+
+        ans = []
+        def DFS(N, num):
+            # base case
+            if N == 0:
+                return ans.append(num)
+
+            tail_digit = num % 10
+            # using set() to avoid duplicates when K == 0
+            next_digits = set([tail_digit + K, tail_digit - K])
+
+            for next_digit in next_digits:
+                if 0 <= next_digit < 10:
+                    new_num = num * 10 + next_digit
+                    DFS(N-1, new_num)
+
+        for num in range(1, 10):
+            DFS(N-1, num)
+
+        return list(ans)
