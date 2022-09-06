ca3a2fd4eebb44f932946666935bb7efeea3f5c1
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-09-06 00:16:44 -0600

    Time: 0 ms (100.00%), Space: 42.4 MB (60.70%) - LeetHub

--- 426-convert-binary-search-tree-to-sorted-doubly-linked-list/426-convert-binary-search-tree-to-sorted-doubly-linked-list.java
+++ 426-convert-binary-search-tree-to-sorted-doubly-linked-list/426-convert-binary-search-tree-to-sorted-doubly-linked-list.java
@@ -1,45 +1,21 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-
-    public Node() {}
-
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val,Node _left,Node _right) {
-        val = _val;
-        left = _left;
-        right = _right;
-    }
-};
-*/
-
 class Solution {
+    Node first = null, last = null;
     public Node treeToDoublyList(Node root) {
-        if(root == null) return null;
-        Node first = null, last = null;
-        Deque<Node> stack = new ArrayDeque<>();
-        while(!stack.isEmpty() || root != null) {
-            while(root != null) {
-                stack.push(root);
-                root = root.left;
-            }
-            root = stack.pop();
-            if(first == null) first = root;
-            else if(last != null) {
-                last.right = root;
-                root.left = last;
-            }
-            last = root;
-            root = root.right;
-        }
+        if(root == null) return root;
+        inorder(root);
         first.left = last;
         last.right = first;
         return first;
+    }
+    public void inorder(Node root) {
+        if(root == null) return;
+        inorder(root.left);
+        if(first == null) first = root;
+        else if(last != null) {
+            last.right = root;
+            root.left = last;
+        }
+        last = root;
+        inorder(root.right);
     }
 }
