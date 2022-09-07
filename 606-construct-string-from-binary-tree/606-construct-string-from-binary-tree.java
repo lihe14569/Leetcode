c956be17eb110340c1fee7a91ecd53671eec5e17
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-09-06 19:00:54 -0600

    Time: 16 ms (71.25%), Space: 43.2 MB (89.85%) - LeetHub

--- 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.java
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.java
@@ -1,15 +1,31 @@
-public class Solution {
-    public String tree2str(TreeNode t) {
-        if (t == null) return "";
-        
-        String result = t.val + "";
-        
-        String left = tree2str(t.left);
-        String right = tree2str(t.right);
-        
-        if (left == "" && right == "") return result;
-        if (left == "") return result + "()" + "(" + right + ")";
-        if (right == "") return result + "(" + left + ")";
-        return result + "(" + left + ")" + "(" + right + ")";
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public String tree2str(TreeNode root) {
+        if(root == null) return "";
+        String r = root.val + "";
+        String left = tree2str(root.left);
+        String right = tree2str(root.right);
+        if(left.equals("") && right.equals(""))
+            return r;
+        else if(left.equals(""))
+            return r + "()" + "(" + right + ")";
+        else if(right.equals(""))
+            return r + "(" + left +")";
+        else
+        return r + "(" + left + ")" + "(" + right + ")";
     }
 }
