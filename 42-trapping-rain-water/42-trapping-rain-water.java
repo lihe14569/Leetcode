c9e44a1da00afba6def877625c64e32c9aa33ac7
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-09-19 00:02:28 -0600

    Time: 11 ms (12.26%), Space: 50.5 MB (5.45%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,20 +1,16 @@
 class Solution {
-     public int trap(int[] height) {
-        //two pointer
-        int res = 0;
-        Stack<Integer> stack = new Stack<>();
+    public int trap(int[] height) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int res =0;
         for(int i = 0; i < height.length; i++) {
-            while(!stack.isEmpty() && height[i] > height[stack.peek()]) {
-                int l = stack.pop();
-                //如果到了最左边，没有左边界，不能蓄水，跳出循环
+            while(!stack.isEmpty() && height[i] > height[stack.peekLast()]) {
+                int lowIndex = stack.pollLast();
                 if(stack.isEmpty()) break;
-                //低点的左右两边，取最小最为蓄水墙高度
-                int h = Math.min(height[i], height[stack.peek()]);
-                //蓄水宽度是左右墙的距离- 2，去掉两个墙
-                int width = i - stack.peek() - 1;
-                res += width * (h  - height[l]);
+                int wall = Math.min(height[i], height[stack.peekLast()]);
+                int width = i - stack.peekLast() - 1;
+                res +=(wall - height[lowIndex]) * width;
             }
-            stack.push(i);
+            stack.offerLast(i);
         }
         return res;
     }
