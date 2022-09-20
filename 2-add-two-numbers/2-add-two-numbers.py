c17610c7e0cd32acc0b8bdba03480ee5c17e0cfc
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-09-20 00:45:48 -0600

    Time: 138 ms (26.07%), Space: 13.8 MB (86.14%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -1,17 +1,24 @@
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        int l = 0, res = 0;
-        for(int i = 0;i < s.length(); i++) {
-            char c1 = s.charAt(i);
-            map.put(c1, map.getOrDefault(c1, 0) + 1);
-            while(map.get(c1) > 1) {
-                char c2 = s.charAt(l);
-                map.put(c2, map.get(c2) - 1);
-                l++;
-            }
-            res = Math.max(res, i - l + 1);
-        }
-        return res;
-    }
-}
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        if not l1: return l2
+        if not l2: return l1
+        
+        dummy = ListNode(-1)
+        curr = dummy
+        carry = 0
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            node = ListNode((val1 + val2 + carry) % 10)
+            carry = (val1 + val2 + carry) // 10
+            curr.next = node
+            curr = curr.next;
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            
+        return dummy.next
