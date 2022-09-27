04af5ea845777ad7488014f6cec806f5c1c32422
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-09-26 22:39:50 -0600

    Time: 39 ms (33.04%), Space: 54.4 MB (44.82%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.java
+++ 838-push-dominoes/838-push-dominoes.java
@@ -1,28 +1,45 @@
 class Solution {
-    public String pushDominoes(String d) {
-        d = 'L' + d + 'R';
-        StringBuilder res = new StringBuilder();
-        for (int i = 0, j = 1; j < d.length(); ++j) {
-            if (d.charAt(j) == '.') continue;
-            int middle = j - i - 1;
-            if (i > 0)
-                res.append(d.charAt(i));
-            if (d.charAt(i) == d.charAt(j))
-                for (int k = 0; k < middle; k++)
-                    res.append(d.charAt(i));
-            else if (d.charAt(i) == 'L' && d.charAt(j) == 'R')
-                for (int k = 0; k < middle; k++)
-                    res.append('.');
-            else {
-                for (int k = 0; k < middle / 2; k++)
-                    res.append('R');
-                if (middle % 2 == 1)
-                    res.append('.');
-                for (int k = 0; k < middle / 2; k++)
-                    res.append('L');
+    public String pushDominoes(String dominoes) {
+        dominoes = "L" + dominoes + "R";
+        char[] arr = dominoes.toCharArray();    
+        int pi = 0;
+        
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == 'L'){
+                if(arr[pi] == 'L'){
+                    for(int x = pi + 1; x < i; x++){
+                        arr[x] = 'L';
+                    }
+                }else if(arr[pi] == 'R'){
+                    int lo = pi + 1;
+                    int hi = i - 1;
+                    
+                    while(lo < hi){
+                        arr[lo] = 'R';
+                        arr[hi] = 'L';
+                        lo++;
+                        hi--;
+                    }
+                }
+                pi = i;
+            }else if(arr[i] == 'R'){
+                if(arr[pi] == 'L'){
+                  // do nothing  
+                }else if(arr[pi] == 'R'){
+                    for(int x = pi + 1; x < i; x++){
+                        arr[x] = 'R';
+                    }
+                }
+                pi = i;
             }
-            i = j;
         }
-        return res.toString();
+        
+        StringBuilder sb = new StringBuilder("");
+        
+        for(int i=1; i<arr.length - 1; i++){
+            sb.append(arr[i]);
+        }
+        
+        return sb.toString();
     }
 }
