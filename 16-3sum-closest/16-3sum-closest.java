39ec881383ea267c4ae48999c9323518363566b8
lihe14569 <54082005+lihe14569@users.noreply.github.com>
2022-10-08 18:50:49 -0600

    Time: 94 ms (53.87%), Space: 52.2 MB (5.57%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.java
+++ 16-3sum-closest/16-3sum-closest.java
@@ -1,24 +1,23 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
-        int diff = Integer.MAX_VALUE;
-        int res = 0;
-        for(int i = 0; i < nums.length - 2; i++) {
-            for(int j = i + 1, k = nums.length -1; j < k; j++) {
-                while(k - 1 > j && nums[i] + nums[j] + nums[k - 1] >= target) k--;
-                int sum = nums[i] + nums[j] + nums[k];
-                if(Math.abs(sum - target) < diff) {
-                    diff = Math.abs(sum - target);
-                    res = sum;
-                }
-                if(k - 1 > j) {
-                    sum = nums[i] + nums[j] + nums[k - 1];
-                    if(target - sum < diff) {
-                        diff = target - sum;
-                        res = sum;
-                    }
-                }
+        //初始一个值，确保可以比较
+        Integer res = nums[0] + nums[1] + nums[2];
+        for(int i = 0; i < nums.length; i++) {
+            int l = i + 1, r = nums.length - 1;
+            res = twoSum(nums, i, l, r, target, res);
+        }
+        return res;
+    }
+    public int twoSum(int[] nums, int i, int l, int r, int target, Integer res) {
+        while(l < r) {
+            int sum = nums[i] + nums[l] + nums[r];
+            if(Math.abs(sum - target) < Math.abs(res - target)) {
+                res = sum;
+                // System.out.println("res: " + res);
             }
+            if(sum > target) r--;
+            else l++;
         }
         return res;
     }
